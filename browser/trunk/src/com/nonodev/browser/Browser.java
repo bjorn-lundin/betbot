f84ba73a47dbee38e47e1f33325b20bd5770d281
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2011-07-11 00:51:13 +0000

    General cleanup

--- browser/trunk/src/com/nonodev/browser/Browser.java
+++ browser/trunk/src/com/nonodev/browser/Browser.java
@@ -1,23 +1,10 @@
 package com.nonodev.browser;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.KeyStore;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLException;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import java.util.ResourceBundle;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -28,12 +15,8 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
-import org.apache.http.conn.ssl.BrowserCompatHostnameVerifier;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.conn.ssl.TrustStrategy;
@@ -41,11 +24,7 @@ import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HTTP;
@@ -55,26 +34,23 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-class Browser {
+public class Browser {
     private String username = null;
     private String password = null;
     private String url = null;
     private String useragent = null;
     private String basicRealm = null;
-    private boolean ignoreHostnameVerifier = true;
-    private boolean ignoreSSLHostnameMatch = true;
+    private boolean allowAllHostname = false;
     private DefaultHttpClient httpclient = null;
 
-    public Browser(Map<String, String> config, boolean ignoreHostnameVerifier,
-            boolean ignoreSSLHostnameMatch) {
-        this.useragent = config.get("useragent");
-        this.username = config.get("username");
-        this.password = config.get("password");
-        this.url = config.get("url");
-        this.basicRealm = config.get("basicRealm");
-        this.ignoreHostnameVerifier = ignoreHostnameVerifier;
-        this.ignoreSSLHostnameMatch = ignoreSSLHostnameMatch;
-        initClient2();
+    public Browser(ResourceBundle properties) {
+        this.useragent = properties.getString("useragent");
+        this.username = properties.getString("username");
+        this.password = properties.getString("password");
+        this.url = properties.getString("url");
+        this.allowAllHostname = Boolean.parseBoolean(
+        		properties.getString("allowAllHostname").toLowerCase());
+        initClient();
     }
 
     public void shutdownClient() {
@@ -82,103 +58,28 @@ class Browser {
     }
 
     private void initClient() {
-        //HttpsURLConnection huc;
-
-        //TrustStrategy
-
-
-        if (ignoreHostnameVerifier) {
-            try {
-                SSLSocketFactory sf = null;
-                // Set up a TrustManager that trusts
-                // everything (self signed et.al.)
-                SSLContext sslContext = SSLContext.getInstance("SSL");
-                sslContext.init(null, new TrustManager[] {
-                    new X509TrustManager() {
-                        @Override
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
-
-                        @Override
-                        public void checkClientTrusted(X509Certificate[] certs,
-                                String authType) {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(X509Certificate[] certs,
-                                String authType) {
-                        }
-                    }
-                }, new SecureRandom());
-                sf = new SSLSocketFactory(sslContext);
-                if (ignoreSSLHostnameMatch) {
-                    sf.setHostnameVerifier(new X509HostnameVerifier() {
-                        public boolean verify(String hostname,
-                                SSLSession session) {
-                            return true;
-                        }
-
-                        public void verify(String host, String[] cns, String[]
-                                subjectAlts) throws SSLException {
-                        }
-
-                        public void verify(String host, X509Certificate cert)
-                                throws SSLException {
-                        }
-
-                        public void verify(String host, SSLSocket ssl)
-                                throws IOException {
-                        }
-                    });
-                }
-
-
-
-                Scheme httpsScheme = new Scheme("https", sf, 443);
-                SchemeRegistry schemeRegistry = new SchemeRegistry();
-                schemeRegistry.register(httpsScheme);
-                HttpParams params = new BasicHttpParams();
-                ClientConnectionManager cm = new SingleClientConnManager(params,
-                        schemeRegistry);
-                httpclient = new DefaultHttpClient(cm, params);
-            }
-            catch (Exception e) {
-            }
-        } else {
-            httpclient = new DefaultHttpClient();
-        }
-        httpclient.getParams().setParameter("USER_AGENT", useragent);
-        httpclient.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy() {
-            @Override
-            public long getKeepAliveDuration(HttpResponse response,
-                    HttpContext context) {
-                long keepAlive = super.getKeepAliveDuration(response, context);
-                if (keepAlive == -1) {
-                    // Keep connections alive 5 seconds if a keep-alive value
-                    // has not be explicitly set by the server
-                    keepAlive = 5000;
-                }
-                return keepAlive;
-            }
-        });
-    }
-
-    private void initClient2() {
         httpclient = new DefaultHttpClient();
-        TrustStrategy trustStrategy = new TrustSelfSignedStrategy();
-        X509HostnameVerifier hostnameVerifier = new AllowAllHostnameVerifier();
-        try {
-            SSLSocketFactory sf =
-                    new SSLSocketFactory(trustStrategy, hostnameVerifier);
-            Scheme sch = new Scheme("https", 443, sf);
-            httpclient.getConnectionManager().getSchemeRegistry().register(sch);
+        
+        if (allowAllHostname) {
+	        TrustStrategy trustStrategy = new TrustSelfSignedStrategy();
+	        X509HostnameVerifier hostnameVerifier = 
+	        		new AllowAllHostnameVerifier();
+	        try {
+	            SSLSocketFactory sf =
+	                    new SSLSocketFactory(trustStrategy, hostnameVerifier);
+	            Scheme scheme = new Scheme("https", 443, sf);
+	            httpclient.getConnectionManager().getSchemeRegistry().
+	            		register(scheme);
+	        }
+	        catch (Exception e) {
+	            e.getStackTrace();
+	        }
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        httpclient.getParams().setParameter(HttpProtocolParams.USER_AGENT, useragent);
-        httpclient.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy() {
+        
+        httpclient.getParams().setParameter(
+        		HttpProtocolParams.USER_AGENT, useragent);
+        httpclient.setKeepAliveStrategy(
+        		new DefaultConnectionKeepAliveStrategy() {
             @Override
             public long getKeepAliveDuration(HttpResponse response,
                     HttpContext context) {
@@ -193,7 +94,7 @@ class Browser {
         });
     }
 
-    private String content(HttpEntity he) {
+    private String getContent(HttpEntity he) {
         StringBuilder sb = null;
         try {
             BufferedReader reader = new BufferedReader(
@@ -213,13 +114,17 @@ class Browser {
     public String get(String url) {
         HttpGet httpget = new HttpGet(url);
         HttpResponse response = null;
-
+        String content = null;
+        
         try {
             response = httpclient.execute(httpget);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return content(response.getEntity());
+        if (null != response) {
+        	content = getContent(response.getEntity());
+        }
+        return content;
     }
 
     public String basicLogin() {
@@ -260,7 +165,7 @@ class Browser {
                         doc.select("input[name=authenticity_token]").first();
                 System.out.println(at.attr("value"));
                 authToken = at.attr("value");
-                System.out.println(content(entity));
+                System.out.println(getContent(entity));
             }
             EntityUtils.consume(entity);
         } catch (Exception e) {
@@ -317,7 +222,7 @@ class Browser {
             }
             System.out.println("Location: " + response. getAllHeaders());
             entity = response.getEntity();
-            System.out.println(content(entity));
+            System.out.println(getContent(entity));
             EntityUtils.consume(entity);
             System.out.println("Login form get: " + response.getStatusLine());
             System.out.println("Post logon cookies:");
--- browser/trunk/src/com/nonodev/browser/Main.java
+++ browser/trunk/src/com/nonodev/browser/Main.java
@@ -1,7 +1,5 @@
 package com.nonodev.browser;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Main {
@@ -18,44 +16,16 @@ public class Main {
                     + "relevant properties for production.");
             System.exit(1);
         }
-        Map<String, String> config = new HashMap<String, String>();
-        config.put("username", properties.getString("username"));
-        config.put("password", properties.getString("password"));
-        config.put("url", properties.getString("url"));
-        config.put("useragent", properties.getString("useragent"));
-        boolean ignoreSSLTrustedCert = true;
-        boolean ignoreSSLHostnameMatch = true;
-        Browser browser = new Browser(config, ignoreSSLTrustedCert,
-                ignoreSSLHostnameMatch);
+        Browser browser = new Browser(properties);
         //System.out.println(browser.get("https://secure.pensionsmyndigheten.se/"));
         //System.out.println(browser.get("http://pensionsmyndigheten.se/"));
-        System.out.println(browser.get("https://nonodev.com"));
-        System.exit(99);
+        
+        //System.out.println(browser.get("https://nonodev.com"));
+        //System.exit(99);
+        
         browser.FormLogin(browser.basicLogin());
         String html = browser.get("https://nonodev.com/its/my/page");
         System.out.println(html);
         browser.shutdownClient();
-    }
-
-    private static void test1(){
-        Map<String, String> m = new HashMap<String, String>();
-        m.put("user", "joakim");
-        m.put("password", "secPasswd");
-        System.out.println(m.get("user"));
-        System.out.println(m.get("password"));
-        System.out.println(m);
-
-        ResourceBundle properties = null;
-        try {
-            properties = ResourceBundle.getBundle("com.nonodev.browser.productio");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Please copy default.properties to "
-                    + "production.properties and set "
-                    + "relevant properties for production.");
-            System.exit(1);
-        }
-        System.out.println(properties.getString("url"));
-        System.exit(0);
     }
 }
