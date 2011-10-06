8fddf6dd11be2e61dcb388ae6b6471767311fac1
nonobetmats <nonobetmats@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2011-10-06 22:38:30 +0000

    Updates of Race

--- client/NoNoBetClient/trunk/NoNoBetDb/Race.vb
+++ client/NoNoBetClient/trunk/NoNoBetDb/Race.vb
@@ -8,6 +8,13 @@ Public Class Race
     Private Const _OrderByDateTrackIdClause As String = " ORDER BY date,track,id"
     Private Const _OrderByIdClause As String = " ORDER BY id"
     Private Const _WhereNullClause As String = " WHERE null = null"
+    Private Const _v75_types As String = "'v75-1','v75-2','v75-3','v75-4','v75-5','v75-6','v75-7'"
+    Private Const _v64_types As String = "'v64-1','v64-2','v64-3','v64-4','v65-5'"
+    Private Const _v3_types As String = "'v3-1','v3-2','v3-3'"
+    Private Const _v4_types As String = "'v4-1','v4-2','v4-3','v4-4'"
+    Private Const _v5_types As String = "'v5-1','v5-2','v5-3','v5-4','v5-5'"
+    Private Const _dd_types As String = "'dd-1','dd-2'"
+    Private Const _ld_types As String = "'ld-1','ld-2'"
 
     ''' <summary>
     ''' Build: SELECT DISTINCT date,track FROM race ORDER BY date,track
@@ -131,6 +138,20 @@ Public Class Race
     ''' <remarks></remarks>
     Public Shared Function BuildNullTrackRacesSelectSql() As String
         Return _TrackRacesSelect + _WhereNullClause
+    End Function
+
+    Public Shared Function BuildV75RacedaysSelectSql(ByVal startDate As Date, ByVal endDate As Date, ByVal addOrderBy As Boolean) As String
+        Dim s As String = Nothing
+
+        s = "SELECT DISTINCT race.date,race.track FROM race JOIN race_bettype ON race.id = race_bettype.race_id " + _
+            "WHERE (race_bettype.bettype_id = 'v75-1')" + _
+                  " AND (race.date >= " + DateToSqlString(startDate, DateFormatMode.DateOnly) + ")" + _
+                  " AND (race.date <= " + DateToSqlString(endDate, DateFormatMode.DateOnly) + ")"
+        If addOrderBy Then
+            s += " ORDER BY race.date"
+        End If
+
+        Return s
     End Function
 
 End Class
