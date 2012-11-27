7a691aa743e01853b99dd519bfe1369532470aab
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2012-11-27 22:25:10 +0000

    parsing/inserting livefeedok

--- bnlbot/live_feeder_2.py
+++ bnlbot/live_feeder_2.py
@@ -16,7 +16,8 @@ import subprocess
 
 class Game(object):
 ###########################################################
-    def __init__(self, line):
+    def __init__(self, line, conn):
+        self.conn = conn
         self.id = None
         self.kickoff = None
         self.home_team_id = None
@@ -99,11 +100,47 @@ class Game(object):
         vals = line.split("|")
         temp = dict(zip(keys, vals))
         
-        self.home_team = temp['home_team'].strip()
-        self.away_team = temp['away_team'].strip()
+        self.home_team = temp['home_team'].strip().lower()
+        self.away_team = temp['away_team'].strip().lower()
         scores = temp["score"].split('-')
         self.home_goals = scores[0]
         self.away_goals = scores[1]
+
+
+        # see if we have the teams in team aliases
+        cur2 = self.conn.cursor()
+        cur2.execute("select TEAM_ID from TEAM_ALIASES \
+                      where TEAM_ALIAS = %s", (self.home_team,))
+        row = cur2.fetchone()
+        if cur2.rowcount == 1 :
+            self.home_team_id = row[0]
+        else :
+            print self.home_team, 'is missing in TEAM_ALIASES'
+        cur2.close()
+        
+        cur3 = self.conn.cursor()
+        cur3.execute("select TEAM_ID from TEAM_ALIASES \
+                      where TEAM_ALIAS = %s", (self.away_team,))
+        row = cur3.fetchone()
+        if cur3.rowcount == 1 :
+            self.away_team_id = row[0] 
+        else :
+            print self.away_team, 'is missing in TEAM_ALIASES'
+        cur3.close()
+
+        # now ,get the match id
+        cur4 = self.conn.cursor()
+        cur4.execute("select XML_SOCCER_ID from GAMES \
+                      where HOME_TEAM_ID = %s and AWAY_TEAM_ID = %s", 
+                      (self.home_team_id,self.away_team_id))
+        row = cur4.fetchone()
+        if cur4.rowcount == 1 :
+            self.id = row[0]
+        else :
+            print self.home_team, 'is missing in TEAM_ALIASES'
+        cur4.close()
+
+
 
 
     #############################################################   
@@ -123,38 +160,41 @@ class Game(object):
               'away_team:', self.away_team, \
               'time_in_game:', self.time_in_game, \
               'home_goals:', self.home_goals, \
-              'away_goals:', self.away_goals
+              'away_goals:', self.away_goals, \
+              'home_team_id:', self.home_team_id, \
+              'away_team_id:', self.away_team_id
     ################################################################
     def print_info(self) :
         print 'info', self.info
     ###############################################################
 
-    def update_db(self, conn):
-        cur = conn.cursor()
-        cur.execute("select * from GAMES where XML_SOCCER_ID = %s", \
-                     (self.Xml_Soccer_Id,))
+    def update_db(self):
+        cur = self.conn.cursor()
+        cur.execute("select * from GAMES where HOME_TEAM_ID = %s \
+                     and AWAY_TEAM_ID = %s", \
+                     (self.home_team_id, self.away_team_id))
         if cur.rowcount == 0 :
-            print 'insert Game', self.Xml_Soccer_Id
+            print 'insert Game', self.id
             cur.execute("insert into GAMES ( \
-                        XML_SOCCER_ID, KICKOFF, HOME_TEAM_ID, \
+                        KICKOFF, HOME_TEAM_ID, \
                         AWAY_TEAM_ID, TIME_IN_GAME, \
                         HOME_GOALS, AWAY_GOALS ) \
                         values \
-                        (%s,%s,%s,%s,%s,%s,%s)", \
-             (self.id, self.kickoff, self.home_team_id, \
+                        (%s,%s,%s,%s,%s,%s)", \
+             ( self.kickoff, self.home_team_id, \
               self.away_team_id, self.time_in_game, \
               self.home_goals, self.away_goals))
         else : 
-            print 'update Game', self.Xml_Soccer_Id
+            print 'update Game', self.id
             cur.execute("update GAMES \
-                         set KICKOFF = %s, TIME_IN_GAME = %s, \
-                         HOME_GOALS = %s, AWAY_GOALS = %s \
+                         set TIME_IN_GAME = %s , \
+                         HOME_GOALS = %s , AWAY_GOALS = %s \
                          where XML_SOCCER_ID = %s ", \
-             (self.kickoff, self.time_in_game, self.home_goals, \
+             (self.time_in_game, self.home_goals, \
               self.away_goals, self.id))
              
-        print 'insert Game into stats', self.Xml_Soccer_Id
-        cur2 = conn.cursor()
+        print 'insert Game into stats', self.id
+        cur2 = self.conn.cursor()
         cur2.execute("insert into GAMES_STATS ( \
                         XML_SOCCER_ID, KICKOFF, HOME_TEAM_ID, \
                         AWAY_TEAM_ID, TIME_IN_GAME, \
@@ -193,15 +233,23 @@ class Live_Feeder(object):
 
     def get_feed(self): 
         """get the feed"""
-        self.my_list = subprocess.check_output("lynx -dump --image_links=0 --hiddenlinks=ignore --notitle --nolist  " + self.URL_LIVE_SCORE, shell=True)
+        self.LAST_TIMESTAMP = str(time())
+        self.my_list = subprocess.check_output("lynx \
+                                                -dump \
+                                                --image_links=0 \
+                                                --hiddenlinks=ignore \
+                                                --notitle \
+                                                --nolist  " + \
+                                                self.URL_LIVE_SCORE + \
+                                                '?tmp=' + \
+                                                self.LAST_TIMESTAMP, \
+                                                shell=True)
         self.lines = self.my_list.split("\n")
-
         
     def do_throttle(self):
         """return only when it is safe to send another data request"""
         print 'Wait for', 15, 'seconds'
-        sleep(15)
-           
+        sleep(15)       
         
     def start(self):
         """start the main loop"""
@@ -209,9 +257,10 @@ class Live_Feeder(object):
                 stripped_line = line.strip()
                 if stripped_line.find('Referenser') > -1 :
                         break
-                my_game = Game(stripped_line)
+                my_game = Game(stripped_line, self.conn)
                 if my_game.found :
                     my_game.print_me_nice()
+                    my_game.update_db()
                     
         self.conn.commit()    
 ###################################################################
