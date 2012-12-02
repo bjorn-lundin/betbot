d6854c22bd8c32fbf20d829cdfdef9c76e98093a
bnl <b.f.lundin@gmail.com>
2012-12-02 21:15:17 +0000

    iphone friendly

--- bnlbot/prospects.py
+++ bnlbot/prospects.py
@@ -27,34 +27,98 @@ class test_db(object):
         
     def difflib_test(self):
         print 'print_team_names start'
-        cur = self.conn.cursor()
-        cur.execute("select away_team from markets")
-        rows = cur.fetchall()
-        cur.close()
-#        print rows
+#        cur = self.conn.cursor()
+#        cur.execute("select away_team from markets")
+#        rows = cur.fetchall()
+#        cur.close()
+##        print rows
+#        cur2 = self.conn.cursor()
+#        cur2.execute("select team_alias from team_aliases")
+#        rows2 = cur2.fetchall()
+#        cur2.close()
+##        print rows2
+#
+#        rows52 = []  
+#        for row in rows2 :
+#            rows52.append(row[0])
+#
+#
+#        rows5 = []  
+#        for row in rows :
+#            rows5.append(row[0])
+#            
+#        
+#        for row in rows5 :
+#            rows3 = difflib.get_close_matches(row,rows52)
+#            print row, ' -> ',  rows3
+#
+#        self.conn.commit()
+#
+####
+        cur1 = self.conn.cursor()
+        my_dict = dict()
+        cur1.execute("select ID, TEAM_NAME from UNIDENTIFIED_TEAMS \
+                      order by EVENTTIME desc")
+        row = cur1.fetchone()
+        if cur1.rowcount >= 1 :
+            my_dict['unk_id']   = row[0] 
+            my_dict['unk_name'] = row[1]
+            print "my_dict " , my_dict
+        cur1.close() 
+        
         cur2 = self.conn.cursor()
-        cur2.execute("select team_alias from team_aliases")
-        rows2 = cur2.fetchall()
+        cur2.execute("select TEAM_ID, TEAM_ALIAS from TEAM_ALIASES")
+        my_whole_list = []
+        
+        rows2 = cur2.fetchone()
+        print "row2" , rows2
+        while rows2 :
+            my_whole_list.append(rows2[1])
+            rows2 = cur2.fetchone()
         cur2.close()
-#        print rows2
+        print "passed 1, my_whole_list" ,my_whole_list 
 
-        rows52 = []  
-        for row in rows2 :
-            rows52.append(row[0])
-
-
-        rows5 = []  
-        for row in rows :
-            rows5.append(row[0])
-            
+        tmp = 'moreco'
+#        tmp.append(my_dict['unk_name'])
+        print "passed tmp ", tmp
+        result_list_name = difflib.get_close_matches(tmp, my_whole_list)
         
-        for row in rows5 :
-            rows3 = difflib.get_close_matches(row,rows52)
-            print row, ' -> ',  rows3
+        if len(result_list_name) == 0 :
+            result_list_name =  ['-', '-', '-']
+        elif len(result_list_name) == 1 :
+            result_list_name.append('-')
+            result_list_name.append('-')
+        elif len(result_list_name) == 2 :
+            result_list_name.append('-')
+        
+        result_list_id = []
+        print "passed result_list_name " , result_list_name
+        for r in result_list_name :
+            cur4 = self.conn.cursor()
+            cur4.execute("select TEAM_ID from TEAM_ALIASES \
+                      where TEAM_ALIAS = %s", (r,))
+            print "r " , r
+            rows4 = cur4.fetchone()
+            if rows4 :
+                result_list_id.append(rows4[0])
+                print "rows4[0] ",rows4[0]
+            cur4.close()
 
-        self.conn.commit()
+        if len(result_list_id) == 0 :
+            result_list_id =  [-1, -1, -1]
+        elif len(result_list_id) == 1 :
+            result_list_id.append(-1)
+            result_list_id.append(-1)
+        elif len(result_list_id) == 2 :
+            result_list_id.append(-1)
+        
+        print "result_list_id", result_list_id
 
         
+        
+        
+        
+        ####        
 
 #make print flush now!
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
