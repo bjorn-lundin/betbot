8f541f1b2c153f6f713df00d25fe5714db115d9d
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-01-10 22:42:23 +0000

    Code ready for daily download of history. Code tested by manual runs about 4 nights in a row.

--- atg/ais/src/trunk/app.py
+++ atg/ais/src/trunk/app.py
@@ -1,50 +1,136 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 '''
 The main application to run when getting data from AIS
 '''
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
+from optparse import OptionParser
 import os
 import errno
 import conf
-import logging
+import logging.handlers
 import ais
 import db
+import util
 
-AIS_RUN_MODE = 'TEST'
+# Set timezone Europe/Stockholm
+# -> central european time, UTC+1
+# -> central european summer time, UTC+2
+os.putenv('TZ','Europe/Stockholm')
 
-try:
-    os.makedirs(conf.DATADIR)
-except OSError as exception:
-    if exception.errno != errno.EEXIST:
-        raise
+LOG = logging.getLogger('AIS')
 
-def run():
-    AIS_LOG_LEVEL = logging.DEBUG
-    LOG = logging.getLogger('AIS')
-    LOG.setLevel(AIS_LOG_LEVEL)
-    FH = logging.FileHandler(conf.DATADIR + '/ais.log', mode='a')
-    FH.setLevel(AIS_LOG_LEVEL)
-    FORMAT_STRING = '%(asctime)s %(name)s %(levelname)s %(message)s'
-    FORMATTER = logging.Formatter(FORMAT_STRING)
-    FH.setFormatter(FORMATTER)
-    LOG.addHandler(FH)
-    SUDS_LOG = logging.getLogger('suds')
-    SUDS_LOG.setLevel(logging.CRITICAL)
+CREATE_DIRS = [conf.AIS_LOGDIR, conf.AIS_DATADIR, conf.AIS_METADIR]
+for cdir in CREATE_DIRS:
+    try:
+        os.makedirs(cdir)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+def init_logging():
+    '''
+    Initiate all logging
+    '''
+    ais_log_level = logging.INFO
+    LOG.setLevel(ais_log_level)
     
+    filehandler = logging.handlers.RotatingFileHandler(
+        conf.AIS_LOGDIR + '/ais.log',
+        mode='a', maxBytes=5000000, backupCount=10, 
+        encoding='utf-8', delay=0)
+    filehandler.setLevel(ais_log_level)
+    format_string = '%(asctime)s %(name)s %(levelname)s %(message)s'
+    formatter = logging.Formatter(format_string)
+    filehandler.setFormatter(formatter)
+    LOG.addHandler(filehandler)
+    
+    if conf.EMAIL_LOG_ERRORS:
+        mailhost = None
+        if conf.EMAIL_LOG_PORT == '':
+            mailhost = conf.EMAIL_LOG_HOST
+        else:
+            mailhost = (conf.EMAIL_LOG_HOST, conf.EMAIL_LOG_PORT)
+        credentials = None
+        secure = None
+        if conf.EMAIL_LOG_USER != '' and conf.EMAIL_LOG_PASS != '':
+            credentials = (conf.EMAIL_LOG_USER, conf.EMAIL_LOG_PASS)
+        if conf.EMAIL_LOG_TLS:
+            secure = ()
+        smtphandler = logging.handlers.SMTPHandler(
+            mailhost=mailhost,
+            fromaddr=conf.EMAIL_LOG_FROM,
+            toaddrs=conf.EMAIL_LOG_TO,
+            subject=conf.EMAIL_LOG_SUBJECT,
+            credentials=credentials,
+            secure=secure
+        )
+        smtphandler.setLevel(logging.ERROR)
+        smtphandler.setFormatter(formatter)
+        LOG.addHandler(smtphandler)
+    
+    suds_log = logging.getLogger('suds')
+    suds_log.setLevel(logging.CRITICAL)
+    
+def main():
+    '''
+    Main loop of application
+    '''
+    init_logging()
     LOG.info('Starting application')
+    ws_client = ais.init_ws_client(conf.AIS_WS_URL, conf.AIS_USERNAME, 
+                                   conf.AIS_PASSWORD)
+    db.init_db_client(conf.AIS_DB_URL, db_init=False)
     
-    WS_CLIENT = ais.init_ws_client(conf.WSDL_URL, conf.USERNAME, conf.PASSWORD)
-    db.init_db_client(conf.DB_URL, db_init=True)
-    
-    ais.write_methods_file(WS_CLIENT, conf.DATADIR)
-    ais.write_wsdl_file(WS_CLIENT, conf.DATADIR)
-    
-    ais.raceday_calendar(WS_CLIENT, conf.DATADIR)
-    #ais.fetchRaceDayCalendarSimple(CLIENT)
-    #ais.fetchCurrentEventSequenceNumber(CLIENT)
+    init_db = 'init_db'
+    meta_files = 'write_meta_files'
+    ais_racedays = 'get_ais_racedays'
+    daily_download = 'daily_download'
+    usage = \
+        "usage: %(prog)s [%(com0)s|%(com1)s|%(com2)s|%(com3)s]" % \
+        {
+            'prog':'%prog',
+            'com0':init_db,
+            'com1':meta_files,
+            'com2':ais_racedays, 
+            'com3':daily_download
+        }
+    parser = OptionParser(usage)
+    args = parser.parse_args()[1]
+    if len(args) < 1:
+        parser.error("Please state command to run!")
+    if init_db in args:
+        LOG.info('Running ' + init_db)
+        db.init_db_client(conf.AIS_DB_URL, db_init=True)
+    if meta_files in args:
+        LOG.info('Running ' + meta_files)
+        util.write_meta_files(client=ws_client, path=conf.AIS_METADIR)
+    if ais_racedays in args:
+        LOG.info('Running ' + ais_racedays)
+        params = {
+            'client':ws_client,
+            'datadir':conf.AIS_DATADIR,
+            'metadir':conf.AIS_METADIR,
+            'ais_version':conf.AIS_VERSION,
+            'ais_type':conf.AIS_TYPE,
+            'save_soap_file':True
+        }
+        ais.raceday_calendar(params)
+    if daily_download in args:
+        LOG.info('Running ' + daily_download)
+        params = {
+            'client':ws_client,
+            'datadir':conf.AIS_DATADIR,
+            'metadir':conf.AIS_METADIR,
+            'ais_version':conf.AIS_VERSION,
+            'ais_type':conf.AIS_TYPE,
+            'save_soap_file':True
+        }
+        ais.download_history_via_calendar(params)
     LOG.info('Ending application')
     logging.shutdown()    
+    exit(0)
 
 if __name__ == "__main__":
-    run()
-    exit(0) 
+    main()
--- atg/ais/src/trunk/util.py
+++ atg/ais/src/trunk/util.py
@@ -1,25 +1,144 @@
+#-*- coding: utf-8 -*-
 '''
 Utilities for converting to and from AIS data types
 '''
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
+import logging
 import datetime
+import os.path
 
+LOG = logging.getLogger('AIS')
+
+#######################################################
+# File handling                                       #
+#######################################################
+def write_meta_files(client=None, path=None):
+    '''
+    Writes a report of methods and types found in WSDL
+    and also writes the WSDL itself to file
+    '''
+    wsdl_file = 'ais_wsdl.xml'
+    method_file = 'ais_methods.txt'
+    
+    LOG.info('Writing meta files')
+    filepath = os.path.join(path, wsdl_file)
+    filepath = os.path.normpath(filepath)
+    try:
+        filehandle = open(filepath, 'w')
+        filehandle.write(str(client.wsdl))
+    except IOError:
+        LOG.exception()
+    except:
+        LOG.exception('Unexpected error!')
+    finally:
+        filehandle.close()
+
+    filepath = os.path.join(path, method_file)
+    filepath = os.path.normpath(filepath)
+    try:
+        filehandle = open(filepath, 'w')
+        filehandle.write(str(client))
+    except IOError:
+        LOG.exception()
+    except:
+        LOG.exception('Unexpected error!')
+    finally:
+        filehandle.close()
+
+def write_file(result=None, file_name_dict=None):
+    '''
+    Write a file
+    '''
+    LOG.info('Writing file ' + file_name_dict['filename'])
+    filepath = os.path.join(file_name_dict['path'], 
+                            file_name_dict['filename'])
+    try:
+        filehandle = open(filepath, 'w')
+        filehandle.write(str(result))
+    except IOError:
+        LOG.exception()
+    except:
+        LOG.exception('Unexpected error!')
+    finally:
+        filehandle.close()
+
+def read_file(file_name_dict=None):
+    '''
+    Read a file
+    '''
+    result = None
+    LOG.info('Reading file ' + file_name_dict['filename'])
+    filepath = os.path.join(file_name_dict['path'], 
+                            file_name_dict['filename'])
+    try:
+        filehandle = open(filepath, 'r')
+        result = filehandle.read()
+    except IOError:
+        LOG.exception()
+    except:
+        LOG.exception('Unexpected error!')
+    finally:
+        filehandle.close()
+    return result
+
+def generate_file_name(datadir = None, ais_service = None,
+                       date = None, track=None,
+                       ais_version = None, ais_type = None):
+    '''
+    Generate a dict containing path, filename and 
+    filepath (path + filename)
+    '''
+    result = {'filename':'', 'path':''}
+    result['path'] = os.path.normpath(datadir)
+    result['filename'] = ais_service
+    result['filename'] += '_'
+    result['filename'] += date_to_string(date)
+    result['filename'] += '_'
+    result['filename'] += str(track)
+    result['filename'] += '_'
+    result['filename'] += ais_version
+    result['filename'] += '_'
+    result['filename'] += ais_type
+    result['filename'] += '.xml'
+    return result
+
+def list_files_in_dir(path):
+    '''
+    List files in directory
+    '''
+    file_list = None
+    try:
+        file_list = os.listdir(path)
+    except IOError:
+        LOG.exception()
+    except:
+        LOG.exception('Unexpected error!')
+    return file_list
+
+#######################################################
+# AIS struct and date/time conversions                #
+#######################################################
 def struct_to_date(date_struct):
     '''
     Converts string parts to a date object
     '''
-    date_string = str(date_struct.year) \
-        + '-' + str(date_struct.month) \
-        + '-' + str(date_struct.date)
-    date = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+    date = datetime.datetime(
+        date_struct.year, 
+        date_struct.month, 
+        date_struct.date
+    )
     return date
 
-def date_to_struct():
+def date_to_struct(client, date):
     '''
-    Converts a date object to string parts?/array?
+    Converts a date object to a AtgDate struct
     '''
-    pass
+    struct = client.factory.create('ns4:AtgDate')
+    struct['year'] = date.year
+    struct['month'] = date.month
+    struct['date'] = date.day
+    return struct
 
 def struct_to_time(time_struct):
     '''
@@ -31,13 +150,35 @@ def struct_to_time(time_struct):
                          time_struct.tenth * 100000)
     return time
 
-def get_or_create(session, caller_clazz, caller_self, **kwargs):
+def struct_to_datetime(datetime_struct):
     '''
-    If entity exist in db return in, else create and save it
+    Converts string parts to a datetime object
     '''
-    instance = session.query(caller_clazz).filter_by(**kwargs).first()
-    if not instance:
-        instance = caller_self
-        session.add(instance)
-        session.commit()
-    return instance
+    date_str = str(datetime_struct.date.year) \
+        + '-' + str(datetime_struct.date.month) \
+        + '-' + str(datetime_struct.date.date)
+    date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+    
+    time = datetime.time(
+        datetime_struct.time.hour,
+        datetime_struct.time.minute,
+        datetime_struct.time.second
+    )
+    _datetime = datetime.datetime.combine(date, time)
+    return _datetime
+
+def date_to_string(date_time):
+    '''
+    Return a string representation of a 
+    datetime object, e.g. '20130105'
+    '''
+    return date_time.strftime('%Y%m%d')
+    
+def track_id_to_struct(client, track_id):
+    '''
+    Converts a track_id to a TrackKey struct
+    '''
+    struct = client.factory.create('ns3:TrackKey')
+    struct['trackId'] = track_id
+    return struct
+
