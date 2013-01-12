539447f64e905f012d0374d2f76a3b6163c2b1df
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-01-12 23:19:01 +0000

    Adding file

---
+++ atg/ais/src/trunk/run.sh
@@ -0,0 +1,10 @@
+# Use this script when running the application from 
+# e.g. cron or in other situaions when virtualenv
+# can't be set manually. For reference see
+# https://gist.github.com/2199506
+
+#!/bin/sh
+export VIRTUAL_ENV="/home/joabi/production/ais/py_env"
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+unset PYTHON_HOME
+exec "${@:-$SHELL}"
