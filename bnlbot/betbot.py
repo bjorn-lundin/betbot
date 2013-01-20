433b5690b79c85f30a51716376ff002abbafdf5f
bnl <b.f.lundin@gmail.com>
2013-01-20 18:35:29 +0000

    test

--- bnlbot/betbot.py
+++ bnlbot/betbot.py
@@ -39,6 +39,15 @@ class BetBot(object):
     DRY_RUN = True
     BET_CATEGORY = None
     
+    MIN_NO_OF_RUNNERS = None
+    INCLUDE_STARTED = None
+    EVENTS = None
+    ALLOWED_MARKET_NAMES = None
+    NO_OF_WINNERS = None
+    COUNTRIES = None
+    MAX_NO_OF_RUNNERS = None
+    NOT_ALLOWED_MARKET_NAMES = None    
+    
     USERNAME = None 
     PASSWORD = None 
     PRODUCT_ID = None
@@ -108,9 +117,9 @@ class BetBot(object):
 ############################# end insert_bet
 
 
-    def get_markets(self):
-        self.log.debug( 'get_markets from BetBot!!!')
-        raise OverrideError('BetBot.get_market MUST be overrridden!!!')
+#    def get_markets(self):
+#        self.log.debug( 'get_markets from BetBot!!!')
+#        raise OverrideError('BetBot.get_market MUST be overrridden!!!')
 ############################# end get_markets
 
     def do_throttle(self):
@@ -126,7 +135,62 @@ class BetBot(object):
 ############################# check_strategy
 
 
+#################### start get-markets-test
+    def get_markets(self):
+        """returns a list of markets or an error string"""
+        
+        markets = self.api.get_all_markets(
+              events = self.EVENTS,
+              hours = self.HOURS_TO_MATCH_START,
+              include_started = self.INCLUDE_STARTED, # exclude in-play markets
+              countries = self.COUNTRIES)
+              #http://en.wikipedia.org/wiki/List_of_FIFA_country_codes
+              #http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+        if type(markets) is list:
+            # sort markets by start time + filter
+            for market in markets[:]:
+#                self.log.info( 'market :' + str(market))
+             # loop through a COPY of markets 
+             #as we're modifying it on the fly...
+                markets.remove(market)
+                
+                #check for NOT allowd names..
+                market_ok = True
+                for not_allowed in self.NOT_ALLOWED_MARKET_NAMES :
+                    market_ok = market_ok and market['market_name'].lower().find(not_allowed) == -1
+                
+                # we now check for allowed market name, if noting was found above
+                if market_ok : 
+                    for allowed in self.ALLOWED_MARKET_NAMES :
+                        market_ok = market_ok and market['market_name'].lower().find(allowed) > -1
+                
+                
+                if (  market_ok
+                    and market['market_status'] == 'ACTIVE' # market is active
+                    and market['market_type'] == 'O' # Odds market only
+                    and market['no_of_winners'] == self.NO_OF_WINNERS # winner
+                    and market['no_of_runners'] >= self.MIN_NO_OF_RUNNERS # minst MIN kusar
+                    and market['no_of_runners'] <= self.MAX_NO_OF_RUNNERS # max MAX kusar
+                    and market['bet_delay'] == 0 # not started
+                    ):
+                    # calc seconds til start of game
+                    delta = market['event_date'] - self.api.API_TIMESTAMP
+                    # 1 day = 86400 sec
+                    sec_til_start = delta.days * 86400 + delta.seconds 
+                    temp = [sec_til_start, market]
+                    markets.append(temp)
+                    self.log.info( 'market :' + str(market))
+            markets.sort() # sort into time order (earliest game first)
+            return markets
+        elif markets == 'API_ERROR: NO_SESSION':
+            self.no_session = True
+        else:
+            return markets
+############################# end get_markets test
 
+
+
+############################################### get_markets
     def market_in_xmlfeed(self, market_id) :
         Found = False
         cur = self.conn.cursor()
@@ -275,7 +339,21 @@ class BetBot(object):
         self.DRY_RUN                         = bool (config.get(bet_category, 'dry_run'))
         self.BET_CATEGORY = bet_category
         if self.DRY_RUN :
-            self.BET_CATEGORY = 'DRY_RUN_' + self.BET_CATEGORY
+            self.BET_CATEGORY                = 'DRY_RUN_' + self.BET_CATEGORY
+
+        tmp_string_1                         = config.get(bet_category, 'events')
+        self.EVENTS                          = tmp_string_1.split(',') 
+        tmp_string_2                         = config.get(bet_category, 'countries')
+        self.COUNTRIES                       = tmp_string_2.split(',') 
+        self.INCLUDE_STARTED                 = bool (config.get(bet_category, 'include_started'))
+        tmp_string_3                         = config.get(bet_category, 'not_allowed_market_names')
+        self.NOT_ALLOWED_MARKET_NAMES        = tmp_string_3.split(',')
+        tmp_string_4                         = config.get(bet_category, 'allowed_market_names')        
+        self.ALLOWED_MARKET_NAMES            = tmp_string_4.split(',')
+        self.NO_OF_WINNERS                   = int (config.get(bet_category, 'no_of_winners'))
+        self.MIN_NO_OF_RUNNERS               = int (config.get(bet_category, 'min_no_of_runners'))
+        self.MAX_NO_OF_RUNNERS               = int (config.get(bet_category, 'max_no_of_runners'))
+
 
         self.USERNAME                        = config.get('Login', 'username') 
         self.PASSWORD                        = config.get('Login', 'password')
