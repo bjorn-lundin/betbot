1320f65289fda6d5645c4635f2c48f0bc9ae6771
bnl <b.f.lundin@gmail.com>
2013-01-24 22:46:13 +0000

    more analysis tools

--- bnlbot/do_plot.bash
+++ bnlbot/do_plot.bash
@@ -1,30 +1,37 @@
 #!/bin/bash
 INPUT_FILE=$1
 
-bet_type_list="DRY_RUN_HORSES_WINNER_LAY_BET \
-DRY_RUN_HOUNDS_WINNER_LAY_BET \
-"
-
-"
-DRY_RUN_HORSES_PLACE_BACK_BET \
-DRY_RUN_HOUNDS_PLACE_BACK_BET \
-DRY_RUN_HORSES_PLACE_LAY_BET \
-DRY_RUN_HOUNDS_PLACE_LAY_BET \
-DRY_RUN_HORSES_WINNER_FAVORITE_LAY_BET \
-DRY_RUN_HOUNDS_WINNER_FAVORITE_LAY_BET \
-DRY_RUN_LESS_THAN_3.5_GOALS \
-DRY_RUN_LESS_THAN_4.5_GOALS \
-DRY_RUN_MORE_THAN_0.5_GOALS \
-DRY_RUN_SCORE_SUM_IS_EVEN \
-DRY_RUN_SENDOFF_NO"
+bet_type_list="dry_run_less_than_3.5_goals \
+ away_full_one_goal_lead_time \
+ horses_winner_lay_bet \
+ home_full_one_goal_lead_time \
+ dry_run_more_than_0.5_goals \
+ dry_run_hounds_winner_lay_bet \
+ horses_place_back_bet \
+ hounds_place_back_bet \
+ dry_run_hounds_place_back_bet \
+ dry_run_hounds_place_lay_bet \
+ dry_run_horses_winner_lay_bet \
+ dry_run_hounds_winner_favorite_lay_bet \
+ hounds_place_lay_bet \
+ dry_run_horses_winner_favorite_lay_bet \
+ sendoff_no \
+ dry_run_horses_place_back_bet \
+ dry_run_horses_place_lay_bet \
+ dry_run_sendoff_no \
+ dry_run_tie_no_bet \
+ dry_run_score_sum_is_even \
+ dry_run_less_than_4.5_goals"
 
 
 date_list="13 14 15 16 17 18 19 20 21 22 23"
 
 for TYP in $bet_type_list ; do 
-  for d in $date_list ; do 
+   echo $TYP |grep lay
+   is_lay=$?
+#  for d in $date_list ; do 
       typ=$(echo $TYP | tr '[A-Z]' '[a-z]')
-      echo "$typ $d "
-      gnuplot -e "typ='$typ'" -e "dat=$d" plot_bets.cmd
-  done
+      echo $typ
+      gnuplot -e "typ='$typ'" -e "dat='Jan-13'" -e "is_lay=$is_lay" plot_bets.cmd
+#  done
 done
--- bnlbot/split.bash
+++ bnlbot/split.bash
@@ -5,27 +5,35 @@
 
 INPUT_FILE=$1
 
-bet_type_list="DRY_RUN_HORSES_WINNER_LAY_BET \
-DRY_RUN_HOUNDS_WINNER_LAY_BET \
-DRY_RUN_HORSES_PLACE_BACK_BET \
-DRY_RUN_HOUNDS_PLACE_BACK_BET \
-DRY_RUN_HORSES_PLACE_LAY_BET \
-DRY_RUN_HOUNDS_PLACE_LAY_BET \
-DRY_RUN_HORSES_WINNER_FAVORITE_LAY_BET \
-DRY_RUN_HOUNDS_WINNER_FAVORITE_LAY_BET \
-DRY_RUN_LESS_THAN_3.5_GOALS \
-DRY_RUN_LESS_THAN_4.5_GOALS \
-DRY_RUN_MORE_THAN_0.5_GOALS \
-DRY_RUN_SCORE_SUM_IS_EVEN \
-DRY_RUN_SENDOFF_NO"
+bet_type_list="dry_run_less_than_3.5_goals \
+ away_full_one_goal_lead_time \
+ horses_winner_lay_bet \
+ home_full_one_goal_lead_time \
+ dry_run_more_than_0.5_goals \
+ dry_run_hounds_winner_lay_bet \
+ horses_place_back_bet \
+ hounds_place_back_bet \
+ dry_run_hounds_place_back_bet \
+ dry_run_hounds_place_lay_bet \
+ dry_run_horses_winner_lay_bet \
+ dry_run_hounds_winner_favorite_lay_bet \
+ hounds_place_lay_bet \
+ dry_run_horses_winner_favorite_lay_bet \
+ sendoff_no \
+ dry_run_horses_place_back_bet \
+ dry_run_horses_place_lay_bet \
+ dry_run_sendoff_no \
+ dry_run_tie_no_bet \
+ dry_run_score_sum_is_even \
+ dry_run_less_than_4.5_goals"
 
 
 date_list="13 14 15 16 17 18 19 20 21 22 23"
 
 for TYP in $bet_type_list ; do 
-  for d in $date_list ; do 
+#  for d in $date_list ; do 
       typ=$(echo $TYP | tr '[A-Z]' '[a-z]')
-      echo "$typ $d "
-      grep $TYP $INPUT_FILE | grep "2013, 1, $d" > target/$typ\_$d.dat
-  done
+      echo "grep -i $TYP $INPUT_FILE "
+      grep -i $TYP $INPUT_FILE > target/$typ.dat
+#  done
 done
