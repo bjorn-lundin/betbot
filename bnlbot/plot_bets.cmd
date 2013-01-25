2121139cbf976e55300fde1cfde1ef81405c92b0
bnl <b.f.lundin@gmail.com>
2013-01-25 23:52:35 +0000

    both teams scores included

--- bnlbot/plot_bets.cmd
+++ bnlbot/plot_bets.cmd
@@ -1,5 +1,5 @@
 
-set terminal png medium size 640,480 background '#ffffffff'
+set terminal png medium size 800,600 background '#ffffffff'
 set output "target/".typ.".png"
 
 set style data linespoints
@@ -26,6 +26,7 @@ set xyplane at 0
 
 #splot 'target/dry_run_hounds_winner_lay_bet.dat' using 3:4:5 notitle
 
+set hidden3d
 
 if (is_lay == 0 ) splot "target/".typ.".dat" using 3:5:6 notitle
 if (is_lay == 1 ) splot "target/".typ.".dat" using 3:4:6 notitle
--- bnlbot/split.bash
+++ bnlbot/split.bash
@@ -25,15 +25,12 @@ bet_type_list="dry_run_less_than_3.5_goals \
  dry_run_sendoff_no \
  dry_run_tie_no_bet \
  dry_run_score_sum_is_even \
+ dry_run_both_teams_scores \
  dry_run_less_than_4.5_goals"
 
 
-date_list="13 14 15 16 17 18 19 20 21 22 23"
-
 for TYP in $bet_type_list ; do 
-#  for d in $date_list ; do 
       typ=$(echo $TYP | tr '[A-Z]' '[a-z]')
       echo "grep -i $TYP $INPUT_FILE "
       grep -i $TYP $INPUT_FILE > target/$typ.dat
-#  done
 done
