bef98a1fc125111e27c713f3e1839b65350d6a0f
bnl <b.f.lundin@gmail.com>
2013-02-02 16:01:48 +0000

    rewriting simulator

--- bnlbot/do_reports.bash
+++ bnlbot/do_reports.bash
@@ -1,11 +1,54 @@
 #!/bin/bash
 
-#make some simulations
+#make some simulations, first day, by day
 
-./do_simulate.bash lay hound Plats 
-./do_simulate.bash back hound Plats
-./do_simulate.bash lay horse Plats 
-./do_simulate.bash back horse Plats
+#./do_simulate.bash -b lay  -a hound -n Plats 
+#./do_simulate.bash -b back -a hound -n Plats
+#./do_simulate.bash -b lay  -a horse -n Plats 
+#./do_simulate.bash -b back -a horse -n Plats
+#./do_simulate.bash -b lay  -a hound -n Vinnare 
+#./do_simulate.bash -b back -a hound -n Vinnare
+#./do_simulate.bash -b lay  -a horse -n Vinnare
+#./do_simulate.bash -b back -a horse -n Vinnare
 
+## then over two days
+#./do_simulate.bash -b lay  -a hound -n Plats   -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b back -a hound -n Plats   -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b lay  -a horse -n Plats   -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b back -a horse -n Plats   -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b lay  -a hound -n Vinnare -u -s "2013-01-30" -t "2013-01-31" 
+#./do_simulate.bash -b back -a hound -n Vinnare -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b lay  -a horse -n Vinnare -u -s "2013-01-30" -t "2013-01-31"
+#./do_simulate.bash -b back -a horse -n Vinnare -u -s "2013-01-30" -t "2013-01-31"
+
+
+
+(python simulator.py --bet_type=lay --bet_name=Plats \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=horse --min_price=1.0 --max_price=30 --summary  && \
+python simulator.py --bet_type=back --bet_name=Plats \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=horse --min_price=1.0 --max_price=30 --summary  ) &
+  
+(python simulator.py --bet_type=lay --bet_name=Plats \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=hound --min_price=1.0 --max_price=30 --summary  && \
+python simulator.py --bet_type=back --bet_name=Plats \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=hound --min_price=1.0 --max_price=30 --summary  ) &
+
+(python simulator.py --bet_type=lay --bet_name=Vinnare \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=horse --min_price=1.0 --max_price=30 --summary  && \
+python simulator.py --bet_type=back --bet_name=Vinnare \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=horse --min_price=1.0 --max_price=30 --summary  ) &
+
+(python simulator.py --bet_type=lay --bet_name=Vinnare \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=hound --min_price=1.0 --max_price=30 --summary  && \
+python simulator.py --bet_type=back --bet_name=Vinnare \
+  --start_date=2013-01-30 --stop_date=2013-02-01 --saldo=10000 \
+  --size=30 --animal=hound --min_price=1.0 --max_price=30 --summary ) &
 
 
--- bnlbot/do_simulate.bash
+++ bnlbot/do_simulate.bash
@@ -18,17 +18,33 @@ back_price_list="1.0 1.20 1.40 1.60 1.80 \
 #horse_date_list="2013-01-25 2013-01-26 2013-01-27 2013-01-28" 
 #human_date_list="2013-01-25 2013-01-26 2013-01-27 2013-01-28" 
 
-hound_date_list="2013-01-30" 
-horse_date_list="2013-01-30" 
-human_date_list="2013-01-30" 
+hound_date_list="2013-01-30 2013-01-31" 
+horse_date_list="2013-01-30 2013-01-31" 
+human_date_list="2013-01-30 2013-01-31" 
 
+use_argument_dates=0
+
+while getopts “a:b:n:s:t:u” OPTION
+do
+     case $OPTION in
+         s) start_date=$OPTARG  ;;
+         t) stop_date=$OPTARG  ;;
+         u) use_argument_dates=1  ;;
+         a) animal=$OPTARG  ;;
+         b) bet_type=$OPTARG  ;;
+         n) bet_name=$OPTARG  ;;
+     esac
+done
+
+echo "bet_name - $bet_name"
+echo "bet_type - $bet_type"
+echo "start_date - $start_date"
+echo "stop_date - $stop_date"
+echo "animal - $animal"
+echo "use_argument_dates - $use_argument_dates"
                    
+
 #delay_list = "0 1 2 3 4 5 6"
-
-
-bet_type=$1
-animal=$2
-bet_name=$3
 
 #  bet_type=$(echo $BET_TYPE | tr '[A-Z]' '[a-z]')
 
@@ -86,11 +102,23 @@ case "$animal" in
 esac
 
 
+echo "3 $date_list"
+#exit
+
 [ ! -d sims ] && mkdir sims
 
-for the_date in $date_list; do
+for the_date in $date_list ; do
+
+#    start_date=$the_date
+#    stop_date=$the_date
+
+    if [ $use_argument_dates -eq 0 ] ; then
+      start_date=$the_date
+      stop_date=$the_date
+    fi
+
     datadir=sims
-    filname=simulation-$animal\-$bet_name\-$bet_type\-$the_date\-$the_date\.dat
+    filname=simulation-$animal\-$bet_name\-$bet_type\-$start_date\-$stop_date\.dat
     fil=$datadir/$filname
     rm -rf $fil
     rm -rf $fil.dat
@@ -101,7 +129,8 @@ for the_date in $date_list; do
             python simulator.py \
                 --bet_type=$bet_type \
                 --bet_name=$bet_name \
-                --date=$the_date \
+                --start_date=$start_date \
+                --stop_date=$stop_date \
                 --saldo=10000 \
                 --size=30 \
                 --animal=$animal \
@@ -113,10 +142,15 @@ for the_date in $date_list; do
     gnuplot -e "animal='$animal'" \
             -e "bet_name='$bet_name'" \
             -e "bet_type='$bet_type'" \
-            -e "start_date='$the_date'" \
-            -e "stop_date='$the_date'" \
+            -e "start_date='$start_date'" \
+            -e "stop_date='$stop_date'" \
             -e "datafil='$filname'" \
             -e "datadir='$datadir'" plot_simulation.gpl
     rm -f /Users/bnl/Dropbox/betfair/png/sim/$fil.png
     cp $fil.png /Users/bnl/Dropbox/betfair/png/sim/
+
+    if [ $use_argument_dates ] ; then
+      break 
+    fi
+    
 done
