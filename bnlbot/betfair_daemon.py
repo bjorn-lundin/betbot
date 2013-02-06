e229bdb1d2fb809552929a803ef904fa915ab94b
bnl <b.f.lundin@gmail.com>
2013-02-06 17:15:09 +0000

    the daemon....

---
+++ bnlbot/betfair_daemon.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import os
+import sys
+#from asyncproc import Process
+import subprocess
+import ConfigParser
+from time import sleep
+
+
+class BetfairDaemon(object) :
+        
+    section_list = None
+    config = None 
+    process_list = []
+    
+    def read_config(self) :
+        self.config = ConfigParser.ConfigParser()
+        self.config.read('betfair_daemon.ini')
+        self.section_list = self.config.sections()
+
+
+
+###############################3
+#make print flush now!
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+
+
+daemon = BetfairDaemon()
+daemon.read_config()
+
+for section in daemon.section_list :
+    run = daemon.config.getboolean(section,'run')
+    name = daemon.config.get(section,'name')
+    the_type = daemon.config.get(section,'type')
+    print 'section', section   
+    print 'name', name
+    print 'run', run
+    print 'type', the_type
+    print "----------------"
+    print
+    if run:
+        args = []
+        args.append('/usr/bin/python')
+#        args.append('/home/bnl/bnlbot/' + name)
+        args.append(name)
+        print args
+        my_process =  subprocess.Popen(args)  
+        daemon.process_list.append(my_process)
+
+
+for proc in daemon.process_list :
+    print 'proc', proc
+
+
+print "sleep 10"
+sleep(10)
+
+
+for proc in daemon.process_list :
+    print "check if alive"
+    print proc.poll()
+
+    print "kill if alive"
+    print proc.terminate()
+
+print "done"
+
+
+
+#        self.DELAY_BETWEEN_TURNS_BAD_FUNDING = float(config.get('Global', 'delay_between_turns_bad_funding'))
+#        self.DELAY_BETWEEN_TURNS_NO_MARKETS  = float(config.get('Global', 'delay_between_turns_no_markets'))
+#        self.DELAY_BETWEEN_TURNS             = float(config.get('Global', 'delay_between_turns'))
+#        self.NETWORK_FAILURE_DELAY           = float(config.get('Global', 'network_failure_delay'))
+
+
+
+#proc_list = []
+#
+#myProc = Process("myprogram.app")
+#
+#while True:
+#    # check to see if process has ended
+#    poll = myProc.wait(os.WNOHANG)
+#    if poll != None:
+#        break
+#    # print any new output
+#    out = myProc.read()
+#    if out != "":
+#        print out
+#
