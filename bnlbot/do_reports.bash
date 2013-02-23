16cdc38a9c2685726cb4bbe585b2e7fe8c99e9ba
bnl <b.f.lundin@gmail.com>
2013-02-23 09:23:23 +0000

    moving db

--- bnlbot/do_reports.bash
+++ bnlbot/do_reports.bash
@@ -105,23 +105,18 @@ echo "start_date = $start_date"
 
 #date_list=$(the_date_list $start_date $yesterday)
 date_list=$yesterday
-date_list="2013-01-30 2013-01-31 2013-02-01 2013-02-02 2013-02-03 2013-02-04 2013-02-05 \
-2013-02-06 2013-02-07 2013-02-08 2013-02-09 2013-02-10 2013-02-11 2013-02-12 \
-2013-02-13 2013-02-14 2013-02-15 2013-02-16 2013-02-17 2013-02-18"
-
-date_list="2013-02-15 2013-02-16 2013-02-17"
-
-#echo "date_list = $date_list"
-
-date_list=$yesterday
-
+#date_list="2013-01-30 2013-01-31 2013-02-01 2013-02-02 2013-02-03 2013-02-04 2013-02-05 \
+#2013-02-06 2013-02-07 2013-02-08 2013-02-09 2013-02-10 2013-02-11 2013-02-12 \
+#2013-02-13 2013-02-14 2013-02-15 2013-02-16 2013-02-17 2013-02-18"
 
 animal_names="Vinnare Plats"
 animals="horse hound"
+variants="normal max_5 max_7"
+#variants="max_5"
 
 
-animal_names="Vinnare"
-animals="horse hound"
+#date_list="2013-02-15 2013-02-16 2013-02-17 2013-02-18"
+
 
 
 #names_football="Utvisning udda 0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 lagen straff"
@@ -139,7 +134,6 @@ animals="horse hound"
 #graph_type=weekly
 #graph_type=biweekly
 
-variants="normal max_5 max_7"
 
 
 
