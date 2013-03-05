8fea9bf6cf65e87fed5ce96bd9b65f438950472e
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-03-05 11:49:42 +0000

    added back bet

--- bnlbot/adabot/local/bot/simulator.adb
+++ bnlbot/adabot/local/bot/simulator.adb
@@ -192,6 +192,15 @@ begin
       when Races.Quad_Weekly => Global_Start_Date := Global_Stop_Date - (27, 0, 0, 0, 0);
    end case;
 
+
+   if Global_Start_Date < Sattmate_Calendar.Time_Type'(2013, 01, 30, 0, 0, 0, 0) then
+      Log ("start date outside range, " & Global_Graph_Type'Img & " " &
+             Sattmate_Calendar.String_Date (Global_Start_Date) & " " &
+             Sattmate_Calendar.String_Date (Global_Stop_Date )
+          );
+      return;
+   end if;
+
    Global_Start_Date.Hour        := 0;
    Global_Start_Date.Minute      := 0;
    Global_Start_Date.Second      := 0;
@@ -207,123 +216,54 @@ begin
    Data_Dir := To_Unbounded_String ("sims");
 
    for Bet_Type in Races.Bet_Type_Type'Range loop
-         for Variant in Races.Variant_Type'Range loop
+      for Variant in Races.Variant_Type'Range loop
 
-            -- new type of simulation, reset dates
-            Global_Last_Loss := Sattmate_Calendar.Time_Type_First;
-            Global_Race_Date := Sattmate_Calendar.Time_Type_First;
+         -- new type of simulation, reset dates
+         Global_Last_Loss := Sattmate_Calendar.Time_Type_First;
+         Global_Race_Date := Sattmate_Calendar.Time_Type_First;
 
-            -- what filename to write this to ?
-            Filename := To_Unbounded_String
-              ("simulation_ada1-" &
-                 Sa_Animal.all &  "-" &
-                 Lower_Case (Sa_Graph_Type.all) & "-" &
-                 Sa_Bet_Name.all & "-" &
-                 Lower_Case (Bet_Type'Img) & "-" &
-                 Lower_Case (Variant'Img) & "-" &
-                 Sattmate_Calendar.String_Date_Iso (Global_Start_Date) & "-" &
-                 Sa_Stop_Date.all &
-               --   Sa_Index.all  & "-" &
-                 ".dat");
+         -- what filename to write this to ?
+         Filename := To_Unbounded_String
+           ("simulation_ada1-" &
+              Sa_Animal.all &  "-" &
+              Lower_Case (Sa_Graph_Type.all) & "-" &
+              Sa_Bet_Name.all & "-" &
+              Lower_Case (Bet_Type'Img) & "-" &
+              Lower_Case (Variant'Img) & "-" &
+              Sattmate_Calendar.String_Date_Iso (Global_Start_Date) & "-" &
+              Sa_Stop_Date.all &
+            --   Sa_Index.all  & "-" &
+              ".dat");
 
-            Fil     := Data_Dir &
-              To_Unbounded_String (Global_Directory_Separator) &
-              Filename;
-            Fil_Gpi := Data_Dir &
-              To_Unbounded_String (Global_Directory_Separator) &
-              Filename &
-              To_Unbounded_String (".gpi");
-            Log ("Filename: '" & To_String (Filename) & "'");
-            --   begin
-            -- create file if not exists
-            Text_Io.Create
-              (Mode => Text_Io.Out_File,
-               Name => To_String (Fil),
-               File => Target_Dat);
+         Fil     := Data_Dir &
+           To_Unbounded_String (Global_Directory_Separator) &
+           Filename;
+         Fil_Gpi := Data_Dir &
+           To_Unbounded_String (Global_Directory_Separator) &
+           Filename &
+           To_Unbounded_String (".gpi");
+         Log ("Filename: '" & To_String (Filename) & "'");
+         --   begin
+         -- create file if not exists
+         Text_Io.Create
+           (Mode => Text_Io.Out_File,
+            Name => To_String (Fil),
+            File => Target_Dat);
 
-            Text_Io.Close (Target_Dat);
-            --   exception
-            --      when others => null;
-            --   end;
-
-
-            Race.Show_Runners;
-            case Bet_Type is
-               when Races.Lay =>
-                  for Max_Price in Max_Price_Index_Type'Range loop
-                     for Min_Price in Min_Price_Index_Type'Range loop
-                        if Integer (Min_Price) < Integer (Max_Price) then
-                           Global_Saldo  := Races.Saldo_Type'Value (Sa_Saldo.all);
-                           Global_Profit := 0.0;
-                           Log ("start simulation, saldo =  " & Integer (Global_Saldo)'Img);
-                           Races.Race_Package.Get_First (Race_List, Race, Eol);
-                           loop
-                              exit when Eol;
-                              Log ("---  main loop start " &  Race.Market.Market_Id'Img &
-                                     " saldo :" & Integer (Global_Saldo)'Img & " -----------------");
-                              -- reset the daily profit when new day is treated
-                              if Global_Race_Date.Day   /= Race.Market.Event_Date.Day or else
-                                Global_Race_Date.Month /= Race.Market.Event_Date.Month or else
-                                Global_Race_Date.Year  /=  Race.Market.Event_Date.Year then
-                                 Global_Race_Date := Race.Market.Event_Date;
-                                 Global_Profit    := 0.0;
-                                 Log ("main loop , race date = " & Sattmate_Calendar.String_Date (Global_Race_Date));
-                              end if;
+         Text_Io.Close (Target_Dat);
+         --   exception
+         --      when others => null;
+         --   end;
 
 
-
-                              Race.Make_Lay_Bet
-                                (Bet_Laid          => Global_Bet_Laid,
-                                 Profit            => Global_Profit,
-                                 Saldo             => Global_Saldo,
-                                 Last_Loss         => Global_Last_Loss,
-                                 Max_Daily_Loss    => Global_Max_Daily_Loss,
-                                 Max_Profit_Factor => Global_Max_Profit_Factor,
-                                 Size              => Global_Size,
-                                 Min_Price         => Races.Min_Price_Type (Min_Price),
-                                 Max_Price         => Races.Max_Price_Type (Max_Price));
-
-                              if Global_Bet_Laid then
-                                 Log ("---  main loop saldo after bet laid :" & Integer (Global_Saldo)'Img & " -----------------");
-                                 Race.Check_Result
-                                   (Profit    => Global_Profit,
-                                    Saldo     => Global_Saldo,
-                                    Last_Loss => Global_Last_Loss,
-                                    Bet_Type  => Bet_Type);
-                              end if;
-                              Races.Race_Package.Get_Next (Race_List, Race, Eol);
-                           end loop;
-                           Log ("main - Global_Profit : " & Integer (Global_Profit)'Img);
-
-                           Log ("stop simulation, saldo =  " & Integer (Global_Saldo)'Img);
-                           Print (Integer (Min_Price)'Img & " " & Integer (Max_Price)'Img & " " & Integer (Global_Saldo)'Img);
-                           -- Append To file
-                           --         begin
-                           -- create file if not exists
-                           Text_Io.Open
-                             (Mode => Text_Io.Append_File,
-                              Name => To_String (Fil),
-                              File => Target_Dat);
-                           Text_Io.Put_Line
-                             (Target_Dat, Integer (Min_Price)'Img & " " & Integer (Max_Price)'Img & " " & Integer (Global_Saldo)'Img);
-                           Text_Io.Close (Target_Dat);
-                           --         exception
-                           --            when others => null;
-                           --         end;
-                           Log ("---  main loop stop " & Race.Market.Market_Id'Img &
-                                  " profit :" & Integer (Global_Profit)'Img & " -----------------");
-
-                        end if; -- min_price < Max_Price
-                     end loop;
-                  end loop;
-
-               when Races.Back =>
-                  for Back_Price in Back_Price_Index_Type'Range loop
-                     for Delta_Price in Delta_Price_Index_Type'Range loop
-
+         Race.Show_Runners;
+         case Bet_Type is
+            when Races.Lay =>
+               for Max_Price in Max_Price_Index_Type'Range loop
+                  for Min_Price in Min_Price_Index_Type'Range loop
+                     if Integer (Min_Price) < Integer (Max_Price) then
                         Global_Saldo  := Races.Saldo_Type'Value (Sa_Saldo.all);
                         Global_Profit := 0.0;
-
                         Log ("start simulation, saldo =  " & Integer (Global_Saldo)'Img);
                         Races.Race_Package.Get_First (Race_List, Race, Eol);
                         loop
@@ -339,7 +279,9 @@ begin
                               Log ("main loop , race date = " & Sattmate_Calendar.String_Date (Global_Race_Date));
                            end if;
 
-                           Race.Make_Back_Bet
+
+
+                           Race.Make_Lay_Bet
                              (Bet_Laid          => Global_Bet_Laid,
                               Profit            => Global_Profit,
                               Saldo             => Global_Saldo,
@@ -347,8 +289,8 @@ begin
                               Max_Daily_Loss    => Global_Max_Daily_Loss,
                               Max_Profit_Factor => Global_Max_Profit_Factor,
                               Size              => Global_Size,
-                              Back_Price        => Races.Back_Price_Type (Back_Price),
-                              Delta_Price       => Races.Delta_Price_Type (Races.Delta_Price_Type (Delta_Price) / 10.0));
+                              Min_Price         => Races.Min_Price_Type (Min_Price),
+                              Max_Price         => Races.Max_Price_Type (Max_Price));
 
                            if Global_Bet_Laid then
                               Log ("---  main loop saldo after bet laid :" & Integer (Global_Saldo)'Img & " -----------------");
@@ -361,8 +303,9 @@ begin
                            Races.Race_Package.Get_Next (Race_List, Race, Eol);
                         end loop;
                         Log ("main - Global_Profit : " & Integer (Global_Profit)'Img);
+
                         Log ("stop simulation, saldo =  " & Integer (Global_Saldo)'Img);
-                        Print (Integer (Back_Price)'Img & " " & Integer (Delta_Price)'Img & " " & Integer (Global_Saldo)'Img);
+                        Print (Integer (Min_Price)'Img & " " & Integer (Max_Price)'Img & " " & Integer (Global_Saldo)'Img);
                         -- Append To file
                         --         begin
                         -- create file if not exists
@@ -371,8 +314,7 @@ begin
                            Name => To_String (Fil),
                            File => Target_Dat);
                         Text_Io.Put_Line
-                          (Target_Dat,
-                           Integer (Back_Price)'Img & " " & Integer (Delta_Price)'Img & " " & Integer (Global_Saldo)'Img);
+                          (Target_Dat, Integer (Min_Price)'Img & " " & Integer (Max_Price)'Img & " " & Integer (Global_Saldo)'Img);
                         Text_Io.Close (Target_Dat);
                         --         exception
                         --            when others => null;
@@ -380,32 +322,99 @@ begin
                         Log ("---  main loop stop " & Race.Market.Market_Id'Img &
                                " profit :" & Integer (Global_Profit)'Img & " -----------------");
 
-                     end loop;
+                     end if; -- min_price < Max_Price
                   end loop;
-            end case;
+               end loop;
 
-            Contents_Gpi :=
-              To_Unbounded_String
-                ("graph_type='" & Sa_Graph_Type.all & "'" & Ada.Characters.Latin_1.Lf &
-                   "animal='" & Sa_Animal.all & "'" & Ada.Characters.Latin_1.Lf &
-                   "bet_name='" & Lower_Case (Bet_Type'Img) & "'" & Ada.Characters.Latin_1.Lf &
-                   "bet_type='" & Sa_Bet_Name.all & "'" & Ada.Characters.Latin_1.Lf &
-                   "variant='" & Lower_Case (Variant'Img) & "'" & Ada.Characters.Latin_1.Lf &
-                   "index='" & "not_supported" & "'" & Ada.Characters.Latin_1.Lf &
-                   "start_date='" & Sattmate_Calendar.String_Date_Iso (Global_Start_Date) & "'" & Ada.Characters.Latin_1.Lf &
-                   "stop_date='" & Sa_Stop_Date.all & "'" & Ada.Characters.Latin_1.Lf &
-                   "datafil='" & To_String (Filename) & "'" & Ada.Characters.Latin_1.Lf &
-                   "datadir='" & To_String (Data_Dir) & "'");
+            when Races.Back =>
+               for Back_Price in Back_Price_Index_Type'Range loop
+                  for Delta_Price in Delta_Price_Index_Type'Range loop
+
+                     Global_Saldo  := Races.Saldo_Type'Value (Sa_Saldo.all);
+                     Global_Profit := 0.0;
+
+                     Log ("start simulation, saldo =  " & Integer (Global_Saldo)'Img);
+                     Races.Race_Package.Get_First (Race_List, Race, Eol);
+                     loop
+                        exit when Eol;
+                        Log ("---  main loop start " &  Race.Market.Market_Id'Img &
+                               " saldo :" & Integer (Global_Saldo)'Img & " -----------------");
+                        -- reset the daily profit when new day is treated
+                        if Global_Race_Date.Day   /= Race.Market.Event_Date.Day or else
+                          Global_Race_Date.Month /= Race.Market.Event_Date.Month or else
+                          Global_Race_Date.Year  /=  Race.Market.Event_Date.Year then
+                           Global_Race_Date := Race.Market.Event_Date;
+                           Global_Profit    := 0.0;
+                           Log ("main loop , race date = " & Sattmate_Calendar.String_Date (Global_Race_Date));
+                        end if;
+
+                        Race.Make_Back_Bet
+                          (Bet_Laid          => Global_Bet_Laid,
+                           Profit            => Global_Profit,
+                           Saldo             => Global_Saldo,
+                           Last_Loss         => Global_Last_Loss,
+                           Max_Daily_Loss    => Global_Max_Daily_Loss,
+                           Max_Profit_Factor => Global_Max_Profit_Factor,
+                           Size              => Global_Size,
+                           Back_Price        => Races.Back_Price_Type (Back_Price),
+                           Delta_Price       => Races.Delta_Price_Type (Races.Delta_Price_Type (Delta_Price) / 10.0));
+
+                        if Global_Bet_Laid then
+                           Log ("---  main loop saldo after bet laid :" & Integer (Global_Saldo)'Img & " -----------------");
+                           Race.Check_Result
+                             (Profit    => Global_Profit,
+                              Saldo     => Global_Saldo,
+                              Last_Loss => Global_Last_Loss,
+                              Bet_Type  => Bet_Type);
+                        end if;
+                        Races.Race_Package.Get_Next (Race_List, Race, Eol);
+                     end loop;
+                     Log ("main - Global_Profit : " & Integer (Global_Profit)'Img);
+                     Log ("stop simulation, saldo =  " & Integer (Global_Saldo)'Img);
+                     Print (Integer (Back_Price)'Img & " " & Integer (Delta_Price)'Img & " " & Integer (Global_Saldo)'Img);
+                     -- Append To file
+                     --         begin
+                     -- create file if not exists
+                     Text_Io.Open
+                       (Mode => Text_Io.Append_File,
+                        Name => To_String (Fil),
+                        File => Target_Dat);
+                     Text_Io.Put_Line
+                       (Target_Dat,
+                        Integer (Back_Price)'Img & " " & Integer (Delta_Price)'Img & " " & Integer (Global_Saldo)'Img);
+                     Text_Io.Close (Target_Dat);
+                     --         exception
+                     --            when others => null;
+                     --         end;
+                     Log ("---  main loop stop " & Race.Market.Market_Id'Img &
+                            " profit :" & Integer (Global_Profit)'Img & " -----------------");
+
+                  end loop;
+               end loop;
+         end case;
+
+         Contents_Gpi :=
+           To_Unbounded_String
+             ("graph_type='" & Sa_Graph_Type.all & "'" & Ada.Characters.Latin_1.Lf &
+                "animal='" & Sa_Animal.all & "'" & Ada.Characters.Latin_1.Lf &
+                "bet_name='" & Lower_Case (Bet_Type'Img) & "'" & Ada.Characters.Latin_1.Lf &
+                "bet_type='" & Sa_Bet_Name.all & "'" & Ada.Characters.Latin_1.Lf &
+                "variant='" & Lower_Case (Variant'Img) & "'" & Ada.Characters.Latin_1.Lf &
+                "index='" & "not_supported" & "'" & Ada.Characters.Latin_1.Lf &
+                "start_date='" & Sattmate_Calendar.String_Date_Iso (Global_Start_Date) & "'" & Ada.Characters.Latin_1.Lf &
+                "stop_date='" & Sa_Stop_Date.all & "'" & Ada.Characters.Latin_1.Lf &
+                "datafil='" & To_String (Filename) & "'" & Ada.Characters.Latin_1.Lf &
+                "datadir='" & To_String (Data_Dir) & "'");
 
 
-            Text_Io.Create
-              (Mode => Text_Io.Out_File,
-               Name => To_String (Fil_Gpi),
-               File => Target_Gpi);
-            Text_Io.Put_Line (Target_Gpi, To_String (Contents_Gpi));
-            Text_Io.Close (Target_Gpi);
+         Text_Io.Create
+           (Mode => Text_Io.Out_File,
+            Name => To_String (Fil_Gpi),
+            File => Target_Gpi);
+         Text_Io.Put_Line (Target_Gpi, To_String (Contents_Gpi));
+         Text_Io.Close (Target_Gpi);
 
-         end loop;
+      end loop;
    end loop;
 
 exception
