42d9bf14f32d34d0398bebe347b3e9ec22a61641
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-03-11 21:19:01 +0000

    pg syntax

--- bnlbot/adabot/sql/drymarketsf.sql
+++ bnlbot/adabot/sql/drymarketsf.sql
@@ -1,41 +1,57 @@
 
+begin;
 create table DRYMARKETSF ( 
-  MARKETID integer  default 1 not null , -- Primary Key
-  BSPMARKET varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' not null , 
-  MARKETTYPE varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' , 
-  EVENTHIERARCHY varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' not null , -- non unique index 2
-  LASTREFRESH datetime2(3)   not null , 
-  TURNINGINPLAY varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' not null , 
-  MENUPATH varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' , 
-  BETDELAY integer  default 1 not null , 
-  EXCHANGEID integer  default 1 not null , 
-  COUNTRYCODE varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' not null , 
-  MARKETNAME varchar(0) COLLATE SQL_Latin1_General_CP1_CS_AS default ' ' , -- non unique index 3
-  MARKETSTATUS integer  default 1 , 
-  EVENTDATE datetime2(3)   , -- non unique index 4
-  NOOFRUNNERS integer  default 1 , 
-  TOTALMATCHED integer  default 1 , 
-  NOOFWINNERS integer  default 1 
-)
-go
+  MARKETID integer default 1 not null , -- Primary Key
+  BSPMARKET varchar default ' ' not null , 
+  MARKETTYPE varchar default ' ' , 
+  EVENTHIERARCHY varchar default ' ' not null , -- non unique index 2
+  LASTREFRESH timestamp without time zone  not null , 
+  TURNINGINPLAY varchar default ' ' not null , 
+  MENUPATH varchar default ' ' , 
+  BETDELAY integer default 1 not null , 
+  EXCHANGEID integer default 1 not null , 
+  COUNTRYCODE varchar default ' ' not null , 
+  MARKETNAME varchar default ' ' , -- non unique index 3
+  MARKETSTATUS integer default 1 , 
+  EVENTDATE timestamp without time zone  , -- non unique index 4
+  NOOFRUNNERS integer default 1 , 
+  TOTALMATCHED integer default 1 , 
+  NOOFWINNERS integer default 1 
+) without OIDS ;
 
 alter table DRYMARKETSF add constraint DRYMARKETSFP1 primary key (
   marketid
-)
-go
+) ;
 
 create index DRYMARKETSFI2 on DRYMARKETSF (
   eventdate
-)
-go
+) ;
 
 create index DRYMARKETSFI3 on DRYMARKETSF (
   eventhierarchy
-)
-go
+) ;
 
 create index DRYMARKETSFI4 on DRYMARKETSF (
   marketname
-)
-go
+) ;
+
+comment on table  DRYMARKETSF is 'collected markets ' ;
+comment on column DRYMARKETSF.marketid is 'market id' ;
+comment on column DRYMARKETSF.bspmarket is 'bsp_market' ;
+comment on column DRYMARKETSF.markettype is 'market_type' ;
+comment on column DRYMARKETSF.eventhierarchy is 'event_hierarchy' ;
+comment on column DRYMARKETSF.lastrefresh is 'last_refresh' ;
+comment on column DRYMARKETSF.turninginplay is 'turning_in_play' ;
+comment on column DRYMARKETSF.menupath is 'menu_path' ;
+comment on column DRYMARKETSF.betdelay is 'bet_delay' ;
+comment on column DRYMARKETSF.exchangeid is 'exchange_id' ;
+comment on column DRYMARKETSF.countrycode is 'country_code' ;
+comment on column DRYMARKETSF.marketname is 'market_name' ;
+comment on column DRYMARKETSF.marketstatus is 'market_status' ;
+comment on column DRYMARKETSF.eventdate is 'event_date' ;
+comment on column DRYMARKETSF.noofrunners is 'no_of_runners' ;
+comment on column DRYMARKETSF.totalmatched is 'total_matched' ;
+comment on column DRYMARKETSF.noofwinners is 'no_of_winners' ;
+
+commit;
 
