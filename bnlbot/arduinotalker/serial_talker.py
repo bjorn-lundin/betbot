9601441d7161fc6f1988f154dcbe63b94aaff907
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-03-18 21:24:08 +0000

    mothly report added

--- bnlbot/arduinotalker/serial_talker.py
+++ bnlbot/arduinotalker/serial_talker.py
@@ -12,6 +12,53 @@ import psycopg2
 import datetime
 import serial
 
+
+ 
+def get_row_days(conn, bet_type, delta_days)  :
+    result = 0
+    today = datetime.datetime.now() 
+    day = today + datetime.timedelta(days = delta_days) 
+    day_start = datetime.datetime(today.year, today.month, today.day,  0,  0,  0)
+    day_stop  = datetime.datetime(day.year, day.month, day.day, 23, 59, 59)
+
+#    print day_start, day_stop
+    
+    cur = conn.cursor()
+    cur.execute("select " \
+                     "sum(PROFIT), " \
+                     "BET_PLACED::date " \
+                 "from " \
+                     "BET_WITH_COMMISSION " \
+                 "where " \
+                     "BET_TYPE = %s " \
+                 "and " \
+                     "CODE = %s " \
+                 "and " \
+                     "BET_PLACED >= %s " \
+                 "and " \
+                     "BET_PLACED <= %s " \
+                 "group by " \
+                     "BET_PLACED::date " \
+                 "order by " \
+                     "BET_PLACED::date desc ", 
+                   (bet_type,'S',day_start,day_stop))
+  
+ #   print bet_type, 'rc', cur.rowcount, 'start', day_start, 'stop', day_stop
+     
+    if cur.rowcount >= 1 :
+        row = cur.fetchone()
+        if row :
+          result = row[0]  
+        else :
+          result = 0
+    cur.close()
+    conn.commit()
+   
+    return result 
+    ################################## end get_row_days
+
+
+
   
 def get_row(conn, bet_type, delta_days)  :
     result = 0
@@ -78,9 +125,9 @@ def main():
           "DRY_RUN_HOUNDS_WINNER_LAY_BET_15_18",
           "DRY_RUN_HOUNDS_WINNER_LAY_BET_16_19",
           "DRY_RUN_HOUNDS_WINNER_LAY_BET_17_18",
-          "DRY_RUN_HOUNDS_WINNER_LAY_BET_LAST",
+#          "DRY_RUN_HOUNDS_WINNER_LAY_BET_LAST",
           "DRY_RUN_HOUNDS_PLACE_LAY_BET",
-          "DRY_RUN_HOUNDS_PLACE_LAY_BET_3_9",
+#          "DRY_RUN_HOUNDS_PLACE_LAY_BET_3_9",
           "DRY_RUN_HOUNDS_WINNER_BACK_BET"   ]
              
   ser.write('-----------------------------------------------------------------------------\r\n')
@@ -101,7 +148,6 @@ def main():
 
   ser.write('-----------------------------------------------------------------------------\r\n')
 
-
   for bet in bets :                               
     row1 = {}
     row1['0'] = get_row(conn, bet, 0)
@@ -116,9 +162,23 @@ def main():
 #    print lcd_row_1
     ser.write(lcd_row_1 + '\r\n')
     
+  row0['typ'] = 'Typ av bet/result veckor tillbaka'    
+  for bet in bets :                               
+    row2 = {}
+    row2['0'] = get_row_days(conn, bet, -6)
+    row2['1'] = get_row_days(conn, bet, -13)
+    row2['2'] = get_row_days(conn, bet, -20)
+    row2['3'] = get_row_days(conn, bet, -27)
+    row2['4'] = get_row_days(conn, bet, -34)
+    row2['5'] = get_row_days(conn, bet, -41)
+    row2['6'] = get_row_days(conn, bet, -48)
+    row2['typ'] = bet
+    lcd_row_2 = '%(typ)35s%(0)6d%(1)6d%(2)6d%(3)6d%(4)6d%(5)6d%(6)6d' % row2
+#    print lcd_row_1
+    ser.write(lcd_row_2 + '\r\n')
     
   ser.write('-----------------------------------------------------------------------------\r\n')
-  ser.write('\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n')
+  ser.write('\r\n\r\n\r\n\r\n\r\n\r\n')
  
 
   ser.close()
