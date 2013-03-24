0cf1b8449c5a6dbcc6c9c6187cbed64650321a86
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-03-24 19:33:53 +0000

    Adding download delay and S3 cloud storage (in separate source file s3_mgmt.py). Smaller fixes incl. renaming methods etc.

--- atg/ais/src/trunk/conf_template.py
+++ atg/ais/src/trunk/conf_template.py
@@ -6,6 +6,7 @@ from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
 import os
 
+AIS_TYPE = 'test'
 AIS_HOME = '/home/sejoabi/workspace/ais/trunk'
 AIS_WS_HOST = 'https://media.atg.se/'
 AIS_VERSION = '8'
@@ -14,8 +15,8 @@ AIS_METADIR = os.path.normpath(os.path.join(AIS_HOME, 'meta_data'))
 AIS_RACEDAY_HISTORY = 6 # Nbr of history days in ATG database
 AIS_RACEDAY_EXCLUDE = {17:'2011-10-21', 23:'2011-10-22'}
 EMAIL_LOG_ERRORS = False
-
-AIS_TYPE = 'test'
+AIS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+AIS_EOD_DOWNLOAD_DELAY = 1 # E.g. 0.1 equals 100 ms, 2 equals 2 seconds
 
 if AIS_TYPE == 'test':
     AIS_WS_URL = AIS_WS_HOST + 'infostub/PartnerInfoEmulator/version' + \
@@ -25,6 +26,9 @@ if AIS_TYPE == 'test':
     AIS_DATADIR = os.path.normpath(os.path.join(AIS_HOME, 'test_data'))
     AIS_DB_URL = 'postgresql://<user>:<pass>@localhost:5432/ais_test_db'
     AIS_LOGFILE = 'ais_test.log'
+    AIS_S3_BUCKET = 'ais-end-of-day-data-test'
+    AIS_S3_USER = ''
+    AIS_S3_PASSWORD = ''
     EMAIL_LOG_HOST = 'smtprelay1.telia.com'
     EMAIL_LOG_PORT = ''
     EMAIL_LOG_FROM = 'ais@nonobet.com'
@@ -42,6 +46,9 @@ elif AIS_TYPE == 'prod':
     AIS_DATADIR = os.path.normpath(os.path.join(AIS_HOME, 'prod_data'))
     AIS_DB_URL = 'postgresql://<user>:<pass>@localhost:5432/ais_prod_db'
     AIS_LOGFILE = 'ais.log'
+    AIS_S3_BUCKET = 'ais-end-of-day-data-prod'
+    AIS_S3_USER = ''
+    AIS_S3_PASSWORD = ''
     EMAIL_LOG_HOST = ''
     EMAIL_LOG_PORT = ''
     EMAIL_LOG_FROM = ''
