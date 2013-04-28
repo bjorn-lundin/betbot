6e34491513fb71bffa56eae3d12d013742bcac44
bnl <b.f.lundin@gmail.com>
2013-04-28 13:39:47 +0000

    min 5 left before + fav must have better odds than 5.0

--- bnlbot/botstart/bot-0.5/source/python/horses_winner_lay_bets.py
+++ bnlbot/botstart/bot-0.5/source/python/horses_winner_lay_bets.py
@@ -71,7 +71,13 @@ class HorsesWinnerLayBetBot(BetBot):
                 market = Market(self.conn, self.log, market_id = market_id)
                 # there must be at least 3 runners with lower odds
                 number_of_runners = len(sorted_list)
-                max_turns = number_of_runners - 2 - market.no_of_winners
+                max_turns = number_of_runners - 4 - market.no_of_winners
+
+
+                favorite_odds = 100.0
+                #loop through list and keep last entry -> favorite
+                for dct in sorted_list :
+                    favorite_odds = float(dct[1])
 
                 for dct in sorted_list :
                     i = i + 1
@@ -100,6 +106,10 @@ class HorsesWinnerLayBetBot(BetBot):
                         back_odds = dct[0]
                         index     = dct[3]
                         break
+
+                if favorite_odds >= 5.0 :
+                    self.log.info( 'Favorite sucks, odds= '+ str(favorite_odds) + ' must be >= 5.0 - exit check_strategy')
+                    return
 
                 if not selection :
                     self.log.info( 'No good runner found, exit check_strategy')
