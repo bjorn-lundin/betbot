4c740360e2ec3c81ac0e689800957463a2fa8c4d
nonobetmats <nonobetmats@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-05-16 22:46:25 +0000

    First version Raceday Selector

---
+++ client/NoNoBetClient/trunk/NoNoBetDb/CountryCode.vb
@@ -0,0 +1,34 @@
+﻿Public Class CountryCode
+
+  Private _Code As String
+  Private _TextSwe As String
+  Private _TextEng As String
+
+  Public ReadOnly Property Code As String
+    Get
+      Return _Code
+    End Get
+  End Property
+
+  Public ReadOnly Property TextSwe As String
+    Get
+      Return _TextSwe
+    End Get
+  End Property
+
+  Public ReadOnly Property TextEng As String
+    Get
+      Return _TextEng
+    End Get
+  End Property
+
+  Public Sub New(code As String, textSwe As String, textEng As String)
+    _Code = code
+    _TextSwe = textSwe
+    _TextEng = textEng
+  End Sub
+
+  Public Overrides Function ToString() As String
+    Return _Code + " - " + _TextSwe
+  End Function
+End Class
