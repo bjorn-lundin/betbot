fc1bf5748e4108ea565a3538aadae9931dbf370c
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2013-05-27 19:20:04 +0000

    new login object

---
+++ bnlbot/botstart/bot-0-9/source/ada/local/bot/token.ads
@@ -0,0 +1,15 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+ 
+package Token is
+  Not_Valid_Token : exception;
+  
+  type Token_Type is private;
+  procedure Login(A_Token : in out Token_Type) ;
+  function  Id (A_Token : Token_Type) return String;
+private
+  type Token_Type is record
+    Token_Is_Set : Boolean          := False;
+    The_Token    : Unbounded_String := Null_Unbounded_String;
+  end record;
+end Token;
