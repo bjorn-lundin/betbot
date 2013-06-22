a4352bdedb0ce5afbc48cd65a7ed8889dc267b54
bnl <b.f.lundin@gmail.com>
2013-06-22 10:14:22 +0000

    feting winners/non-runners works

--- bnlbot/botstart/bot-0-9/source/ada/local/bot/token.ads
+++ bnlbot/botstart/bot-0-9/source/ada/local/bot/token.ads
@@ -2,14 +2,23 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 
 package Token is
+
+  App_Key          : constant String   := "q0XW4VGRNoHuaszo";
+  App_Key_Delayed  : constant String   := "fhGwsk2Qay4zEw2Q";
+  Application_ID   : constant Positive := 1662;
+  Application_Name : constant String := "bnl-bot";
+  
+  URL : constant String := "https://api-ng.betstores.com/betting/betfair/services/beta-api.betfair.com/betting/json-rpc";
+  
+  
   Not_Valid_Token,
   Login_Failed  : exception;
 
-  type Token_Type is private;
+  type Token_Type is tagged private;
   procedure Login(A_Token : in out Token_Type) ;
   function  Id (A_Token : Token_Type) return String;
 private
-  type Token_Type is record
+  type Token_Type is tagged record
     Token_Is_Set : Boolean          := False;
     The_Token    : Unbounded_String := Null_Unbounded_String;
   end record;
