e34713d5b8d6ddc94b1229b1b5c9eb2495aed987
bnl <b.f.lundin@gmail.com>
2013-08-10 16:38:33 +0000

    sql reports for pgadmin

---
+++ bnlbot/botstart/bot-0-9/script/sql/ada_bet_by_week.sql
@@ -0,0 +1,22 @@
+﻿select
+  count('a'),
+  round(avg(profit)::numeric, 2) as avgprofit,
+  round(sum(profit)::numeric, 2) as sumprofit,
+  round(avg(price)::numeric, 2) as avgprice,
+  extract(week from betplaced) as week,
+  betname
+from
+  abets
+where
+  betplaced::date > (select CURRENT_DATE - interval '42 days')
+  and STATUS = 'EXECUTION_COMPLETE'
+  and betwon is not null
+  and betname like '%HO%'
+group by
+  extract(week from betplaced),
+  betname
+--having sum(profit) > 0
+order by
+  betname,
+  extract(week from betplaced) desc
+  ;
---
+++ bnlbot/botstart/bot-0-9/script/sql/sim_ada_profitable_bets_by_country.sql
@@ -0,0 +1,32 @@
+﻿select
+  count('a'),
+  round(avg(b.profit)::numeric, 2) as avgprofit,
+  round(sum(b.profit)::numeric, 2) as sumprofit,
+  round(avg(b.price)::numeric, 2) as avgprice,
+  min(b.startts)::date as mindate,
+  max(b.startts)::date as maxdate,
+  max(b.startts)::date - min(b.startts)::date  + 1 as days, 
+  round(count('a')/(max(b.startts)::date - min(b.startts)::date  + 1)::numeric,2) as betsperday, 
+  round((sum(profit)/(max(b.startts)::date - min(b.startts)::date  + 1))::numeric, 2) as profitperday,
+  e.countrycode,
+  b.betname --,
+--  round(sum(m.totalmatched)::numeric, 2) as totalmatched
+from
+  abets b, amarkets m, aevents e
+where
+  b.startts::date > (select current_date - interval '42 days')
+  and b.status = 'EXECUTION_COMPLETE'
+  and b.betwon is not null
+  and b.betname like '%HO%'
+  and b.marketid = m.marketid
+  and m.eventid = e.eventid
+group by
+  e.countrycode,
+  b.betname
+having sum(b.profit) > -0.0
+order by
+--  count desc ,
+  sum(b.profit) desc,
+  b.betname
+  ;
+
