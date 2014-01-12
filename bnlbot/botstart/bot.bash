c608d61d3c7ce313568615e1e02d2515747e0456
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2014-01-12 20:07:36 +0000

    new env vars

--- bnlbot/botstart/bot.bash
+++ bnlbot/botstart/bot.bash
@@ -28,5 +28,35 @@ export BOT_SCRIPT=$BOT_ROOT/script
 export BOT_SOURCE=$BOT_ROOT/source
 export BOT_DOC=$BOT_ROOT/docbook
 
+
+#amazon machines starts with 'ip'
+
+HOSTNAME=$(hostname)
+case $HOSTNAME in
+  ip*)
+    export BOT_MACHINE_ROLE=PROD
+    ;;   
+  sebjlun*)
+    export BOT_MACHINE_ROLE=SIM
+    ;;
+  tova)
+    export BOT_MACHINE_ROLE=TEST
+    ;;
+  *)  
+    export BOT_MACHINE_ROLE=$HOSTNAME
+    ;;
+esac
+
+
+case $HOSTNAME in
+  ip*) 
+    export BOT_MODE=real
+    ;;
+    *)
+    export BOT_MODE=simulation
+    ;;
+esac
+
+
 #dialog --msgbox "run with BOT_USER= $BOT_USER"  7 45
 
