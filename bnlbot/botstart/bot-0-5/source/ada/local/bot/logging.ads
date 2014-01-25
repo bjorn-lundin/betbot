ef4efb4041b109c4a84f1ac78dc388fcf56403cd
bnl <b.f.lundin@gmail.com>
2014-01-25 10:56:29 +0000

    removed old structures

--- bnlbot/botstart/bot-0-5/source/ada/local/bot/logging.ads
+++
@@ -1,9 +0,0 @@
-
-
-package Logging is
-   procedure Set_Quiet (Q: Boolean) ;
-
-   procedure Log (What : in String) ;
-   procedure Print (What : in String) ;
-
-end Logging;
--- bnlbot/botstart/bot-0-9/source/tcl/test_socket.tcl
+++
@@ -1,6 +0,0 @@
-
-
-set s [socket localhost 27124]
-fconfigure $s -buffering none 
-puts $s "avail=2342.0,expos=3245.94"
-close $s 
