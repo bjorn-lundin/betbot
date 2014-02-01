602f9f2e9dccfac7111cce436cfac28513a25bfd
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2014-02-01 15:39:51 +0000

    mailer

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/send_aws_mail.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/send_aws_mail.adb
@@ -4,18 +4,23 @@ use  Ada, AWS;
  
 procedure Send_aws_mail is
    Status : SMTP.Status;
---   Auth : aliased constant SMTP.Authentication.Plain.Credential :=
---      SMTP.Authentication.Plain.Initialize ("bnlbetbot@gmail.com", "Alice2010");
+   Auth   : aliased constant SMTP.Authentication.Plain.Credential :=
+           SMTP.Authentication.Plain.Initialize ("AKIAJZDDS2DVUNB76S6A",
+                                                 "AhVJXW+YJRE/AMBPoUEOaCjAaWJWWRTDC8JoU039baJG");
    Isp : SMTP.Receiver;
+   SMTP_Server: String := "email-smtp.eu-west-1.amazonaws.com";
 begin
    Isp :=
-      SMTP.Client.Initialize
-        ("smtp.telenor.se",
-         Port       => 25);
+     Server : SMTP.Receiver := SMTP.Client.Initialize
+                                (SMTP_Server,
+                                 Port       => 465,
+                                 Secure     => True,
+                                 Credential => Auth'Unchecked_Access);
+
    SMTP.Client.Send
      (Isp,
-      From    => SMTP.E_Mail ("Me", "bnlbetbot@gmail.com"),
-      To      => SMTP.E_Mail ("You", "b.f.lundin@gmail.com"),
+      From    => SMTP.E_Mail ("Nonobet Betbot", "betbot@nonobet.com"),
+      To      => SMTP.E_Mail ("Björn Lundin", "b.f.lundin@gmail.com"),
       Subject => "subject",
       Message => "Here is the text",
       Status  => Status);
