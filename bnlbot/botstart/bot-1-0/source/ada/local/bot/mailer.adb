e318177760641dab9f1ca22a0c3d9dd64cdaa8cf
bnl <b.f.lundin@gmail.com>
2014-02-01 15:39:51 +0000

    mailer

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/mailer.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/mailer.adb
@@ -15,16 +15,21 @@ procedure Mailer is
   SMTP_Server : SMTP.Receiver := SMTP.Client.Initialize
                                 (SMTP_Server_Name,
                                  Port       => 465,
---                                 Secure     => True,
+                                 Secure     => True,
                                  Credential => Auth'Unchecked_Access);
 begin
 
+
+  Text_Io.Put_Line ("1");
+
+
   SMTP.Client.Send(Server  => SMTP_Server,
                    From    => SMTP.E_Mail ("Nonobet Betbot", "betbot@nonobet.com"),
-                   To      => SMTP.E_Mail("B Lundin", "b.f.lundin@gmail.com"),
+                   To      => SMTP.E_Mail("", "b.f.lundin@gmail.com"),
                    Subject => "About AWS SMTP protocol",
                    Message => "From Amazon",
                    Status  => Status);
+  Text_Io.Put_Line ("2");
                    
   if not SMTP.Is_Ok (Status) then
     Text_Io.Put_Line ("Can't send message: " & SMTP.Status_Message (Status));
