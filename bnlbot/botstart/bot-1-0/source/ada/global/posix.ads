6e0b351718285339926136c88e87de6f5330cf00
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2014-04-20 21:23:31 +0000

    autogenerate posix constants

--- bnlbot/botstart/bot-1-0/source/ada/global/posix.ads
+++ bnlbot/botstart/bot-1-0/source/ada/global/posix.ads
@@ -3,6 +3,7 @@
 
 with Interfaces.C.Strings;
 with Sattmate_Types; use Sattmate_Types;
+with C_Constants;
 
 package Posix is
 
@@ -27,70 +28,52 @@ package Posix is
   type Pid_T is range -1 .. 2_147_483_647;
   for Pid_T'SIZE use 32;
 
-  O_RDONLY   : constant Int :=      8#00#; -- open for reading only
-  O_WRONLY   : constant Int :=      8#01#; -- open for writing only
-  O_RDWR     : constant Int :=      8#02#; -- open for reading and writing
-  O_CREAT    : constant Int :=    8#0100#; -- no file? create it
-  O_EXCL     : constant Int :=    8#0200#; -- lock file (see below)
-  O_NOCTTY   : constant Int :=    8#0400#; -- if tty, don't acquire it
-  O_TRUNC    : constant Int :=   8#01000#; -- file exists? truncate it
-  O_APPEND   : constant Int :=   8#02000#; -- file exists? move to end
-  O_NONBLOCK : constant Int :=   8#04000#; -- if pipe, don't wait for data
-  O_SYNC     : constant Int :=  8#010000#; -- don't cache writes
-  O_ASYNC    : constant Int :=  8#020000#; -- async. IO via SIGIO
-  O_DIRECT   : constant Int :=  8#040000#; -- direct disk access
-  O_LARGEFILE: constant Int := 8#0100000#; -- not implemented in Linux (yet)
-  O_DIRECTORY: constant Int := 8#0200000#; -- error if file isn't a dir
-  O_NOFOLLOW : constant Int := 8#0400000#; -- if sym link, open link itself  
+  O_RDONLY   : constant Int := Int(C_Constants.O_RDONLY);    -- open for reading only
+  O_WRONLY   : constant Int := Int(C_Constants.O_WRONLY);    -- open for writing only
+  O_RDWR     : constant Int := Int(C_Constants.O_RDWR);      -- open for reading and writing
+  O_CREAT    : constant Int := Int(C_Constants.O_CREAT);     -- no file? create it
+  O_EXCL     : constant Int := Int(C_Constants.O_EXCL);      -- lock file (see below)
+  O_NOCTTY   : constant Int := Int(C_Constants.O_NOCTTY);    -- if tty, don't acquire it
+  O_TRUNC    : constant Int := Int(C_Constants.O_TRUNC);     -- file exists? truncate it
+  O_APPEND   : constant Int := Int(C_Constants.O_APPEND);    -- file exists? move to end
+  O_NONBLOCK : constant Int := Int(C_Constants.O_NONBLOCK);  -- if pipe, don't wait for data
+  O_SYNC     : constant Int := Int(C_Constants.O_SYNC);      -- don't cache writes
+  O_ASYNC    : constant Int := Int(C_Constants.O_ASYNC);     -- async. IO via SIGIO
+  O_DIRECT   : constant Int := Int(C_Constants.O_DIRECT);    -- direct disk access
+  O_LARGEFILE: constant Int := Int(C_Constants.O_LARGEFILE); -- not implemented in Linux (yet)
+  O_DIRECTORY: constant Int := Int(C_Constants.O_DIRECTORY); -- error if file isn't a dir
+  O_NOFOLLOW : constant Int := Int(C_Constants.O_NOFOLLOW);  -- if sym link, open link itself  
 
--- /usr/include/i386-linux-gnu/bits/fcntl.h
---#define O_ACCMODE	   0003
---#define O_RDONLY	     00
---#define O_WRONLY	     01
---#define O_RDWR		     02
---#define O_CREAT		   0100	/* not fcntl */
---#define O_EXCL		   0200	/* not fcntl */
---#define O_NOCTTY	   0400	/* not fcntl */
---#define O_TRUNC		  01000	/* not fcntl */
---#define O_APPEND	  02000
---#define O_NONBLOCK	  04000
---#define O_NDELAY	O_NONBLOCK
---#define O_SYNC	       04010000
---#define O_FSYNC		 O_SYNC
---#define O_ASYNC		 020000
-
-
-
-
+  
 
    ------------ lock files start --------------
    type Short_Integer is new Sattmate_Types.Integer_2;
    
    type aLock is new Short_Integer;
-   F_RDLCK : constant aLock := 0; -- read lock
-   F_WRLCK : constant aLock := 1; -- write lock
-   F_UNLCK : constant aLock := 2; -- unlock (remove a lock)
-   F_EXLCK : constant aLock := 3; -- exclusive lock
-   F_SHLCK : constant aLock := 4; -- shared lock
+   F_RDLCK : constant aLock := aLock(C_Constants.F_RDLCK); -- read lock
+   F_WRLCK : constant aLock := aLock(C_Constants.F_WRLCK); -- write lock
+   F_UNLCK : constant aLock := aLock(C_Constants.F_UNLCK); -- unlock (remove a lock)
+   F_EXLCK : constant aLock := aLock(C_Constants.F_EXLCK); -- exclusive lock
+   F_SHLCK : constant aLock := aLock(C_Constants.F_SHLCK); -- shared lock
    
-   F_DUPFD  : constant int :=  0;
-   F_GETFD  : constant int :=  1;
-   F_SETFD  : constant int :=  2;
-   F_GETFL  : constant int :=  3;
-   F_SETFL  : constant int :=  4;
-   F_GETLK  : constant int :=  5;
-   F_SETLK  : constant int :=  6;
-   F_SETLKW : constant int :=  7;
-   F_SETOWN : constant int :=  8;
-   F_GETOWN : constant int :=  9;
-   F_SETSIG : constant int := 10;
-   F_GETSIG : constant int := 11;   
+   F_DUPFD  : constant Int := Int(C_Constants.F_DUPFD);
+   F_GETFD  : constant Int := Int(C_Constants.F_GETFD);
+   F_SETFD  : constant Int := Int(C_Constants.F_SETFD);
+   F_GETFL  : constant Int := Int(C_Constants.F_GETFL);
+   F_SETFL  : constant Int := Int(C_Constants.F_SETFL);
+   F_GETLK  : constant Int := Int(C_Constants.F_GETLK);
+   F_SETLK  : constant Int := Int(C_Constants.F_SETLK);
+   F_SETLKW : constant Int := Int(C_Constants.F_SETLKW);
+   F_SETOWN : constant Int := Int(C_Constants.F_SETOWN);
+   F_GETOWN : constant Int := Int(C_Constants.F_GETOWN);
+   F_SETSIG : constant Int := Int(C_Constants.F_SETSIG);
+   F_GETSIG : constant Int := Int(C_Constants.F_GETSIG);   
    
    
    type aWhenceMode is new Short_Integer;
-   SEEK_SET : constant aWhenceMode := 0; -- absolute position
-   SEEK_CUR : constant aWhenceMode := 1; -- offset from current position
-   SEEK_END : constant aWhenceMode := 2; -- offset from end of file  
+   SEEK_SET : constant aWhenceMode := aWhenceMode(C_Constants.SEEK_SET); -- absolute position
+   SEEK_CUR : constant aWhenceMode := aWhenceMode(C_Constants.SEEK_CUR); -- offset from current position
+   SEEK_END : constant aWhenceMode := aWhenceMode(C_Constants.SEEK_END); -- offset from end of file  
    
    type Lockstruct is record
      L_Type   : Alock;         -- type of lock
@@ -105,12 +88,7 @@ package Posix is
    pragma import_valued_procedure( fcntl );   
    --------------- lockfiles stop --------------
 
-
-
-
   function Getpid return Pid_T;
-  
-  
   function Setsid return Pid_T;
   function Fork return Pid_T;
   Fork_Failed : exception;
