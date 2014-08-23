16e74b591958ce91543d673643516fceaf9d13a2
bnl <b.f.lundin@gmail.com>
2014-08-23 14:24:54 +0000

    exposure + new strategies

--- bnlbot/arduinotalker/serial_printer.py
+++ bnlbot/arduinotalker/serial_printer.py
@@ -23,6 +23,7 @@ def signal_handler(signal, frame):
 
 class global_obj():
     profit = 0
+    exposure = 0
     time_to_print = None
 
     def __init__(self) :
@@ -33,6 +34,7 @@ class global_obj():
 
     def to_string(self) :
         print 'to_string.profit', self.profit
+        print 'to_string.exposure', self.exposure
         print 'to_string.ts', self.ts
         print 'to_string.last_print_ts', self.last_print_ts
     ############end to_string #######################
@@ -63,7 +65,8 @@ class progress_bar():
 
 ############ end progressbar ###############
 
-def findout_result_change(conn, g, s):
+#def findout_result_change(conn, g, s):
+def findout_result_change(conn, g):
     #only if the real db is connected
     #get todays profit up til now
     today = datetime.datetime.now()
@@ -93,6 +96,35 @@ def findout_result_change(conn, g, s):
 
 ############ end findout_result_change ###############
 
+def findout_exposure(conn, g):
+    #only if the real db is connected
+    #get current exposure
+    today = datetime.datetime.now()
+    start = datetime.datetime(today.year, today.month, today.day, 0, 0, 0)
+    stop = datetime.datetime(today.year, today.month, today.day, 23, 59, 59)
+    cur = conn.cursor()
+    cur.execute("select sum(B.SIZE)  \
+                 from ABETS B   \
+                 where B.BETWON is NULL  \
+                 and BETNAME not like 'DR%%'  \
+                 and B.BETPLACED >= %s  \
+                 and B.BETPLACED <= %s " ,
+                 (start, stop))
+    if cur.rowcount >= 1 :
+        row = cur.fetchone()
+        if row :
+          result = row[0]
+        else :
+          result = 0
+
+    if result is None:
+        result = 0
+
+    g.exposure = int(result)
+    cur.close()
+    conn.commit()
+############ end findout_exposure ###############
+
 def print_to_printer(p,g):
     p.linefeed()
     p.print_text("-----------------------")
@@ -110,8 +142,11 @@ def print_to_printer(p,g):
 
 def show(c,g,s,p):
   #check and perhaps print to screen
-  findout_result_change(c, g, s)
+#  findout_result_change(c, g, s)
+  findout_result_change(c, g)
   s.display_profit(g.profit)
+  findout_exposure(c, g)
+  s.display_exposure(g.exposure)
   today = datetime.datetime.now()
 
   if today > g.time_to_print :
@@ -132,7 +167,7 @@ g = global_obj()
 p = thermal_printer.thermal_printer(serialport='/dev/ttyAMA0')
 
 cnt = 600
-maxcnt = cnt / 1.0 # amke it a float
+maxcnt = cnt / 1.0 # make it a float
 while True:
     cnt = cnt + 1
     event = pygame.event.poll()
