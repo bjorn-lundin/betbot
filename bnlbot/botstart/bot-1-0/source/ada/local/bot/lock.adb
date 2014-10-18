0857ee1cfe7a97747e301c6099b65f73e131ba33
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2014-10-18 19:50:49 +0000

    fixed locks issue via lockf instead of fcntl

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/lock.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/lock.adb
@@ -1,23 +1,16 @@
 with Ada.Environment_Variables;
 with Logging; use Logging;
---with General_Routines; use General_Routines;
---with Types; use Types;
 with Utils; use Utils;
 with Interfaces.C.Strings;
 with Calendar2;
-
 
 package body Lock is
   Me : constant String := "Lock.";  
 
   package EV renames Ada.Environment_Variables;
   
-  
-  
-  ------------------------------------------------------------------
   procedure Take(A_Lock : in out Lock_Type; Name : in String) is
-    L      : Lockstruct;
-    result : int;
+    Result : Int;
     use Interfaces.C.Strings;
   begin
   
@@ -26,21 +19,17 @@ package body Lock is
     declare
       C_Name : Chars_Ptr := New_String (To_String(A_Lock.Name));
     begin
-      A_Lock.Fd := Posix.Open(C_name, O_RDWR + O_CREAT , 8#644#); --O_NONBLOCK is not needed
+      A_Lock.Fd := Posix.Open(C_name, O_WRONLY + O_CREAT , 8#644#); --O_NONBLOCK is not needed
       Free(C_Name);
     end;    
       
-     L.L_Type := F_WRLCK;
-     L.L_Whence := 0;
-     L.L_Start := 0;
-     L.L_Len := 0;
-     Fcntl( result, A_Lock.Fd, F_SETLK, L );
-     A_Lock.Currently_Holding_Lock := True;
-     if result = -1 then
-        Log(Me & "Take", "Take lock failed, Errno =" & Errno'Img);
-        A_Lock.Currently_Holding_Lock := False;
-        raise Lock_Error with "Errno =" & Errno'Img ;
-     end if;
+    Result := Posix.Lockf(A_Lock.Fd, F_TLOCK, 0); -- test lock, and lock if unlocked, error if already locked
+    A_Lock.Currently_Holding_Lock := True;
+    if Result = -1 then
+      Log(Me & "Take", "Take lock failed, Errno =" & Errno'Img);
+      A_Lock.Currently_Holding_Lock := False;
+      raise Lock_Error with "Errno =" & Errno'Img ;
+    end if;
   -- file is now locked
     --put pid in file  
     declare
@@ -55,37 +44,34 @@ package body Lock is
     begin
       Size := Posix.Write(A_Lock.Fd, C_Pid_Str, Str'Length);
       Free(C_Pid_Str);
-      if integer(size) = -1 then
+      if Integer(Size) = -1 then
         Log(Me & "Take", "write pid Errno =" & Errno'Img);
       end if;
-      if integer(size) /= Str'length then
-        Log(Me & "Take", "size/str'length =" & size'Img & "/" & Str'Length'Img);
+      if Integer(Size) /= Str'length then
+        Log(Me & "Take", "Size/str'length =" & Size'Img & "/" & Str'Length'Img);
       end if;
     end;    
     Log(Me & "Take", "Lock taken");
   exception    
     when others => raise Lock_Error;    
   end Take;
+  
   ------------------------------------------------------------------
   
   procedure Finalize(A_Lock : in out Lock_Type) is
-    L      : Lockstruct;
     Result : int;
   begin
---      Log(Me & "Finalize", "Remove loc");    
+    Log(Me & "Finalize", " start Remove loc");    
       -- unlock file
     if A_Lock.Currently_Holding_Lock then
-      L.L_Start := 0;
-      L.L_Len := 0;
-      L.L_Type := F_UNLCK;
-      L.L_Whence := 0;
-      fcntl( result, A_Lock.Fd, F_SETLKW, L );
-      if result = -1 then
+      Result := Posix.Lockf(A_Lock.Fd, F_ULOCK, 0); --unlock
+      if Result = -1 then
         Log(Me & "Finalize", "fcntl failed in unlock/Finalize, Errno =" & Errno'Img);
       end if;      
---      Log(Me & "Finalize", "Lock removed");
+      Log(Me & "Finalize", "Lock removed");
     end if; 
-   end Finalize;
+    Log(Me & "Finalize", " end Remove loc");    
+  end Finalize;
   ------------------------------------------------------------------
-  
+
 end Lock;
