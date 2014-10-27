93a73db67255710d8a598c90652ed31b57f0a21c
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2014-10-27 16:12:58 +0000

    from new, days_allowed + other bets

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
@@ -2,9 +2,10 @@ with Bot_Types     ; use Bot_Types;
 with Types; use Types;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
+with Calendar2;
 
 package Config is
+  type Allowed_Days_Array is array(Calendar2.Week_Day_Type'range) of Boolean;
 
   type Config_Type is tagged record
     Size                       : Bet_Size_Type    := 30.0;
@@ -14,6 +15,7 @@ package Config is
     Max_Turns_Not_Started_Race : Integer_4        := 17;  --17*30s -> 8.5 min
     Enabled                    : Boolean          := False;
     Allowed_Countries          : Unbounded_String := Null_Unbounded_String;
+    Allowed_Days               : Allowed_Days_Array     := (others => False);
   end record;  
   
   function Create(Filename : String) return Config_Type;
