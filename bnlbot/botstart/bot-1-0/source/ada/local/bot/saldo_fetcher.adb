d3c0f05cec3078bda15c6f91c22f35aefa7efa00
bnl <b.f.lundin@gmail.com>
2014-12-18 12:17:35 +0000

    is no daemon anymore, started once every night, and exit instead, like data_mover

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/saldo_fetcher.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/saldo_fetcher.adb
@@ -3,25 +3,25 @@ with Ada.Environment_Variables;
 with Ada.Directories;
 with Ada.Exceptions;
 with Ada.Command_Line;
-
 with Ada.Characters;
 with Ada.Characters.Latin_1;
-
 with Ada.Strings;
 with Ada.Strings.Fixed;
---with Unicode;
---with Unicode.Encodings;
---with Unicode.CES;
---with Text_Io;
-with Stacktrace;
-with Types; use Types;
-with Sql;
---with General_Routines; use General_Routines;
 
 with GNAT;
 with GNAT.Sockets;
 with GNAT.Command_Line; use GNAT.Command_Line;
 with GNAT.Strings;
+
+with AWS;
+with AWS.SMTP; 
+with AWS.SMTP.Authentication;
+with AWS.SMTP.Authentication.Plain;
+with AWS.SMTP.Client;
+
+with Stacktrace;
+--with Types; use Types;
+with Sql;
 
 with Utils;
 with Calendar2; use Calendar2;
@@ -34,32 +34,21 @@ with Table_Abalances;
 with Ini;
 with Logging; use Logging;
 
-with Process_IO;
-with Core_Messages;
-
-with AWS;
-with AWS.SMTP; 
-with AWS.SMTP.Authentication;
-with AWS.SMTP.Authentication.Plain;
-with AWS.SMTP.Client;
+--with Process_IO;
+--with Core_Messages;
 
 
 procedure Saldo_Fetcher is
   package EV renames Ada.Environment_Variables;
-
   use type Rpc.Result_Type;
   
   Me : constant String := "Main.";  
 
-  Msg      : Process_Io.Message_Type;
-  Sa_Par_Token : aliased Gnat.Strings.String_Access;
   Sa_Par_Bot_User : aliased Gnat.Strings.String_Access;
-  Ba_Daemon    : aliased Boolean := False;
-  Cmd_Line : Command_Line_Configuration;
-  
-  Betfair_Result : Rpc.Result_Type := Rpc.Ok;
- 
-  My_Lock  : Lock.Lock_Type;
+  Ba_Daemon       : aliased Boolean := False;
+  Cmd_Line        : Command_Line_Configuration;
+  Betfair_Result  : Rpc.Result_Type := Rpc.Ok;
+  My_Lock         : Lock.Lock_Type;
 ---------------------------------------------------------
 
   function Get_Db_Size(Db_Name : String ) return String ; -- forward declaration only
@@ -131,13 +120,6 @@ procedure Saldo_Fetcher is
     T : Sql.Transaction_Type;
     Now : Calendar2.Time_Type := Calendar2.Clock;
   begin
-    Log(Me & "Balance", "Login in db");
-    Sql.Connect
-          (Host     => Ini.Get_Value("database_saldo_fetcher","host",""),
-           Port     => Ini.Get_Value("database_saldo_fetcher","port",5432),
-           Db_Name  => Ini.Get_Value("database_saldo_fetcher","name",""),
-           Login    => Ini.Get_Value("database_saldo_fetcher","username",""),
-           Password => Ini.Get_Value("database_saldo_fetcher","password",""));
 
     Rpc.Get_Balance(Betfair_Result,Saldo);           
          
@@ -148,10 +130,7 @@ procedure Saldo_Fetcher is
       T.Commit;
       Mail_Saldo(Saldo);
     end if;  
-    Log(Me & "Balance", "Logoff from db");
-    Sql.Close_Session;
   end Balance;    
-     
      
   -----------------------------------------------------------
   function Get_Db_Size(Db_Name : String ) return String is
@@ -178,13 +157,6 @@ procedure Saldo_Fetcher is
   end Get_Db_Size;
      
 ------------------------------ main start -------------------------------------
-  Is_Time_To_Check_Balance,
-  Is_Time_To_Exit  : Boolean := False;
-  Day_Last_Check : Calendar2.Day_Type := 1;
-  Now : Calendar2.Time_Type := Calendar2.Clock;
-  Last_Keep_Alive : Calendar2.Time_Type := Calendar2.Time_Type_First;
-  
-  OK : Boolean := False;
   Saldo : Table_Abalances.Data_Type;
   Global_Enabled : Boolean := True;
 begin
@@ -200,20 +172,13 @@ begin
     Help        => "user of bot");
     
   Define_Switch
-    (Cmd_Line,
-     Sa_Par_Token'access,
-     "-t:",
-     Long_Switch => "--token=",
-     Help        => "use this token, if token is already retrieved");
-
-  Define_Switch
      (Cmd_Line,
       Ba_Daemon'access,
       "-d",
       Long_Switch => "--daemon",
       Help        => "become daemon at startup");
   Getopt (Cmd_Line);  -- process the command line
-   
+     
   if Ba_Daemon then
      Posix.Daemonize;
   end if;
@@ -231,87 +196,37 @@ begin
           );    
   Rpc.Login; 
   Log(Me, "Login betfair done");
+  Log(Me, "Login in db");
+  Sql.Connect
+        (Host     => Ini.Get_Value("database_saldo_fetcher","host",""),
+         Port     => Ini.Get_Value("database_saldo_fetcher","port",5432),
+         Db_Name  => Ini.Get_Value("database_saldo_fetcher","name",""),
+         Login    => Ini.Get_Value("database_saldo_fetcher","username",""),
+         Password => Ini.Get_Value("database_saldo_fetcher","password",""));
 
---  Sql.Connect
---        (Host     => Ini.Get_Value("database_saldo_fetcher","host",""),
---         Port     => Ini.Get_Value("database_saldo_fetcher","port",5432),
---         Db_Name  => Ini.Get_Value("database_saldo_fetcher","name",""),
---         Login    => Ini.Get_Value("database_saldo_fetcher","username",""),
---         Password => Ini.Get_Value("database_saldo_fetcher","password",""));
-
-  
-  -- to get mail on the 1st of each month too, if restart
-  case Now.Day is
-    when  1 .. 10 => Day_Last_Check := 20;
-    when 11 .. 20 => Day_Last_Check := 30;
-    when 21 .. 31 => Day_Last_Check := 10;
-  end case;
  
-  Main_Loop : loop  
-    Receive_Loop : loop   
-      begin
-        Process_Io.Receive(Msg, 5.0);
-        Log(Me, "msg : "& Process_Io.Identity(Msg)'Img & " from " & Utils.Trim(Process_Io.Sender(Msg).Name));
-        case Process_Io.Identity(Msg) is
-          when Core_Messages.Exit_Message                  => exit Main_Loop;
-          when others => Log(Me, "Unhandled message identity: " & Process_Io.Identity(Msg)'Img);  --??
-        end case;  
-      exception
-        when Process_Io.Timeout => 
-          Now := Calendar2.Clock;
-          if Now - (0,0,10,0,0) > Last_Keep_Alive then
-            Rpc.Keep_Alive(OK); 
-            Last_Keep_Alive := Now;
-            exit Main_Loop when not OK;     
-          end if;
-      end;
-      Now := Calendar2.Clock;
-      --restart every day
-      Is_Time_To_Exit          := Now.Hour = 01 and then 
-                                  Now.Minute = 00 and then
-                                  Now.Second >= 50 and then 
-                                  Day_Last_Check /= Now.Day;
-                                                                      
-      Is_Time_To_Check_Balance := Now.Hour = 05 and then 
-                                  Now.Minute = 00 and then
-                                  Now.Second >= 50 and then 
-                                  Day_Last_Check /= Now.Day;
-      Log(Me, "Is_Time_To_Check_Balance: " & Is_Time_To_Check_Balance'Img &
-      " Day_Last_Check:" & Day_Last_Check'Img &
-      " Now.Day:" & Now.Day'Img);  --??
---      Is_Time_To_Check_Balance := True;
-      exit Receive_Loop when Is_Time_To_Check_Balance;
-      
-      exit Main_Loop when Is_Time_To_Exit;
-      
-    end loop Receive_Loop;  
-    Day_Last_Check := Now.Day;
-    
-    
-    if Global_Enabled then   
-      Ask : loop
-        Balance(Betfair_Result, Saldo );
-        Log(Me, "Ask_Balance result : " & Betfair_Result 'Img);
-        case Betfair_Result is
-          when Rpc.Ok => exit Ask ;
-          when Rpc.Logged_Out => 
-            delay 2.0;
-            Log(Me, "Logged_Out, will log in again");  --??
-            Rpc.Login;    
-          when Rpc.Timeout =>  delay 5.0;
-        end case;           
-      end loop Ask;
-    else  
-      Log(Me, "sending mails not enabled in [email] section of login.ini");
-    end if;  
-  end loop Main_Loop; 
-               
---  Log(Me, "shutting down, close db");
---  Sql.Close_Session;
+  if Global_Enabled then   
+    Ask : loop
+      Balance(Betfair_Result, Saldo );
+      Log(Me, "Ask_Balance result : " & Betfair_Result 'Img);
+      case Betfair_Result is
+        when Rpc.Ok => exit Ask ;
+        when Rpc.Logged_Out => 
+          delay 2.0;
+          Log(Me, "Logged_Out, will log in again");  --??
+          Rpc.Login;    
+        when Rpc.Timeout =>  delay 5.0;
+      end case;           
+    end loop Ask;
+  else  
+    Log(Me, "sending mails not enabled in [email] section of login.ini");
+  end if;  
+
+  Log(Me, "Logoff from db");
+  Sql.Close_Session;
   Rpc.Logout;
   Log(Me, "do_exit");
   Posix.Do_Exit(0); -- terminate
-  Log(Me, "after do_exit");
  
 exception
   when Lock.Lock_Error => 
