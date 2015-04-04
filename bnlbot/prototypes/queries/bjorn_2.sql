ffe994a5d6c389e85a8bcb72b31cb1ac865b05bf
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-04 19:40:15 +0000

    Removing old queries

--- bnlbot/prototypes/queries/bjorn_2.sql
+++
@@ -1,6 +0,0 @@
-﻿select MW.*,MP.* from AMARKETS MW, AMARKETS MP 
-        where MW.EVENTID = MP.EVENTID
-        and MW.STARTTS = MP.STARTTS 
-        and MW.MARKETID = '1.114498877' 
-        and MP.MARKETTYPE = 'PLACE' 
-        and MW.MARKETTYPE = 'WIN'
