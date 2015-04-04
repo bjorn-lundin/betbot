6ea37a47f5cc335be83fb5c8900b86ec15329297
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-04 19:41:32 +0000

    Work in progress

--- bnlbot/prototypes/analysis.py
+++ bnlbot/prototypes/analysis.py
@@ -5,144 +5,54 @@ Main prototype module for Betfair analysis
 from __future__ import print_function, division, absolute_import
 import psycopg2
 from queries import queries
+import entity
+
+def diff_ts(ts0, ts1):
+    '''
+    Return the difference between time stamps in seconds
+    '''
+    diff = ts1 - ts0
+    return diff.seconds
 
 
-def call_db_and_print(conn, queries):
-
+def analyse_1(conn):
+    '''
+    Main analysis 1
+    '''
+    cur = None
+    data = None
     try:
         cur = conn.cursor()
-        for _ in queries:
-            print(_[0])
-            print('=' * 80)
-            cur.execute(_[1])
-            print(cur.fetchall())
-            print()
-    except Exception as e:
-        print(_, e)
+        cur.execute(queries.QUERIES['q1'])
+        data = cur.fetchall()
+    except psycopg2.Error as error:
+        print(error)
     finally:
         cur.close()
         conn.commit()
 
+    u_m = {} # Dict with entity Market as unique key
+    for row in data:
+        market = entity.Market(row[0])
+        t_s = row[1] # timestamp
+        
+        if market not in u_m:
+            u_m[market] = []
 
-def check_data(conn):
-    # eventid's with eventtypeid = 7
-    q1 = \
-        ''' 
-        SELECT
-            eventid
-        FROM
-            aevents
-        WHERE
-            eventtypeid = 7
-        AND countrycode in ('GB','IE');
-        '''
-    event_ids = None
-    try:
-        cur = conn.cursor()
-        cur.execute(q1)
-        event_ids = cur.fetchall()
-    except Exception as e:
-        print('q1', e)
-        print('q1', _)
-    finally:
-        cur.close()
-        conn.commit()
-    print('q1', len(event_ids))
-
-    # marketid's from events above with markettype WIN 
-    q2 = \
-        '''
-        SELECT
-            marketid
-        FROM
-            amarkets
-        WHERE
-            eventid = %s
-        AND markettype = 'WIN'
-        AND betdelay > 0;
-        '''
-    market_ids = []
-    try:
-        cur = conn.cursor()
-        for _ in event_ids:
-            cur.execute(q2, _)
-            market_ids.extend(cur.fetchall())
-    except Exception as e:
-        print('q2', e)
-        print('q2', _)
-    finally:
-        cur.close()
-        conn.commit()
-    print('q2', len(market_ids))
-    
-    # Join marketid's from above with selectionid's in dict raceprice_keys 
-    #(key: marketid, value: [list of selectionid's])
-    q3 = \
-        '''
-        SELECT
-            selectionid
-        FROM
-            arunners
-        WHERE
-            marketid = %s;
-        '''
-    raceprice_keys = {}
-    try:
-        cur = conn.cursor()
-        for _ in market_ids:
-            selection_ids = []
-            cur.execute(q3, _)
-            selection_ids.extend(cur.fetchall())
-            if _ not in raceprice_keys:
-                raceprice_keys[_] = selection_ids
-    except Exception as e:
-        print('q3', e)
-        print('q3', _)
-    finally:
-        cur.close()
-        conn.commit()
-    print('q3', len(raceprice_keys))
-
-    q4 = \
-        '''
-        SELECT
-            backprice
-        FROM
-            apricesfinish 
-        WHERE
-            marketid = %s 
-            AND selectionid = %s
-        LIMIT 1;
-        '''
-    backprice = 0
-    no_backprice = 0
-    try:
-        cur = conn.cursor()
-        # For every marketid...
-        for _ in raceprice_keys.keys():
-            # ...together with every selectionid...
-            for _2 in raceprice_keys[_]:
-                cur.execute(q4, [_, _2])
-                # ...see if backprices exist
-                row = cur.fetchone()
-                if (row is not None):
-                    backprice += 1
-                else:
-                    no_backprice += 1
-    except Exception as e:
-        print('q4', e)
-        print('q4', _)
-    finally:
-        cur.close()
-        conn.commit()
-    print('q4', 'no_backprice:', no_backprice)
-    print('q4', 'backprice:', backprice)
+        if t_s not in u_m[market]:
+            u_m[market].append(t_s)
+            if len(u_m[market]) > 1:
+                diff = diff_ts(u_m[market][-2], u_m[market][-1])
+                if diff < 1:
+                    starttime = u_m[market][-2]
+                    market.starttime = starttime
+                    print(market)
+                    break
 
 
 if __name__ == "__main__":
     CONN = psycopg2.connect("dbname=dry user=joakim")
-    call_db_and_print(CONN, queries.QUERIES)
-    #check_data(CONN)
+    analyse_1(CONN)
     CONN.close()
     exit(0)
 
