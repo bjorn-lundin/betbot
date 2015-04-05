c9261eb4183f4d17de471de5e85da6d7e6a28703
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-05 12:51:00 +0000

    Cleanup

--- bnlbot/prototypes/analyse.py
+++ bnlbot/prototypes/analyse.py
@@ -7,26 +7,16 @@ import psycopg2
 import query
 import entity
 
-def diff_ts(ts0, ts1):
-    '''
-    Return the difference between time stamps in seconds
-    '''
-    diff = ts1 - ts0
-    return diff.seconds
-
-
 def analyse(conn, q_data, q_name):
     '''
     Main analysis
     '''
-
-    cur = None
     data = None
 
     try:
         cur = conn.cursor()
-        print(cur.mogrify(query.query.named(q_name), q_data))
-        cur.execute(named(q_name), q_data)
+        print(cur.mogrify(query.named(q_name), q_data))
+        cur.execute(query.named(q_name), q_data)
         data = cur.fetchall()
     except psycopg2.Error as error:
         print(error)
@@ -34,10 +24,10 @@ def analyse(conn, q_data, q_name):
         cur.close()
         conn.commit()
 
-    u_market = {} # Dict with entity Market as unique key
+    u_market = {} # Entity Market as unique key
     for row in data:
         market = entity.Market(row[0])
-        t_stamp = row[1] # timestamp
+        t_stamp = row[1]
 
         if market not in u_market:
             u_market[market] = []
@@ -45,33 +35,33 @@ def analyse(conn, q_data, q_name):
         if t_stamp not in u_market[market]:
             u_market[market].append(t_stamp)
             if len(u_market[market]) > 1:
-                diff = diff_ts(u_market[market][-2], u_market[market][-1])
-                if diff < 1:
-                    starttime = u_market[market][-2]
-                    market.starttime = starttime
-                    print(market.marketid)
-                    print(market.starttime)
+                time_diff = u_market[market][-1] - u_market[market][-2]
+                if time_diff.seconds < 1:
+                    market.starttime = u_market[market][-2]
                     break
+
+
+def run_analysis(conn):
+    '''
+    Setting parameters and run analyse
+    '''
+    status = ('WINNER', 'LOSER')
+    markettype = 'WIN'
+    date = ('2014-09-02',)
+    marketid = ('1.115258242',)
+    q_name = 'q-with-marketid'
+    q_data = (status, markettype, date, marketid)
+
+    if len(marketid) == 0:
+        q_name = 'q-without-marketid'
+        q_data = (status, markettype, date)
+
+    analyse(conn, q_data, q_name)
 
 
 if __name__ == "__main__":
     CONN = psycopg2.connect("dbname=dry user=joakim")
-
-    STATUS = ('WINNER', 'LOSER')
-    MARKETTYPE = 'WIN'
-    DATE = ('2014-09-02',)
-    MARKETID = ('1.115258242',)
-    #MARKETID = ()
-
-    Q_NAME = 'q-with-marketid'
-    Q_DATA = (STATUS, MARKETTYPE, DATE, MARKETID)
-
-    if len(MARKETID) == 0:
-        print('hej')
-        Q_NAME = 'q-without-marketid'
-        Q_DATA = (STATUS, MARKETTYPE, DATE)
-
-    analyse(CONN, Q_DATA, Q_NAME)
+    run_analysis(CONN)
     CONN.close()
     exit(0)
 
