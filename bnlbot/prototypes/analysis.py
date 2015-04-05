7a91387fd753e41a9b09bcc689e639f0535ed116
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-05 09:55:29 +0000

    Work in progress

--- bnlbot/prototypes/analysis.py
+++ bnlbot/prototypes/analysis.py
@@ -15,15 +15,18 @@ def diff_ts(ts0, ts1):
     return diff.seconds
 
 
-def analyse_1(conn):
+def analyse(conn, q_data, q_name):
     '''
-    Main analysis 1
+    Main analysis
     '''
+
     cur = None
     data = None
+
     try:
         cur = conn.cursor()
-        cur.execute(queries.QUERIES['q1'])
+        print(cur.mogrify(queries.QUERIES[q_name], q_data))
+        cur.execute(queries.QUERIES[q_name], q_data)
         data = cur.fetchall()
     except psycopg2.Error as error:
         print(error)
@@ -31,28 +34,44 @@ def analyse_1(conn):
         cur.close()
         conn.commit()
 
-    u_m = {} # Dict with entity Market as unique key
+    u_market = {} # Dict with entity Market as unique key
     for row in data:
         market = entity.Market(row[0])
-        t_s = row[1] # timestamp
-        
-        if market not in u_m:
-            u_m[market] = []
+        t_stamp = row[1] # timestamp
 
-        if t_s not in u_m[market]:
-            u_m[market].append(t_s)
-            if len(u_m[market]) > 1:
-                diff = diff_ts(u_m[market][-2], u_m[market][-1])
+        if market not in u_market:
+            u_market[market] = []
+
+        if t_stamp not in u_market[market]:
+            u_market[market].append(t_stamp)
+            if len(u_market[market]) > 1:
+                diff = diff_ts(u_market[market][-2], u_market[market][-1])
                 if diff < 1:
-                    starttime = u_m[market][-2]
+                    starttime = u_market[market][-2]
                     market.starttime = starttime
-                    print(market)
+                    print(market.marketid)
+                    print(market.starttime)
                     break
 
 
 if __name__ == "__main__":
     CONN = psycopg2.connect("dbname=dry user=joakim")
-    analyse_1(CONN)
+
+    STATUS = ('WINNER', 'LOSER')
+    MARKETTYPE = 'WIN'
+    DATE = ('2014-09-02',)
+    MARKETID = ('1.115258242',)
+    #MARKETID = ()
+
+    Q_NAME = 'q-with-marketid'
+    Q_DATA = (STATUS, MARKETTYPE, DATE, MARKETID)
+
+    if len(MARKETID) == 0:
+        print('hej')
+        Q_NAME = 'q-without-marketid'
+        Q_DATA = (STATUS, MARKETTYPE, DATE)
+
+    analyse(CONN, Q_DATA, Q_NAME)
     CONN.close()
     exit(0)
 
