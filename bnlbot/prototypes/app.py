737093c2c76e544888b04fafd260db0d6b6566d7
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-25 21:36:27 +0000

    Introduce map reduce

--- bnlbot/prototypes/app.py
+++ bnlbot/prototypes/app.py
@@ -3,6 +3,7 @@ Main application for Betfair analysis
 '''
 from __future__ import print_function, division, absolute_import
 import sys
+import conf
 import collect
 import report
 
@@ -12,11 +13,40 @@ def main(argv):
     Main method
     '''
 
-    if len(argv) > 1 and 't' in argv[1]:
-        print('Will be running multi-process when implemented...')
-        #collect.run_collection_multiproc(conn, collection)
-        exit(1)
+
+    def map_func(dates):
+        collections = []
+        for date in dates:
+            print(date)
+            collector = collect.Collector()
+            collection = collector.run_collection_map_reduce((date,))
+            print(len(collection))
+            collections.append(collection)
+        return collections
+
+
+    def red_func(a, b):
+        a[0].update(b[0])
+
+
+    #argv.append('r')
+    if len(argv) == 2:
+        if 'r' in argv[1]:
+            print('Running map reduce...')
+            dates = [] # Split on date
+            for date in conf.Q_DATE_MAP_REDUCE:
+                dates.append((date,))
+            collections = map(m_func, dates)
+            reduce(r_func, collections)
+            print(len(collections[0][0]))
+        elif 'p' in argv[1]:
+            print('Running multi process...')
+            #collect.run_collection_multiproc(conn, collection)
+        else:
+            print('Nope!')
+            exit(1)
     else:
+        print('Running normal...')
         collector = collect.Collector()
         collection = collector.run_collection()
         report.report_collection(collection)
