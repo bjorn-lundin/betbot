f59e66d28db1cc3f55f492ef7259938a3ce8ece3
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-04-26 22:42:57 +0000

    Working map reduce

--- bnlbot/prototypes/report.py
+++ bnlbot/prototypes/report.py
@@ -4,22 +4,22 @@ Report module for Betfair analysis
 from __future__ import print_function, division, absolute_import
 
 
-def report_collection(collection):
+def report_collection(markets):
     '''
     Report parts from collection
     '''
-    nbr_markets = len(collection)
+    nbr_markets = len(markets)
     nbr_from_start = 0
     not_from_start_markets = []
     not_sufficient_ts = []
 
-    for marketid in collection:
-        if collection[marketid][0].data_from_start:
+    for market in markets:
+        if market.data_from_start:
             nbr_from_start += 1
         else:
-            not_from_start_markets.append(marketid)
-            if len(collection[marketid][0].tstamps) < 100:
-                not_sufficient_ts.append(marketid)
+            not_from_start_markets.append(market.marketid)
+            if len(market.tstamps) < 100:
+                not_sufficient_ts.append(market.marketid)
 
     print('Markets:', nbr_markets)
     print('TS from start:', nbr_from_start)
@@ -33,3 +33,4 @@ def report_collection(collection):
     print('Markets not having sufficient TS:')
     for market in not_sufficient_ts:
         print(market)
+
