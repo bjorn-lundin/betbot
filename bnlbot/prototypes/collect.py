f08a453a1d9fb3d200c57866c30f9ccd9f65fb95
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-05-11 05:36:49 +0000

    Add more collect data to Runner and get market winners

--- bnlbot/prototypes/collect.py
+++ bnlbot/prototypes/collect.py
@@ -6,7 +6,7 @@ import psycopg2
 import query
 import entity
 import conf
-import clean
+#import clean
 
 class Collector(object):
     '''
@@ -18,12 +18,12 @@ class Collector(object):
         self.markets = [] # Condensed to market objects and returned
 
 
-    def collect_step_1(self, db_conn, q_name, q_params):
+    def collect_step_1(self, db_conn_str, q_name, q_params):
         '''
         Query DB
         '''
-        conn = psycopg2.connect(db_conn)
         try:
+            conn = psycopg2.connect(db_conn_str)
             cur = conn.cursor()
             cur.execute(query.named(q_name), q_params)
             self.data = cur.fetchall()
@@ -88,36 +88,75 @@ class Collector(object):
 
     def collect_step_4(self):
         '''
-        Collect runners
+        Collect runners and their data
         '''
         for row in self.data:
             marketid = row[0]
-            selectionid = row[3]
             runnername = row[2]
+            selectionid = row[3]
+            backprice = row[4]
+            layprice = row[5]
+            totalmatched = row[6]
 
             if selectionid not in self.collection[marketid][1]:
                 self.collection[marketid][1].append(selectionid)
                 runner = entity.Runner(selectionid)
                 runner.name = runnername
+                runner.backprices.append(backprice)
+                runner.layprices.append(layprice)
+                runner.totalmatched.append(totalmatched)
                 self.collection[marketid][0].runners.append(runner)
+            else:
+                for runner in self.collection[marketid][0].runners:
+                    if selectionid == runner.selectionid:
+                        runner.backprices.append(backprice)
+                        runner.layprices.append(layprice)
+                        runner.totalmatched.append(totalmatched)
+                        break
 
 
-    def run_collection(self, db_conn, q_name, q_params):
+    def collect_step_5(self, db_conn_str):
+        '''
+        Query DB for market winners
+        '''
+        winners = None
+        markets = (tuple(self.collection.keys()),)
+
+        try:
+            conn = psycopg2.connect(db_conn_str)
+            cur = conn.cursor()
+            cur.execute(query.named('q-get-win-winner'), markets)
+            winners = cur.fetchall()
+        except psycopg2.Error as error:
+            print(error)
+        finally:
+            cur.close()
+            conn.commit()
+            conn.close()
+
+        for row in winners:
+            marketid = row[0]
+            selectionid = row[1]
+            self.collection[marketid][0].win_winner_id = selectionid
+
+
+    def run_collection(self, db_conn_str, q_name, q_params):
         '''
         Collect data
         '''
-        self.collect_step_1(db_conn, q_name, q_params)
+        self.collect_step_1(db_conn_str, q_name, q_params)
         self.collect_step_2()
         self.collect_step_3()
         self.collect_step_4()
+        self.collect_step_5(db_conn_str)
 
         #collect_clean = clean.Clean()
-        #collect_clean.collect_clean(db_conn, self.markets)
+        #collect_clean.collect_clean(db_conn_str, self.markets)
 
         return self.markets
 
 
-def safe_run_collection_date(db_conn, q_name, q_params_part, date):
+def safe_run_collection_date(db_conn_str, q_name, q_params_part, date):
     '''
     Run collection based on date in separate collector (thread safe)
     '''
@@ -130,7 +169,7 @@ def safe_run_collection_date(db_conn, q_name, q_params_part, date):
     q_params.append(date)
     q_params = tuple(q_params)
     collector = Collector()
-    markets = collector.run_collection(db_conn, q_name, q_params)
+    markets = collector.run_collection(db_conn_str, q_name, q_params)
     return markets
 
 
