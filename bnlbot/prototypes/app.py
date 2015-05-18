f725affe31f67b0a248745bfe12fce90b6753c44
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-05-18 07:21:41 +0000

    Clean up main(). Clean up conf.py by add get_date_range in collect.py

--- bnlbot/prototypes/app.py
+++ bnlbot/prototypes/app.py
@@ -2,49 +2,21 @@
 Main application for Betfair analysis
 '''
 from __future__ import print_function, division, absolute_import
-import sys
-import conf
 import collect
-import report
-import multiprocessing
 import clean
+import report
 
 
-def main(argv):
+def main():
     '''
     Main method
     '''
-    argv.append('p')
-    markets = []
-
-    if len(argv) == 2:
-        if 'r' in argv[1]:
-            print('Running map reduce...')
-            markets = [collect.safe_run_collection_date(conf.DB, \
-                    'q-without-marketid', conf.Q_PARAMS_MAP_REDUCE, date) \
-                    for date in conf.Q_DATE_MAP_REDUCE]
-        elif 'p' in argv[1]:
-            print('Running multiprocess...')
-            pool = multiprocessing.Pool(multiprocessing.cpu_count())
-            markets = pool.map(collect.multi_run, conf.Q_DATE_MAP_REDUCE)
-        else:
-            print('Nope!')
-            exit(1)
-        markets = reduce(lambda x, y: x + y, markets)
-    else:
-        print('Running normal...')
-        collector = collect.Collector()
-        markets = collector.run_collection(conf.DB, 'q-with-marketid', \
-                conf.Q_PARAMS_ID)
-
-    if False:
-        print('Preparing data for clean...')
-        clean.collect_clean(conf.DB, markets)
-
+    markets = collect.run_collection_dates_multiproc()
+    if True:
+        clean.collect_clean(markets)
     report.report_collection(markets)
     exit(0)
 
-
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
 
