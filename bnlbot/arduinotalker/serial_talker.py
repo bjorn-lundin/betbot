bb02960e185a537da60a4134fd9a64b9ab0ead69
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-06-06 12:52:33 +0000

    added HORSES_WIN_LAY_FINISH_1.10_25.0_4

--- bnlbot/arduinotalker/serial_talker.py
+++ bnlbot/arduinotalker/serial_talker.py
@@ -39,12 +39,15 @@ def get_row_weeks_back(conn, betname, delta_weeks)  :
 
 #                 "and B.BETMODE = 2 " \
     cur = conn.cursor()
-    cur.execute("select sum(B.PROFIT) " \
-                 "from ABETS B  " \
-                 "where B.BETNAME = %s " \
-                 "and B.BETWON is not NULL " \
-                 "and B.BETPLACED >= %s " \
-                 "and B.BETPLACED <= %s " ,
+    cur.execute("""
+                 select sum(B.PROFIT) 
+                 from ABETS B 
+                 where B.BETNAME = %s
+                 and B.BETWON is not NULL 
+                 and B.EXESTATUS = 'SUCCESS'  
+                 and B.STATUS in ('SETTLED')                 
+                 and B.BETPLACED >= %s 
+                 and B.BETPLACED <= %s """ ,
                    (betname, mon2, sun2))
 
 #    print bet_type, 'rc', cur.rowcount, 'start', day_start, 'stop', day_stop
@@ -72,13 +75,16 @@ def get_row(conn, betname, delta_days)  :
 #    print betname, day_start, day_stop
     cur = conn.cursor()
 #                 "and B.BETMODE = 2 " \
-    cur.execute("select " \
-                 "sum(B.PROFIT) " \
-                 "from ABETS B " \
-                 "where B.BETNAME = %s " \
-                 "and B.BETWON is not NULL " \
-                 "and B.BETPLACED >= %s " \
-                 "and B.BETPLACED <= %s ",
+    cur.execute("""
+                 select 
+                   sum(B.PROFIT) 
+                 from ABETS B 
+                 where B.BETNAME = %s
+                 and B.BETWON is not NULL 
+                 and B.EXESTATUS = 'SUCCESS'  
+                 and B.STATUS in ('SETTLED')                 
+                 and B.BETPLACED >= %s 
+                 and B.BETPLACED <= %s """,
                    (betname,day_start,day_stop))
 #    print betname, 'rc', cur.rowcount, 'start', day_start, 'stop', day_stop
     if cur.rowcount >= 1 :
@@ -96,20 +102,22 @@ def get_row(conn, betname, delta_days)  :
     return result
     ################################## end get_row
 
-def print_bet_ratio(conn)  :
+def get_bet_ratio(conn, betname, delta_days)  :
+    result=0
     cur = conn.cursor()
 #                 "and B.BETMODE = 2 " \
     cur.execute("""
                 select 
                   BETNAME, 
-                  sum(PROFIT) / sum(SIZEMATCHED) as PROFITRATIO 
+                  sum(PROFIT)*100.0 / sum(SIZEMATCHED) as PROFITRATIO 
                 from 
                   ABETS 
-                where BETPLACED::date > (select CURRENT_DATE - interval '4 days') 
+                where BETPLACED::date > (select CURRENT_DATE - interval '%s days') 
                   and BETWON is not null 
                   and EXESTATUS = 'SUCCESS' 
                   and STATUS in ('SETTLED') 
-                  and SIDE = 'BACK' 
+                  and SIDE = 'BACK'
+                  and BETNAME = %s
                 group by 
                   BETNAME 
                 having 
@@ -117,25 +125,20 @@ def print_bet_ratio(conn)  :
                   and max(BETPLACED)::date >= '2015-01-01' 
                 order by 
                   PROFITRATIO desc 
-                  """
-                  );
-
+                  """,(delta_days,betname)
+                  )
+                  
     if cur.rowcount >= 1 :
-        rows = cur.fetchall()
-        print ""
-        print "------------------------------------------------------------------------------"
-        print "   bets order based on winnings (sum(PROFIT) / sum(SIZEMATCHED)) 4 days back"
-        print "   35, 25, 15, 10, 8, 7 %"
-        print "------------------------------------------------------------------------------"
-        for row in rows:
-           r={}
-           r['0']=row[0]
-           r['1']=row[1]*100
-          # print row,r
-           print '%(0)37s  %(1).2f' % r
-           #print row[0],row[1]
+        row = cur.fetchone()
+        if row :
+          result = row[1]
+        else :
+          result = 0
 
-        print "------------------------------------------------------------------------------"
+    if result == None :
+       result = 0
+
+    return result   
     cur.close()
     conn.commit()
 #end print_bet_ratio
@@ -146,53 +149,23 @@ def main(g):
   # return
   # Main program block
   buff = ""
-  if g.source == 1 :
-    g.source = 2
-  else :
-    g.source = 2
+  conn = psycopg2.connect("dbname=bnl \
+                         user=bnl \
+                         host=db.nonodev.com \
+                         password=BettingFotboll1$ \
+                         sslmode=require \
+                         application_name=serial_talker")
 
-  if g.source == 1 :
-    conn = psycopg2.connect("dbname=dry \
-                           user=bnl \
-                           host=db.nonodev.com \
-                           password=BettingFotboll1$ \
-                           sslmode=require \
-                           application_name=serial_talker")
+  bets = ['HORSES_PLC_BACK_FINISH_1.10_7.0_1',
+          'HORSES_WIN_LAY_FINISH_1.10_25.0_4']
 
-    bets = ['HORSES_WIN_22.0_24.0_6_25_BACK_GB',
-            'HORSES_WIN_7.4_8.4_6_25_LAY_GB',
-            'HORSES_WIN_26.0_26.0_6_25_BACK_GB',
-            'HORSES_WIN_30.0_30.0_6_50_LAY_GB',
-            'HORSES_WIN_14.5_14.5_6_25_BACK_IE',
-            'HORSES_WIN_30.0_30.0_6_50_LAY_IE',
-            'HUMAN_PENALTY-TAKEN_NO_1.3_BACK_AL',
-            'HUMAN_SENDING-OFF_NO_1.3_BACK_AL',
-            'HUMAN_HAT-TRICKED-SCORED_NO_1.02_BACK_AL'   ]
-
-  elif g.source == 2 :
-    conn = psycopg2.connect("dbname=bnl \
-                           user=bnl \
-                           host=db.nonodev.com \
-                           password=BettingFotboll1$ \
-                           sslmode=require \
-                           application_name=serial_talker")
-
-    bets = ['HORSES_PLC_BACK_FINISH_1.10_7.0_1',
-            'HORSES_PLC_BACK_FINISH_1.10_20.0_1',
-            'HORSES_PLC_BACK_FINISH_1.10_30.0_1',
-            'HORSES_PLC_BACK_FINISH_1.25_12.0_1',
-            'HORSES_PLC_BACK_FINISH_1.40_50.0_1',
-            'HORSES_PLC_BACK_FINISH_1.50_30.0_1',
-            'HORSES_WIN_LAY_FINISH_1.10_20.0_3',
-            'HORSES_WIN_LAY_FINISH_1.10_20.0_4',            
-            'HORSES_WIN_LAY_FINISH_1.10_30.0_3',
-            'HORSES_WIN_LAY_FINISH_1.10_30.0_4']
     
     #bets = ['HORSES_WIN_LAY_FINISH_1.10_20.0_3',
     #        'HORSES_WIN_LAY_FINISH_1.10_20.0_4',            
     #        'HORSES_WIN_LAY_FINISH_1.10_30.0_3',
     #        'HORSES_WIN_LAY_FINISH_1.10_30.0_4']
-
+    #        'HORSES_PLC_BACK_FINISH_1.25_12.0_1',
+    #        'HORSES_PLC_BACK_FINISH_1.50_30.0_1'
   row0 = {}
   row0['0'] = 0
   row0['1'] = 1
@@ -215,13 +188,13 @@ def main(g):
   for bet in bets :
     row1 = {}
     # offset days from monday
-    row1['0'] = get_row(conn, bet,  0)
-    row1['1'] = get_row(conn, bet, -1)
-    row1['2'] = get_row(conn, bet, -2)
-    row1['3'] = get_row(conn, bet, -3)
-    row1['4'] = get_row(conn, bet, -4)
-    row1['5'] = get_row(conn, bet, -5)
-    row1['6'] = get_row(conn, bet, -6)
+    row1['0'] = get_row(conn, bet,   row0['0'])
+    row1['1'] = get_row(conn, bet, - row0['1'])
+    row1['2'] = get_row(conn, bet, - row0['2'])
+    row1['3'] = get_row(conn, bet, - row0['3'])
+    row1['4'] = get_row(conn, bet, - row0['4'])
+    row1['5'] = get_row(conn, bet, - row0['5'])
+    row1['6'] = get_row(conn, bet, - row0['6'])
                 
     if len(bet) > 37 :
       row1['typ'] = bet[7:]
@@ -246,12 +219,12 @@ def main(g):
 
   for bet in bets :
     row2 = {}
-    row2['0'] = get_row_weeks_back(conn, bet,  0)
-    row2['1'] = get_row_weeks_back(conn, bet, -1)
-    row2['2'] = get_row_weeks_back(conn, bet, -2)
-    row2['3'] = get_row_weeks_back(conn, bet, -3)
-    row2['4'] = get_row_weeks_back(conn, bet, -4)
-    row2['5'] = get_row_weeks_back(conn, bet, -5)
+    row2['0'] = get_row_weeks_back(conn, bet,   row0['0'])
+    row2['1'] = get_row_weeks_back(conn, bet, - row0['1'])
+    row2['2'] = get_row_weeks_back(conn, bet, - row0['2'])
+    row2['3'] = get_row_weeks_back(conn, bet, - row0['3'])
+    row2['4'] = get_row_weeks_back(conn, bet, - row0['4'])
+    row2['5'] = get_row_weeks_back(conn, bet, - row0['5'])
     #remove HORSES_ from HORSES_WIN_9.0_10.0_GREENUP_GB_LB_7_2_5.0
     if len(bet) > 37 :
       row2['typ'] = bet[7:]
@@ -265,12 +238,46 @@ def main(g):
     buff += lcd_row_2 + '\r\n'
 
     
+  buff += '-------------------------------------------------------------------------------\r\n'
+  buff +=  "   35, 25, 15, 10, 8, 7 %\n"
+  buff +=  "------------------------------------------------------------------------------\r\n"
     
+  row0['typ'] = 'sum(PROFIT)/sum(SIZEMATCHED)'
+  row0['0'] = 4
+  row0['1'] = 7
+  row0['2'] = 14
+  row0['3'] = 28
+  row0['4'] = 182
+  row0['5'] = 365
+  row0['6'] = 900
+  lcd_row_0 = '%(typ)37s%(0)6d%(1)6d%(2)6d%(3)6d%(4)6d%(5)6d%(6)6d' % row0
+  buff += lcd_row_0 + '\r\n' 
+  
+  for bet in bets :
+    row3 = {}
+    row3['0'] = get_bet_ratio(conn, bet, row0['0'])
+    row3['1'] = get_bet_ratio(conn, bet, row0['1'])
+    row3['2'] = get_bet_ratio(conn, bet, row0['2'])
+    row3['3'] = get_bet_ratio(conn, bet, row0['3'])
+    row3['4'] = get_bet_ratio(conn, bet, row0['4'])
+    row3['5'] = get_bet_ratio(conn, bet, row0['5'])
+    row3['6'] = get_bet_ratio(conn, bet, row0['6'])
+    if len(bet) > 37 :
+      row3['typ'] = bet[7:]
+    else :
+      row3['typ'] = bet
+    
+    lcd_row_3 = '%(typ)37s%(0)6.2f%(1)6.2f%(2)6.2f%(3)6.2f%(4)6.2f%(5)6.2f%(6)6.2f' % row3
+    buff += lcd_row_3 + '\r\n'
+
+
   #clear screen
   tmp = sp.call('clear',shell=True)   
   #print to screen  
   print buff[:-2]
-  print_bet_ratio(conn)    
+  
+  
+  
   conn.close()
 #  return source
 
