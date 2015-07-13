7aca40ec3c2b019ec47f6fa98ad8ff85ce58e5ee
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-07-13 16:46:45 +0000

    login prel

--- bnlbot/botstart/bot-1-0/source/ada/bot_gui/src/bot_gui-controller.ads
+++ bnlbot/botstart/bot-1-0/source/ada/bot_gui/src/bot_gui-controller.ads
@@ -1,9 +1,13 @@
 with Gnoga.Gui.Window;
 with Gnoga.Application.Multi_Connect;
+with Gnoga.Gui.Base;
 
 package Bot_Gui.Controller is
    procedure Default
      (Main_Window : in out Gnoga.Gui.Window.Window_Type'Class;
-      Connection  : access
-        Gnoga.Application.Multi_Connect.Connection_Holder_Type);
+      Connection  : access Gnoga.Application.Multi_Connect.Connection_Holder_Type);
+        
+--   procedure On_Login (Object : in out Gnoga.Gui.Base.Base_Type'Class) ;
+   procedure On_Submit (Object : in out Gnoga.Gui.Base.Base_Type'Class) ;
+        
 end Bot_Gui.Controller;
--- bnlbot/botstart/bot-1-0/source/ada/bot_gui/src/bot_gui-view.adb
+++ bnlbot/botstart/bot-1-0/source/ada/bot_gui/src/bot_gui-view.adb
@@ -1,3 +1,4 @@
+with Logging ; use Logging;
 package body Bot_Gui.View is
 
    ------------
@@ -10,31 +11,50 @@ package body Bot_Gui.View is
       Parent  : in out Gnoga.Gui.Base.Base_Type'Class;
       ID      : in     String  := "") is
    begin
+      Log ("start Bot_Gui.View.Create");
       Gnoga.Gui.View.View_Type (View).Create (Parent, ID);
+      
+      View.Login_Form.Create (Parent => View,
+                              Method => Gnoga.Gui.Element.Form.Post);
+      
+      View.User.Create (Form  => View.Login_Form,
+                        Size  => 40,
+                        Name  => "User");
+      View.User.Required;
+                                 
+      View.Password.Create (Form  => View.Login_Form,
+                            Size  => 40,
+                            Name  => "Password");
+      View.Password.Required;
+--      View.Do_Login.Create (Form => View.Login_Form, Value => "Login");
+      View.Do_Login.Create (PArent => View, Content => "Login");
+      
+      
+      View.Data_Holder.Create(Parent => View);
+      --make the data invisible until logged id ok
+      View.Data_Holder.Visible(False);
+      
       View.Overflow_Y(Gnoga.Gui.Element.Auto);
 
-      View.Click_Button.Create (View, "stop db task");
+      View.Horizontal_Rule;
       View.Run_Query.Create (View, "Run query");
       View.Horizontal_Rule;
       
-      View.Table_Holder(Buffer_One).Create(View);
+      View.Table_Holder(Buffer_One).Create(View.Data_Holder);
+      
       View.Daily_Table(Buffer_One).Create (View.Table_Holder(Buffer_One));
       View.Horizontal_Rule;
       View.Weekly_Table(Buffer_One).Create (View.Table_Holder(Buffer_One));
       
-     -- View.Table_Holder(Buffer_Two).Create(View);
-     -- View.Table_Holder(Buffer_Two).Visible(False);
-     --  
-     -- View.Daily_Table(Buffer_Two).Create ( View.Table_Holder(Buffer_Two));
-     -- View.Weekly_Table(Buffer_Two).Create (View.Table_Holder(Buffer_Two));
-      
-      
-      View.Horizontal_Rule;
-      View.Image_Holder.Create (View);
-      View.Matched_Image.Create(View.Image_Holder,"img/profit_vs_matched_182.png");
-      View.Lapsed_Image.Create(View.Image_Holder,"img/settled_vs_lapsed_182.png");
+      View.Image_Holder.Create (View.Data_Holder);
+      View.Matched_Image_42.Create(View.Image_Holder,"img/profit_vs_matched_42.png");
+      View.Lapsed_Image_42.Create(View.Image_Holder,"img/settled_vs_lapsed_42.png");
+      View.Matched_Image_182.Create(View.Image_Holder,"img/profit_vs_matched_182.png");
+      View.Lapsed_Image_182.Create(View.Image_Holder,"img/settled_vs_lapsed_182.png");
       View.Horizontal_Rule;
       View.Label_Text.Create (View);
+      Log ("stop Bot_Gui.View.Create");
+      
    end Create;
    
 end Bot_Gui.View;
