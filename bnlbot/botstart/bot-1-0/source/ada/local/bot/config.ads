3e31baeb948e2ca49f5aab5d17e425c33d232833
bnl <b.f.lundin@gmail.com>
2015-08-04 15:25:45 +0000

    inifile structire changes - 1 section per stratgy + global. Also fixed bug asking for markets + 3 min when it really should have been - 3 min before. as it where, 16:00 race was asked for at 16:03 instead of 15:57 ...

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
@@ -5,18 +5,40 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Calendar2;
 
 package Config is
+
+  type Bet_Type is (Back_1_05_7_1_PLC,
+                    Back_1_05_7_1_WIN,
+                    Back_1_10_7_1_PLC,
+                    Back_1_10_7_1_WIN,
+                    Back_1_05_10_1_PLC,
+                    Back_1_05_10_1_WIN,
+                    Back_1_10_10_1_PLC,
+                    Back_1_10_10_1_WIN,
+                    Back_1_50_30_1_PLC,
+                    Back_1_50_30_1_WIN,
+                    Lay_160_200,
+                    Lay_1_10_25_4
+                    );
+
+
   type Allowed_Days_Array is array(Calendar2.Week_Day_Type'range) of Boolean;
 
-  type Config_Type is tagged record
+  type Bet_Config_Type is tagged record
     Size                       : Bet_Size_Type    := 30.0;
     Max_Loss_Per_Day           : Float_8          := -500.0;
-    Fav_Max_Price              : Back_Price_Type  := 1.10;
-    Second_Fav_Min_Price       : Back_Price_Type  := 7.0; 
-    Max_Turns_Not_Started_Race : Integer_4        := 17;  --17*30s -> 8.5 min
+    Enabled                    : Boolean          := False;
+  end record;
+  
+  type Bet_Config_Array_Type is array(Bet_Type'range) of Bet_Config_Type;
+  
+  type Config_Type is tagged record
+    --Size                       : Bet_Size_Type    := 30.0;
+    --Max_Loss_Per_Day           : Float_8          := -500.0;
+    Max_Turns_Not_Started_Race : Integer_4        := 102;  --102*5s -> 8,5 min
     Enabled                    : Boolean          := False;
     Allowed_Countries          : Unbounded_String := Null_Unbounded_String;
     Allowed_Days               : Allowed_Days_Array     := (others => False);
-    Allow_Lay_During_Race      : Boolean          := False;
+    Bet                        : Bet_Config_Array_Type;
   end record;  
   
   function Create(Filename : String) return Config_Type;
