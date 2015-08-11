e3f6eaf10469cbc8a5c7154317cd46d4932752e3
bnl <b.f.lundin@gmail.com>
2015-08-11 13:53:06 +0000

    arrow adjustments

--- bnlbot/botstart/bot-1-0/source/ada/local/utils/race_time.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/utils/race_time.adb
@@ -1,4 +1,4 @@
---with Types;    use Types;
+with Types;    use Types;
 --with Sql;
 with Calendar2; use Calendar2;
 with Text_IO;
@@ -32,7 +32,7 @@ procedure Race_Time is
   Start_Time_List : Rpc.Calendar2_Pack.List;
   Arrow_Is_Printed : Boolean := False;
   Now : Time_Type := Time_Type_First;
-  
+  Start : Time_Type := Clock;
 begin
 
   Ini.Load(Ev.Value("BOT_HOME") & "/login.ini");
@@ -44,31 +44,41 @@ begin
             Vendor_Id  => Ini.Get_Value("betfair","vendor_id",""),
             App_Key    => Ini.Get_Value("betfair","appkey","")
           );
-  Rpc.Login;
-  Log(Me, "Login betfair done");
-
-  Rpc.Get_Starttimes(Start_Time_List);
-  Rpc.Logout;
-  loop
-    Arrow_Is_Printed := False;
-    Now := Calendar2.Clock;
-    for s of Start_Time_List loop
-      if not Arrow_Is_Printed and then
-        Now >= S.Starttime then
-           Print(
-             S.Starttime.String_Time(Seconds => False) & " | " &
-             S.Venue(1..30) & " <----"
-           ) ;
-        Arrow_Is_Printed := True;
-      else
-           Print(
-             S.Starttime.String_Time(Seconds => False) & " | " &
-             S.Venue(1..30)
-           ) ;
-      end if;      
-    end loop;
-    Text_Io.New_Line(25);   
-    delay 5.0;
-  end loop;  
+  Days : loop        
+    Rpc.Login;
+    Log(Me, "Login betfair done");
+    Start_Time_List.Clear;
+    Rpc.Get_Starttimes(Start_Time_List);
+    Rpc.Logout;
+    Day : loop
+      Arrow_Is_Printed := False;
+      Now := Calendar2.Clock;
+      Text_Io.New_Line(Text_Io.Count(Start_Time_List.Length));   
+      for S of Start_Time_List loop
+        if not Arrow_Is_Printed and then
+          Now <= S.Starttime then
+             Print(
+               S.Starttime.String_Time(Seconds => False) & " | " &
+               S.Venue(1..15) & " <----"
+             ) ;
+          Arrow_Is_Printed := True;
+        else
+             Print(
+               S.Starttime.String_Time(Seconds => False) & " | " &
+               S.Venue(1..15)
+             ) ;
+        end if;      
+      end loop;
+      for i in 1 .. 30 loop
+        Text_Io.Put('.');   
+        delay 1.0;
+      end loop;  
+      if Start.Day /= Now.Day then -- new day, get new list
+        Start := Now;
+        exit Day;
+      end if;        
+    end loop Day;
+    
+  end loop Days;    
 
 end Race_Time;
