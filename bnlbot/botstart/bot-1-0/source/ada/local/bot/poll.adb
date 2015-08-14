0b67c2754b9db968a28c0e0815cbbdc152549b77
bnl <b.f.lundin@gmail.com>
2015-08-14 15:11:11 +0000

    fix open log name

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/poll.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/poll.adb
@@ -695,8 +695,8 @@ begin
    --The parent pid dies, and would release the lock...
   My_Lock.Take(EV.Value("BOT_NAME"));
 
-  Logging.Open(EV.Value("BOT_HOME") & "/log/poll.log");
-
+  Logging.Open(EV.Value("BOT_HOME") & "/log/" & EV.Value("BOT_NAME") & ".log");
+  
   Log("Bot svn version:" & Bot_Svn_Info.Revision'Img);
 
   Cfg := Config.Create(Ev.Value("BOT_HOME") & "/" & Sa_Par_Inifile.all);
