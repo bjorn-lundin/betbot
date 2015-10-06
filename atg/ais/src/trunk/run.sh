a1ae4a0d058e36090011f8361f9d5e8fc03cc6ba
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-10-06 18:17:24 +0000

    Add time zone handling

--- atg/ais/src/trunk/run.sh
+++ atg/ais/src/trunk/run.sh
@@ -1,23 +1,34 @@
-# Use this script when running the application from 
+#!/usr/bin/env bash
+
+# Use this script when running the application from
 # e.g. cron or in other situaions when virtualenv
 # can't be set manually. For reference see
 # https://gist.github.com/2199506
 
-#!/bin/sh
-AIS_VIRT_ENV="/home/sejoabi/workspace/py_virt_envs/ais_py_env"
-AIS_HOME="/home/sejoabi/workspace/ais/trunk"
+### Config ###
+AIS_VIRT_ENV="/home/joakim/ais/py_env"
+AIS_HOME="/home/joakim/ais/app"
 AIS_APP="$AIS_HOME/app.py"
+EOD_TIME="22:45"
+DEV="false"
 
-export VIRTUAL_ENV=$AIS_VIRT_ENV
-export PATH="$VIRTUAL_ENV/bin:$PATH"
-unset PYTHON_HOME
+export TZ="/usr/share/zoneinfo/Europe/Stockholm"
+TIME=`date "+%H:%M"`
 
-$AIS_APP eod_download
-$AIS_APP save_files_in_cloud
-$AIS_APP email_log_stats
-$AIS_APP load_eod_racingcard
-$AIS_APP load_eod_vppoolinfo
-$AIS_APP load_eod_vpresult
-$AIS_APP load_eod_ddpoolinfo
-$AIS_APP load_eod_ddresult
-$AIS_APP save_db_dump_in_cloud
+if  [ "$DEV" == "true" ] || [ "$TIME" == "$EOD_TIME" ]; then
+    export VIRTUAL_ENV=$AIS_VIRT_ENV
+    export PATH="$VIRTUAL_ENV/bin:$PATH"
+    unset PYTHON_HOME
+    #$AIS_APP eod_download
+    #$AIS_APP save_files_in_cloud
+    #$AIS_APP email_log_stats
+
+    $AIS_APP load_eod_racingcard
+    $AIS_APP load_eod_vppoolinfo
+    $AIS_APP load_eod_vpresult
+
+    #$AIS_APP load_eod_ddpoolinfo
+    #$AIS_APP load_eod_ddresult
+    #$AIS_APP save_db_dump_in_cloud
+fi
+
