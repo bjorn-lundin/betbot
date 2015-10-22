ab28647bcffa51ffac1004b7554606a5f772b3bb
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-10-22 20:11:11 +0000

    workaround for gnat FSF bug with arrays of sqlstm

--- bnlbot/botstart/bot-1-0/source/ada/global/bot_system_number.ads
+++ bnlbot/botstart/bot-1-0/source/ada/global/bot_system_number.ads
@@ -3,7 +3,7 @@ with Types; use Types;
 
 package Bot_System_Number is
 
-  type System_Number_Type_Type is (Betid, Sampleid);
+  type System_Number_Type_Type is (Betid);
 
   function New_Number (System_Number_Type : in System_Number_Type_Type) return Integer_4;
 
