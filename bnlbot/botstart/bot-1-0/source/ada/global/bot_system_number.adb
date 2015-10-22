456c556cf2af285b6dd472617c2b77dd4fa461c2
bnl <b.f.lundin@gmail.com>
2015-10-22 20:11:11 +0000

    workaround for gnat FSF bug with arrays of sqlstm

--- bnlbot/botstart/bot-1-0/source/ada/global/bot_system_number.adb
+++ bnlbot/botstart/bot-1-0/source/ada/global/bot_system_number.adb
@@ -5,8 +5,8 @@ with Logging;
 package body Bot_System_Number is
 
   Object : constant String := "Bot_System_Number.";
-  Select_Table_Statements  : array(System_Number_Type_Type'range) of Sql.Statement_Type;
-  Get_Number               : array(System_Number_Type_Type'range) of Sql.Statement_Type;
+  Select_Table_Statements  : Sql.Statement_Type ; --array(System_Number_Type_Type'range) of Sql.Statement_Type;
+  Get_Number               : Sql.Statement_Type ; --array(System_Number_Type_Type'range) of Sql.Statement_Type;
 
 --------------------------------------------------------------------------------
   function Is_Number_Taken(System_Number_Type : in System_Number_Type_Type;
@@ -17,14 +17,13 @@ package body Bot_System_Number is
   begin
 --    Logging.Log(Object & Service, "start");
     case System_Number_Type is
-      when Betid    => Select_Table_Statements(System_Number_Type).Prepare("select BETID from ABETS where BETID = :NUM");      
-      when Sampleid => Select_Table_Statements(System_Number_Type).Prepare("select SAMPLEID from APRICESFINISH2 where SAMPLEID = :NUM");      
+      when Betid    => Select_Table_Statements.Prepare("select BETID from ABETS where BETID = :NUM");      
     end case;
     
-    Select_Table_Statements(System_Number_Type).Set("NUM", Number);
-    Select_Table_Statements(System_Number_Type).Open_Cursor;
-    Select_Table_Statements(System_Number_Type).Fetch(End_Of_Set);
-    Select_Table_Statements(System_Number_Type).Close_Cursor;
+    Select_Table_Statements.Set("NUM", Number);
+    Select_Table_Statements.Open_Cursor;
+    Select_Table_Statements.Fetch(End_Of_Set);
+    Select_Table_Statements.Close_Cursor;
 --    Logging.Log( Object & Service, "stop");
     return not End_Of_Set;
   end Is_Number_Taken;
@@ -46,23 +45,22 @@ package body Bot_System_Number is
    
     case Sql.Database is
       when Sql.PostgreSQL =>
-        Get_Number(System_Number_Type).Prepare("select nextval(:SERIAL)");
+        Get_Number.Prepare("select nextval(:SERIAL)");
         case System_Number_Type is
-          when Betid    => Get_Number(System_Number_Type).Set("SERIAL","BET_ID_SERIAL");
-          when Sampleid => Get_Number(System_Number_Type).Set("SERIAL","SAMPLE_ID_SERIAL");
+          when Betid    => Get_Number.Set("SERIAL","BET_ID_SERIAL");
         end case;
     end case;
 
     while not Is_Number_Found and No_Of_Tries < Max_Tries loop
-      Get_Number(System_Number_Type).Open_Cursor;
-      Get_Number(System_Number_Type).Fetch( End_Of_Set);
+      Get_Number.Open_Cursor;
+      Get_Number.Fetch( End_Of_Set);
       if End_Of_Set then
-        Get_Number(System_Number_Type).Close_Cursor;
+        Get_Number.Close_Cursor;
         Transaction.Rollback;
         raise No_More_System_Numbers;
       else
-        Get_Number(System_Number_Type).Get("NEXTVAL", Number);
-        Get_Number(System_Number_Type).Close_Cursor;
+        Get_Number.Get("NEXTVAL", Number);
+        Get_Number.Close_Cursor;
         No_Of_Tries := No_Of_Tries + 1;
         Is_Number_Found := not Is_Number_Taken(System_Number_Type, Number);       
       end if;
