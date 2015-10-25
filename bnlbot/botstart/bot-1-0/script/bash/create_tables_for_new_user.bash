db148806e492225dc08e8a0f75bfe0ec11e8ef43
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-10-25 11:19:09 +0000

    new crontab for longpoll

--- bnlbot/botstart/bot-1-0/script/bash/create_tables_for_new_user.bash
+++ bnlbot/botstart/bot-1-0/script/bash/create_tables_for_new_user.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p abalances    > tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p abethistory >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p abets       >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p aevents     >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p amarkets    >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p anonrunners >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p aprices     >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p arunners    >> tbl.sql
-tclsh $BOT_SCRIPT/tcl/make_table_package.tcl -p awinners    >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=abalances      > tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=apriceshistory >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=abets         >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=aevents       >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=amarkets      >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=aprices       >> tbl.sql
+ $BOT_TARGET/bin/repo --postgresql=arunners      >> tbl.sql
