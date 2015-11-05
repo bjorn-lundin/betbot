33dd26435d3c7245eddf637d0cd17b1c4701d9f1
nonobetmats <nonobetmats@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-11-05 21:38:53 +0000

    Updating BaseNavigatorForm

---
+++ client/NoNoBetClient/trunk/BaseComponents/BaseOverviewGrid.vb
@@ -0,0 +1,11 @@
+﻿Public MustInherit Class BaseOverviewGrid
+  Inherits BaseGrid
+  Implements IOverviewComponent
+
+  Public Sub New()
+    MyBase.New()
+  End Sub
+
+  Public MustOverride Sub NodeChangeHandler(nodeLevel As Integer, keyObject As Object) Implements IOverviewComponent.NodeChangeHandler
+
+End Class
