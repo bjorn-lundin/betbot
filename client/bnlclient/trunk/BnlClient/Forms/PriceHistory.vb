0ff14459542c5588ae722da0e27acddd114bc46f
nonobetmats <nonobetmats@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-11-05 23:10:13 +0000

    First version of PriceHistory form

---
+++ client/bnlclient/trunk/BnlClient/Forms/PriceHistory.vb
@@ -0,0 +1,35 @@
+﻿Imports NoNoBetResources
+Imports NoNoBetResources.ApplicationResourceManager
+Imports NoNoBetBaseComponents
+Imports NoNoBetDbInterface
+Imports Npgsql
+
+
+Public Class PriceHistory
+  Inherits BaseNavigatorForm
+
+  Public Sub New(rManager As ApplicationResourceManager)
+    MyBase.New()
+    MyBase.ResourceManager = rManager
+  End Sub
+
+  Private Sub FillNavigator()
+    Dim dbReader As Npgsql.NpgsqlDataReader
+    Dim sql As String = "SELECT distinct startts FROM amarkets"
+
+    MyBase.Navigator.Nodes.Clear()
+
+    dbReader = MyBase.ResourceManager.DbConnection.ExecuteSqlCommand(sql)
+
+    While dbReader.Read
+      Dim d As DateTime = ConvertToDate(dbReader.Item("startts"))
+
+      MyBase.Navigator.Nodes.Add(d.ToString("yyyy-MM-dd HH:mm"))
+
+    End While
+  End Sub
+
+  Private Sub PriceHistory_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+    FillNavigator()
+  End Sub
+End Class
