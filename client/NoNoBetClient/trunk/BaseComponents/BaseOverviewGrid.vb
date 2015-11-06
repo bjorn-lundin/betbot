1f39f3a375f109860dee2c20b42e9cc15914f5e4
nonobetmats <nonobetmats@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-11-06 23:32:32 +0000

    Updating BaseNavigatorForm and BaseOverviewGrid

--- client/NoNoBetClient/trunk/BaseComponents/BaseOverviewGrid.vb
+++ client/NoNoBetClient/trunk/BaseComponents/BaseOverviewGrid.vb
@@ -1,10 +1,25 @@
-﻿Public MustInherit Class BaseOverviewGrid
+﻿Imports NoNoBetResources
+Imports NoNoBetBaseComponents
+
+Public MustInherit Class BaseOverviewGrid
   Inherits BaseGrid
+  Implements IBaseComponent
   Implements IOverviewComponent
+
+  Private _ResourceManager As ApplicationResourceManager
 
   Public Sub New()
     MyBase.New()
   End Sub
+
+  Public Property ResourceManager As ApplicationResourceManager Implements IBaseComponent.ResourceManager
+    Get
+      Return _ResourceManager
+    End Get
+    Set(value As ApplicationResourceManager)
+      _ResourceManager = value
+    End Set
+  End Property
 
   Public MustOverride Sub NodeChangeHandler(nodeLevel As Integer, keyObject As Object) Implements IOverviewComponent.NodeChangeHandler
 
