3496672ac1a92183eea7b6b4602f31d480e02e66
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2015-11-24 14:52:34 +0000

    login with POST works

---
+++ bnlbot/botstart/bot-1-0/source/ada/bot_ws/html/js/b2.js
@@ -0,0 +1,41 @@
+  //track changes in radiobtn clicks
+var selectedRadio ='bnl'; 
+$(document).ready(function(){
+     $('#loginform input').on('change', function() {
+         selectedRadio = $('input[name="username"]:checked', '#loginform').val();
+         console.log("selectedRadio " + selectedRadio);
+     });
+});  
+$(document).on('pageinit', '#login', function(){  
+        $(document).on('click', '#submit', function() { // catch the form's submit event
+                // Send data to server through the Ajax call
+                // action is functionality we want to call and outputJSON is our data
+                    $.ajax({url: 'login',
+                        data: {action : 'login', formData : $('#loginform').serialize()},
+                        type: 'post',                   
+                        async: 'true',
+                        dataType: 'json',
+                        beforeSend: function() {
+                            // This callback function will trigger before data is sent
+                            console.log("beforeSend");
+                        },
+                        complete: function() {
+                            // This callback function will trigger on data sent/received complete
+                            console.log("complete");
+                        },
+                        success: function (result) {
+                            console.log("success");
+                            if(result.result == "OK") {
+                                console.log("success OK");
+                                $.mobile.changePage("#menu");                         
+                            } else {
+                                console.log("success NOT OK");
+                            }
+                        },
+                        error: function (request,error) {
+                            console.log("error " + error);
+                        }
+                    });                   
+            return false; // cancel original event to prevent form submitting
+        });    
+});    
