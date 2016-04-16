555ef5c228a6774538e8c3bcc0cc1bf858b0aff7
joakim <joakim@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2016-04-16 16:25:41 +0000

    Add to raceday exclude

--- atg/ais/src/trunk/conf_template.py
+++ atg/ais/src/trunk/conf_template.py
@@ -15,12 +15,14 @@ AIS_LOGDIR = os.path.normpath(os.path.join(AIS_DATA, 'log'))
 AIS_DBDUMPDIR = os.path.normpath(os.path.join(AIS_DATA, 'db_dump'))
 AIS_RACEDAY_HISTORY = 6 # Nbr of history days in ATG database
 AIS_RACEDAY_EXCLUDE = {
-    '2011-10-21':[17], 
-    '2011-10-22':[23], 
-    '2013-01-30':[54], 
-    '2013-04-07':[35], 
+    '2011-10-21':[17],
+    '2011-10-22':[23],
+    '2013-01-30':[54],
+    '2013-04-07':[35],
     '2013-12-30':[17],
-    '2014-08-14':[49]
+    '2014-08-14':[49],
+    '2016-03-20':[17],
+    '2016-04-09':[77],
 }
 AIS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 AIS_EOD_DOWNLOAD_DELAY = 1 # E.g. 0.1 equals 100 ms, 2 equals 2 seconds
