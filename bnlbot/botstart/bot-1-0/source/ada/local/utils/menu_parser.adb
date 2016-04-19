8f99480863134d48a480c29135d3a36373f64381
bnl <b.f.lundin@gmail.com>
2016-04-19 14:03:23 +0000

    added sql and daemon handling

--- bnlbot/botstart/bot-1-0/source/ada/local/utils/menu_parser.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/utils/menu_parser.adb
@@ -1,15 +1,18 @@
 with Ada.Exceptions;
 with Ada.Command_Line;
-with Ada.Direct_IO ;
-with Ada.Directories;
+--with Ada.Direct_IO ;
+--with Ada.Directories;
 with Ada.Environment_Variables;
 --with Ada.Strings; use Ada.Strings;
 --with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Text_io;
 with Gnat.Command_Line; use Gnat.Command_Line;
 with Gnat.Strings;
 with Gnatcoll.JSON ;use Gnatcoll.JSON;
 
-with Text_io;
+with Sql;
+with Lock ;
+with Posix;
 with Logging; use Logging;
 with Stacktrace;
 with Ini;
@@ -18,149 +21,175 @@ with Rpc;
 procedure Menu_Parser is
   package EV renames Ada.Environment_Variables;
   use type Rpc.Result_Type;
+  -----------------
+  --function Load_File(Filename : in String) return String is
+  --   use Ada.Directories;
+  --   File_Size    : constant Natural := Natural (Size (Filename));
+  --   subtype JSON_String is String (1 .. File_Size);
+  --   type JSON_String_Ptr is access JSON_String;
+  --   Content : constant JSON_String_Ptr := new JSON_String; 
+  --   package File_IO is new Ada.Direct_IO(JSON_String);
+  --   File : File_IO.File_Type;
+  --begin
+  --   File_IO.Open (File => File, Mode => File_IO.In_File, Name => Filename);
+  --   File_IO.Read (File => File, Item => Content.all);
+  --   File_IO.Close(File => File);
+  --   return Content.all;    
+  --end Load_File;
+  --------------------------  
+  Me                 : constant String := "Menu_Parser.Main";
+  My_Lock            : Lock.Lock_Type;
   Cmd_Line           : Command_Line_Configuration;
   Sa_Input_File      : aliased Gnat.Strings.String_Access;
-  Me : constant String := "Menu_Parser.Main";
-  -----------------
-  function Load_File(Filename : in String) return String is
-     use Ada.Directories;
-     File_Size    : constant Natural := Natural (Size (Filename));
-     subtype JSON_String is String (1 .. File_Size);
-     type JSON_String_Ptr is access JSON_String;
-     Content : constant JSON_String_Ptr := new JSON_String; 
-     package File_IO is new Ada.Direct_IO(JSON_String);
-     File : File_IO.File_Type;
-  begin
-     File_IO.Open (File => File, Mode => File_IO.In_File, Name => Filename);
-     File_IO.Read (File => File, Item => Content.all);
-     File_IO.Close(File => File);
-     return Content.all;    
-  end Load_File;
-  --------------------------  
-  Menu : Json_Value;
-  Children : array (1..7) of JSON_Array := (others => Empty_Array);
+  Menu               : Json_Value;
+  Children           : array (1..7) of JSON_Array := (others => Empty_Array);
+  Ba_Daemon          : aliased Boolean := False;
 
-
-  
 begin
- --  Define_Switch
- --   (Cmd_Line,
- --    Sa_Input_File'access,
- --    Long_Switch => "--file=",
- --    Help        => "input json file");
-
- --  Getopt (Cmd_Line);  -- process the command line
- --  Menu := Read (Strm     => Load_File(Sa_Input_File.all),
- --                Filename => "debug.out");
-
-   Ini.Load(Ev.Value("BOT_HOME") & "/" & "login.ini");
-
-   Rpc.Init(
-            Username   => Ini.Get_Value("betfair","username",""),
-            Password   => Ini.Get_Value("betfair","password",""),
-            Product_Id => Ini.Get_Value("betfair","product_id",""),
-            Vendor_Id  => Ini.Get_Value("betfair","vendor_id",""),
-            App_Key    => Ini.Get_Value("betfair","appkey","")
-          );
-   Rpc.Login;
-   Log(Me, "Login betfair done");
-
-   Rpc.Get_Navigation_Data(Menu);
+  Define_Switch
+   (Cmd_Line,
+    Sa_Input_File'access,
+    Long_Switch => "--file=",
+    Help        => "input json file");
+    
+  Define_Switch
+    (Cmd_Line,
+     Ba_Daemon'access,
+     "-d",
+     Long_Switch => "--daemon",
+     Help        => "become daemon at startup");
+ 
+  Getopt (Cmd_Line);  -- process the command line
+  --Menu := Read (Strm     => Load_File(Sa_Input_File.all),
+  --              Filename => "debug.out");
+ 
+  Ini.Load(Ev.Value("BOT_HOME") & "/" & "login.ini");
+  Logging.Open(EV.Value("BOT_HOME") & "/log/menu_parser.log");
+  if Ba_Daemon then
+     Posix.Daemonize;
+  end if;
+   --must take lock AFTER becoming a daemon ... 
+   --The parent pid dies, and would release the lock...
+  My_Lock.Take(EV.Value("BOT_NAME"));    
+  
+ 
+ 
+  
+ 
+  Rpc.Init(
+           Username   => Ini.Get_Value("betfair","username",""),
+           Password   => Ini.Get_Value("betfair","password",""),
+           Product_Id => Ini.Get_Value("betfair","product_id",""),
+           Vendor_Id  => Ini.Get_Value("betfair","vendor_id",""),
+           App_Key    => Ini.Get_Value("betfair","appkey","")
+         );
+  Rpc.Login;
+  Log(Me, "Login betfair done");
+  Sql.Connect
+       (Host     => Ini.Get_Value("database","host",""),
+        Port     => Ini.Get_Value("database","port",5432),
+        Db_Name  => Ini.Get_Value("database","name",""),
+        Login    => Ini.Get_Value("database","username",""),
+        Password => Ini.Get_Value("database","password",""));
+ 
+  Rpc.Get_Navigation_Data(Menu);
+                
+ 
+  Log("0 type:" & Menu.Get("type") & " name:" & Menu.Get("name"));
+  if Menu.Has_Field("children") then
+    Children(1) := Menu.Get("children");
+    for i in 1 .. Length(Children(1)) loop
+      declare
+        Child1 : Json_Value := Get(Children(1),i);
+      begin
+        if Child1.Has_Field("children") and then Child1.Get("type") = "EVENT_TYPE" and then  Child1.Get("name") ="Soccer" then 
+          Log("    1 type:" & Child1.Get("type") & " name:" & Child1.Get("name"));
+          Children(2) := Child1.Get("children");
+          for j in 1 .. Length(Children(2)) loop
+            declare
+              Child2 : Json_Value := Get(Children(2),j);
+            begin
+              if Child2.Has_Field("children") and then Child2.Get("type") = "GROUP" and then
+                 (--Child2.Get("name") = "Argentinian Soccer" or
+                  Child2.Get("name") = "Belgian Soccer" or
+                 -- Child2.Get("name") = "Brazilian Soccer" or
+                  Child2.Get("name") = "Danish Soccer" or
+                  Child2.Get("name") = "Dutch Soccer" or
+                  Child2.Get("name") = "English Soccer" or
+                  Child2.Get("name") = "German Soccer" or
+                  Child2.Get("name") = "Italian Soccer" or
+                  Child2.Get("name") = "Portuguese Soccer" or
+                  Child2.Get("name") = "Spanish Soccer" or
+                  Child2.Get("name") = "Swedish Soccer" ) then
                  
-
-   Log("0 type:" & Menu.Get("type") & " name:" & Menu.Get("name"));
-   if Menu.Has_Field("children") then
-     Children(1) := Menu.Get("children");
-     for i in 1 .. Length(Children(1)) loop
-       declare
-         Child1 : Json_Value := Get(Children(1),i);
-       begin
-         if Child1.Has_Field("children") and then Child1.Get("type") = "EVENT_TYPE" and then  Child1.Get("name") ="Soccer" then 
-           Log("    1 type:" & Child1.Get("type") & " name:" & Child1.Get("name"));
-           Children(2) := Child1.Get("children");
-           for j in 1 .. Length(Children(2)) loop
-             declare
-               Child2 : Json_Value := Get(Children(2),j);
-             begin
-               if Child2.Has_Field("children") and then Child2.Get("type") = "GROUP" and then
-                  (--Child2.Get("name") = "Argentinian Soccer" or
-                   Child2.Get("name") = "Belgian Soccer" or
-                  -- Child2.Get("name") = "Brazilian Soccer" or
-                   Child2.Get("name") = "Danish Soccer" or
-                   Child2.Get("name") = "Dutch Soccer" or
-                   Child2.Get("name") = "English Soccer" or
-                   Child2.Get("name") = "German Soccer" or
-                   Child2.Get("name") = "Italian Soccer" or
-                   Child2.Get("name") = "Portuguese Soccer" or
-                   Child2.Get("name") = "Spanish Soccer" or
-                   Child2.Get("name") = "Swedish Soccer" ) then
-                  
-                 Log("        2 type:" & Child2.Get("type") & " name:" & Child2.Get("name"));
-                 Children(3) := Child2.Get("children");
-                 for k in 1 .. Length(Children(3)) loop
-                   declare
-                     Child3 : Json_Value := Get(Children(3),k);
-                   begin
-                     if Child3.Has_Field("children") and then Child3.Get("type") = "EVENT" and then
-                        (Child3.Get("name") = "Belgian Jupiler League" or  -- belgien
-                         Child3.Get("name") = "Danish Superliga" or        -- danmark
-                         Child3.Get("name") = "Eredivisie" or              -- holland
-                         Child3.Get("name") = "Barclays Premier League" or -- england
-                         Child3.Get("name") = "Bundesliga 1" or            -- tyskland
-                         Child3.Get("name") = "Bundesliga 2" or            -- tyskland
-                         Child3.Get("name") = "Serie A" or                 -- italien
-                         Child3.Get("name") = "Primeira Liga" or           -- portugal
-                         Child3.Get("name") = "Allsvenskan" or             -- sverige
-                         Child3.Get("name") = "Primera Division" ) then    -- spanien
-                       Log("            3 type:" & Child3.Get("type") & " name:" & Child3.Get("name"));
-                       Children(4) := Child3.Get("children");
-                       for l in 1 .. Length(Children(4)) loop
-                         declare
-                           Child4 : Json_Value := Get(Children(4),l);
-                         begin
-                           if Child4.Has_Field("children") and then Child4.Get("type") = "GROUP" and then Child4.Get("name")(1..8) = "Fixtures" then
-                             Log("                4 type:" & Child4.Get("type") & " name:" & Child4.Get("name"));
-                             Children(5) := Child4.Get("children");
-                             for m in 1 .. Length(Children(5)) loop
-                               declare
-                                 Child5 : Json_Value := Get(Children(5),m);
-                               begin
-                                 if Child5.Has_Field("children") then
-                                   Log("                    5 type:" & Child5.Get("type") & " name:" & Child5.Get("name"));
-                                   Children(6) := Child5.Get("children");
-                                   for n in 1 .. Length(Children(6)) loop
-                                     declare
-                                       Child6 : Json_Value := Get(Children(6),n);
-                                     begin
-                                         --Log("                        6 type:" & Child6.Get("type") & " name:" & Child6.Get("name"));
-                                       if Child6.Get("type") = "MARKET" and then
-                                          Child6.Get("exchangeId") = "1" and then
-                                          (Child6.Get("marketType") = "MATCH_ODDS" or 
-                                           Child6.Get("marketType") = "CORRECT_SCORE" ) then   
-                                         Log("                        6 id:" & Child6.Get("id") & " name:" & Child6.Get("name"));
-                                         -- do stuff here
-                                         --insert into new table
-                                       end if;
-                                     end;
-                                   end loop;
-                                 end if;
-                               end;
-                             end loop;
-                           end if;
-                         end;
-                       end loop;
-                     end if;
-                   end;
-                 end loop;
-               end if;
-             end;
-           end loop;
-         end if;
-       end;
-     end loop;
-   end if;
-
-   Rpc.Logout;
+                Log("        2 type:" & Child2.Get("type") & " name:" & Child2.Get("name"));
+                Children(3) := Child2.Get("children");
+                for k in 1 .. Length(Children(3)) loop
+                  declare
+                    Child3 : Json_Value := Get(Children(3),k);
+                  begin
+                    if Child3.Has_Field("children") and then Child3.Get("type") = "EVENT" and then
+                       (Child3.Get("name") = "Belgian Jupiler League" or  -- belgien
+                        Child3.Get("name") = "Danish Superliga" or        -- danmark
+                        Child3.Get("name") = "Eredivisie" or              -- holland
+                        Child3.Get("name") = "Barclays Premier League" or -- england
+                        Child3.Get("name") = "Bundesliga 1" or            -- tyskland
+                        Child3.Get("name") = "Bundesliga 2" or            -- tyskland
+                        Child3.Get("name") = "Serie A" or                 -- italien
+                        Child3.Get("name") = "Primeira Liga" or           -- portugal
+                        Child3.Get("name") = "Allsvenskan" or             -- sverige
+                        Child3.Get("name") = "Primera Division" ) then    -- spanien
+                      Log("            3 type:" & Child3.Get("type") & " name:" & Child3.Get("name"));
+                      Children(4) := Child3.Get("children");
+                      for l in 1 .. Length(Children(4)) loop
+                        declare
+                          Child4 : Json_Value := Get(Children(4),l);
+                        begin
+                          if Child4.Has_Field("children") and then Child4.Get("type") = "GROUP" and then Child4.Get("name")(1..8) = "Fixtures" then
+                            Log("                4 type:" & Child4.Get("type") & " name:" & Child4.Get("name"));
+                            Children(5) := Child4.Get("children");
+                            for m in 1 .. Length(Children(5)) loop
+                              declare
+                                Child5 : Json_Value := Get(Children(5),m);
+                              begin
+                                if Child5.Has_Field("children") then
+                                  Log("                    5 type:" & Child5.Get("type") & " name:" & Child5.Get("name"));
+                                  Children(6) := Child5.Get("children");
+                                  for n in 1 .. Length(Children(6)) loop
+                                    declare
+                                      Child6 : Json_Value := Get(Children(6),n);
+                                    begin
+                                        --Log("                        6 type:" & Child6.Get("type") & " name:" & Child6.Get("name"));
+                                      if Child6.Get("type") = "MARKET" and then
+                                         Child6.Get("exchangeId") = "1" and then
+                                         (Child6.Get("marketType") = "MATCH_ODDS" or 
+                                          Child6.Get("marketType") = "CORRECT_SCORE" ) then   
+                                        Log("                        6 id:" & Child6.Get("id") & " name:" & Child6.Get("name"));
+                                        -- do stuff here
+                                        --insert into new table
+                                      end if;
+                                    end;
+                                  end loop;
+                                end if;
+                              end;
+                            end loop;
+                          end if;
+                        end;
+                      end loop;
+                    end if;
+                  end;
+                end loop;
+              end if;
+            end;
+          end loop;
+        end if;
+      end;
+    end loop;
+  end if;
+ 
+  Rpc.Logout;
+  Sql.Close_Session;
+  delay 180.0; -- 3 min
                  
 --   Log(Me,Menu.Write); 
 exception
