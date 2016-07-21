e1947d5699e4c3a448f9579af141bdd6e3c33c24
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2016-07-21 19:37:13 +0000

    new sql

---
+++ bnlbot/botstart/bot-1-0/script/sql/ada_best_candidates_from_ael.sql
@@ -0,0 +1,33 @@
+﻿select 
+  sum(cnt),
+  sum(sum_profit) vinst,
+  round(avg(pm),3)::numeric avg_odds,
+  fi,
+  si
+from (
+--         1         2
+--12345678901234567890123456789
+--BACK_1_51_1_55_01_04_1_2_PLC
+select 
+  count('a') cnt,
+  betwon,
+   round((case betwon
+     when true then  sum(profit)* 0.935
+     when false then sum(profit) 
+     else 0.0
+  end)::numeric,2) as sum_profit, 
+  avg(pricematched) pm,
+  SUBSTRING(betname from 6 for 9) fi,
+  SUBSTRING(betname from 16 for 5) si
+from abets
+where 1=1
+and betname like '%PLC%'
+and status = 'SUCCESS'
+group by si,fi,betwon
+order by si,fi,betwon
+
+) tmp
+group by si,fi
+order by -- si,fi
+vinst desc 
+--si,fi
--- bnlbot/botstart/bot-1-0/script/sql/ada_yearly_stats.sql
+++ bnlbot/botstart/bot-1-0/script/sql/ada_yearly_stats.sql
@@ -1,19 +1,19 @@
 ﻿select 
   extract(year from startts)::int as year,
-  betwon,
+ -- betwon,
   sum(sizematched) sum_sizematched,
-  sum(profit)*0.935 sum_profit,
-  avg(profit)*0.935 avg_profit,
-  avg(sizematched) avg_sizematched,
-  avg(pricematched) avg_pricematched,
+  round(sum(profit)*0.935,0)::numeric sum_profit,
+  round(avg(profit)*0.935,2)::numeric avg_profit,
+  round(avg(sizematched),2)::numeric avg_sizematched,
+  round(avg(pricematched),3)::numeric avg_pricematched,
   count('a') cnt --,
 --  extract(week from startts)
 from ABETS
 where 1=1
-and betname ='BACK_1_10_07_1_2_PLC'
+--and betname ='BACK_1_10_07_1_2_PLC_1_01'
 and status = 'SETTLED'
 --and extract(year from startts) = 2015
-group by extract(year from startts), betwon
-order by extract(year from startts), betwon
+group by extract(year from startts)--, betwon
+order by extract(year from startts)--, betwon
 --group by  extract(week from startts)
 --order by  extract(week from startts)
