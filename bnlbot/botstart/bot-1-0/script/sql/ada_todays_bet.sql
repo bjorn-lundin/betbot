f6eec09489b6cce8df161a73122aaf70f18866c1
bnl <b.f.lundin@gmail.com>
2016-08-03 16:48:20 +0000

    updates

--- bnlbot/botstart/bot-1-0/script/sql/ada_todays_bet.sql
+++ bnlbot/botstart/bot-1-0/script/sql/ada_todays_bet.sql
@@ -4,9 +4,9 @@ from ABETS B, ARUNNERS R
 where B.STARTTS::date = (select current_date)
 and  B.MARKETID = R.MARKETID
 and B.SELECTIONID = R.SELECTIONID
---and b.status = 'SETTLED'
---and B.SIDE = 'BACK'
---and betname like '%PLC'
+and b.status = 'SETTLED'
+--and B.SIDE = 'LAY'
+--and betname ='LAY_1_30_05_WIN_2_00'
 --and betname not in ('BACK_1_50_30_1_4_PLC','BACK_1_10_07_1_2_PLC')
-order by  betplaced ,betname, pricematched ,B.STARTTS
+--order by  betplaced ,betname, pricematched ,B.STARTTS
 
