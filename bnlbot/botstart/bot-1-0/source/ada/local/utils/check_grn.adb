8da4e10f48caaf62cfd5cfd603800afc13ed75b8
bnl <b.f.lundin@gmail.com>
2016-10-03 19:25:04 +0000

    new greenup simulator

--- bnlbot/botstart/bot-1-0/source/ada/local/utils/check_grn.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/utils/check_grn.adb
@@ -5,7 +5,6 @@ with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 --with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Environment_Variables;
-with Ada.Containers.Doubly_Linked_Lists;
 
 with Gnat.Command_Line; use Gnat.Command_Line;
 with Gnat.Strings;
@@ -21,12 +20,12 @@ with Logging; use Logging;
 with Markets;
 with Runners;
 with Bets;
-with Price_Histories;
+with Prices;
 with Bot_Svn_Info;
 --with Utils; use Utils;
 
-procedure Check_Lay is
-  Me                  : constant String := "Check_Lay.";
+procedure Check_Grn is
+  Me                  : constant String := "Check_Grn.";
   package EV renames Ada.Environment_Variables;
   T                   : Sql.Transaction_Type;
   Select_Cand         : Sql.Statement_Type;
@@ -36,9 +35,7 @@ procedure Check_Lay is
   Cmd_Line            : Command_Line_Configuration;
   
   Global_Betname   : Betname_Type := (others => ' ');
-  Global_Max_Price : Float_8 := 0.0;
-  Global_Min_Price : Float_8 := 0.0;
-  Global_Laysize   : Bet_Size_Type := 50.0;
+  Global_Laysize   : Bet_Size_Type := 40.0;
 
   SA_Max_Price     : aliased Gnat.Strings.String_Access;
   SA_Min_Price     : aliased Gnat.Strings.String_Access;
@@ -49,26 +46,8 @@ procedure Check_Lay is
   
   Bad_Runners_Place : exception;
 
-  package Ts_List_Pack is new Ada.Containers.Doubly_Linked_Lists(Calendar2.Time_Type);
-  Ts_List             : Ts_List_Pack.List;
 
   ---------------------------------------------------
-
-  procedure Read_Ts(Stm  : in out Sql.Statement_Type;
-                    List : in out Ts_List_Pack.List) is
-    Eos : Boolean := False;
-    Ts : Calendar2.Time_Type;
-  begin
-     Stm.Open_Cursor;
-     loop
-       Stm.Fetch(Eos);
-       exit when Eos;
-       Stm.Get("PRICETS", Ts);
-       List.Append(Ts);
-     end loop;
-     Stm.Close_Cursor;
-  end Read_Ts;
-  --------------------------------------------------
 
 begin
   if not EV.Exists("BOT_NAME") then
@@ -112,8 +91,8 @@ begin
   Getopt (Cmd_Line);  -- process the command line
 
   Move(SA_Betname.all,Global_Betname);
-  Global_Max_Price := Float_8'Value(SA_Max_Price.all);
-  Global_Min_Price := Float_8'Value(SA_Min_Price.all);
+ -- Global_Max_Price := Float_8'Value(SA_Max_Price.all);
+ -- Global_Min_Price := Float_8'Value(SA_Min_Price.all);
   
   case IA_Runners_Place is
     when 1 .. 50 => null;
@@ -142,10 +121,9 @@ begin
 
   Select_Cand.Prepare(
       "select * " &
-      "from APRICESHISTORY " &
+      "from ARUNNERS " &
       "where MARKETID = :MARKETID " &
-      "and PRICETS = :PRICETS " &
-      "order by BACKPRICE"
+      "and STATUS <> 'LAPSED'"
   );
 
   Select_Timestamps.Prepare(
@@ -163,11 +141,11 @@ begin
 
   
   declare
-    Ph_List     : Price_Histories.List_Pack.List;
+    Runner_List     : Runners.List_Pack.List;
     Market_List : Markets.List_Pack.List;
     Cnt         : Natural := 0;
     --type Has_Type is (Lay);
-    subtype Max_Runners_Type is Integer range 1 .. 50;
+--    subtype Max_Runners_Type is Integer range 1 .. 50;
   begin
     T.Start;
     Log(Me & "Main" , "read start");
@@ -175,68 +153,65 @@ begin
     Log(Me & "Main" , "read done");
     T.Commit;
   
-    Market_Loop : for The_Market of Market_List loop
+    Market_Loop : for Market of Market_List loop
       T.Start;
       Cnt := Cnt +1;
       if Cnt rem 100 = 0 then
-        Log(Me & "Main" , "treat: " & The_Market.To_String);
+        Log(Me & "Main" , "treat: " & Market.To_String);
       end if;
-      Ts_List.Clear;
-      Select_Timestamps.Set("MARKETID", The_Market.Marketid);
-      Read_Ts(Select_Timestamps, Ts_List);
+      Select_Cand.Set("MARKETID",Market.Marketid);
+      Runners.Read_List(Select_Cand, Runner_List);
   
-      Timestamp_Loop : for Ts of Ts_List loop
-        Ph_List.Clear;
-        Select_Cand.Set("MARKETID",The_Market.Marketid);
-        Select_Cand.Set("PRICETS",Ts);
-        Price_Histories.Read_List(Select_Cand, Ph_List);
+      Runner_Loop : for Runner of Runner_List loop
   
         declare
-          Idx : Integer := 0;
-          type Best_Runners_Array_Type is array (Max_Runners_Type'range) of Price_Histories.Price_History_Type ;
-          Best_Runners : Best_Runners_Array_Type := (others => Price_Histories.Empty_Data);
-          The_Runner   : Runners.Runner_Type;
-          Eos          : Boolean := False;
-          The_Bet      : Bets.Bet_Type;
+          Eos   : Boolean := False;
+          Bet   : array (Bet_Side_Type'Range) of Bets.Bet_Type;
+          Price : Prices.Price_Type;
+          Backsize : Bet_Size_Type := 0.0;
         begin
-          Ph_Loop : for Ph of Ph_List loop
-            Idx := Idx +1;
-            exit Ph_Loop when Idx > Best_Runners'Last;
-            Best_Runners(Idx) := Ph;
-          end loop Ph_Loop;
-          -- Best_Runners is sorted lowest backprice to highest, max 20 entries
 
-          The_Runner := Runners.Empty_Data;
-          The_Runner.Marketid    := Best_Runners(IA_Runners_Place).Marketid;
-          The_Runner.Selectionid := Best_Runners(IA_Runners_Place).Selectionid;
-          The_Runner.Read(Eos);
-          The_Bet.Clear;
+          Price.Marketid := Runner.Marketid;
+          Price.Selectionid := Runner.Selectionid;
+          Price.Read(Eos);
            
           if not Eos and then
-             Best_Runners(1).Backprice <= Global_Min_Price and then
-             Best_Runners(1).Backprice >= Float_8(1.01) and then
-             Best_Runners(IA_Runners_Place).Backprice <  Float_8(1000.0) and then
-             Best_Runners(IA_Runners_Place).Layprice  <  Global_Max_Price + Float_8(IA_Addon_Odds) and then
-             Best_Runners(IA_Runners_Place).Backprice >= Global_Max_Price and then
-             Best_Runners(IA_Runners_Place).Layprice  >= Global_Max_Price then
+             Price.Backprice >= Float_8(1.01)  and then
+             Price.Layprice  <=  Float_8(29.0) and then
+             Price.Layprice  >=  Float_8(10.0) and then
+             Price.Backprice <=  Float_8(30.0) and then
+             Price.Backprice >=  Float_8(10.0) and then
+             Price.Layprice/Price.Backprice <= Float_8(1.1) then -- max 10% difference Lay/Back
   
-              The_Bet := Bets.Create(Side       => Lay,
-                                    Name       => Global_Betname,
-                                    Size       => Global_Laysize,
-                                    Price      => Price_Type(Global_Max_Price + Float_8(IA_Addon_Odds)),  
-                                    Placed     => Best_Runners(IA_Runners_Place).Pricets,                  
-                                    Runner => The_Runner,
-                                    Market => The_Market);
-
-              The_Bet.Match_Directly(True);
-              The_Bet.Insert;
-              The_Bet.Check_Matched;
-              The_Bet.Check_Outcome;
-              The_Bet.Update_Withcheck;
-              exit Timestamp_Loop; -- 1 bet per market
+            Bet(Lay) := Bets.Create(Side   => Lay,
+                                    Name   => Global_Betname,
+                                    Size   => Global_Laysize,
+                                    Price  => Price_Type(Price.Layprice),  
+                                    Placed => Price.Pricets,                  
+                                    Runner => Runner,
+                                    Market => Market);
+            Bet(Lay).Match_Directly(False);
+            Bet(Lay).Insert;
+            Bet(Lay).Check_Matched;
+            Bet(Lay).Check_Outcome;
+            Bet(Lay).Update_Withcheck;
+            
+            Backsize := Global_Laysize * (Bet_Size_Type(Price.Layprice - Float_8(1.0)) / (Price.Layprice + Float_8(1.0)));  
+            Bet(Back) := Bets.Create(Side   => Back,
+                                     Name   => Global_Betname,
+                                     Size   => Backsize,
+                                     Price  => Price_Type(Price.Layprice + Float_8(1.0)),  
+                                     Placed => Price.Pricets,                  
+                                     Runner => Runner,
+                                     Market => Market);
+            Bet(Back).Match_Directly(False);
+            Bet(Back).Insert;
+            Bet(Back).Check_Matched;
+            Bet(Back).Check_Outcome;
+            Bet(Back).Update_Withcheck;
           end if; --Eos
         end;
-      end loop Timestamp_Loop;
+      end loop Runner_Loop;
   
       T.Commit;
     end loop Market_Loop;
@@ -261,4 +236,4 @@ exception
     Logging.Close;
     Posix.Do_Exit(0); -- terminate
 
-end Check_Lay;
+end Check_Grn;
