ee69c59a20d86a99a335b6d42294c4767d32c1e9
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2016-10-16 11:18:46 +0000

    make sure second bet alway has valid prices (calculated)

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/bot_types.ads
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/bot_types.ads
@@ -1,7 +1,7 @@
 
 with Types; use Types;
 with Unchecked_Conversion;
---with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ; 
+--with Ada.Strings.Unbounded ; use Ada.Strings.Unbounded ;
 
 with Table_Amarkets;
 with Table_Arunners;
@@ -12,15 +12,16 @@ package Bot_Types is
 
 --   subtype Bot_Name_Type is Unbounded_String;
 --   subtype Bot_Log_File_Name_Type is Unbounded_String;
-   type Bet_Market_Type is (Place, 
-                            Winner, 
-                            Match_Odds, 
-                            Correct_Score, 
-                            Half_Time_Score, 
-                            Hat_Tricked_Scored, 
+   type Bet_Market_Type is (Place,
+                            Winner,
+                            Match_Odds,
+                            Correct_Score,
+                            Half_Time_Score,
+                            Hat_Tricked_Scored,
                             Penalty_Taken,
                             Sending_Off);
    type Bet_Side_Type is (Back, Lay);
+   type Team_Type is (Home, Away);
    type Animal_Type is (Horse, Hound, Human);
    type Max_Daily_Profit_Type is new Float_8;
    type Max_Daily_Loss_Type is new Float_8;
@@ -33,28 +34,28 @@ package Bot_Types is
    type Bet_Size_Type is new Float_8;
    type Bet_Price_Type is new Float_8;
    type Min_Num_Runners_Type is new Byte;
-   type Max_Num_Runners_Type is new Byte;   
+   type Max_Num_Runners_Type is new Byte;
    type Num_Winners_Type is new Byte;
    type Favorite_By_Type is new Float_8;
    type Profit_Type is new Float_8;
    type Max_Exposure_Type is new Float_8;
    type Bet_Size_Portion_Type is new Float_8;
-   
+
    type Bet_Persistence_Type is (Lapse, Persist, Market_On_Close);
-   
+
    type Bot_Mode_Type is (Real, Simulation);
    for Bot_Mode_Type'Size use Integer_4'Size;
    for Bot_Mode_Type use ( Real => 2, Simulation => 3);
    function Bot_Mode is new Unchecked_Conversion(Bot_Mode_Type, Integer_4);
    function Bot_Mode is new Unchecked_Conversion(Integer_4, Bot_Mode_Type);
-     
+
    type Bet_Status_Type is (Executable, Execution_Complete, Voided, Cancelled, Lapsed, Settled);
    subtype Cleared_Bet_Status_Type is Bet_Status_Type range  Voided .. Settled ;
-   
+
    type JSON_Data_Type is (I4,Flt,Ts,Str);
-      
+
    type Green_Up_Mode_Type is (None, Lay_First_Then_Back, Back_First_Then_Lay);
-   
+
    subtype Marketid_Type       is String(Table_Amarkets.Empty_Data.Marketid'range);
    subtype Eventname_Type      is String(Table_Aevents.Empty_Data.Eventname'range);
    subtype Runnername_Type     is String(Table_Arunners.Empty_Data.Runnername'range);
@@ -63,7 +64,7 @@ package Bot_Types is
    subtype Bet_Side_String_Type is String(Table_Abets.Empty_Data.Side'range);
    subtype Bet_Timestamp_Image_Type is String(1..23);
    subtype Venue_Type           is String (1..50);
-  
+
    function "-" (Left : Back_Price_Type ; Right : Delta_Price_Type) return Back_Price_Type;
    function "+" (Left : Back_Price_Type ; Right : Delta_Price_Type) return Back_Price_Type;
    function "+" (Left : Back_Price_Type ; Right : Favorite_By_Type) return Back_Price_Type;
@@ -74,9 +75,9 @@ package Bot_Types is
    function "<" (Left : Min_Lay_Price_Type ; Right : Float_8) return Boolean;
    function "<=" (Left : Min_Lay_Price_Type ; Right : Float_8) return Boolean;
    function "<=" (Left : Float_8 ; Right : Max_Lay_Price_Type) return Boolean ;
-   
+
    function "*" (Left : Bet_Size_Type ; Right : Price_Type) return Float_8;
-   
+
    function ">=" (Left : Profit_Type ; Right : Max_Daily_Profit_Type) return Boolean ;
    function ">=" (Left : Profit_Type ; Right : Max_Daily_Loss_Type) return Boolean ;
    function "<" (Left : Profit_Type ; Right : Max_Daily_Loss_Type) return Boolean ;
@@ -91,13 +92,13 @@ package Bot_Types is
    function "*" (Left : Bet_Size_Type ; Right : Bet_Price_Type) return Bet_Size_Type;
    function "/" (Left : Bet_Size_Type ; Right : Bet_Price_Type) return Bet_Size_Type;
 
-   function ">" (Left : Float_8 ; Right : Max_Exposure_Type) return Boolean ; 
-   
+   function ">" (Left : Float_8 ; Right : Max_Exposure_Type) return Boolean ;
+
    function "/" (Left : Bet_Size_Type ; Right : Float_8) return Bet_Size_Type;
 
 
  --  function "*" (Left : Bet_Size_Type ; Right : Lay_Price_Type) return Float_8;
    function "/" (Left : Float_8 ; Right : Back_Price_Type) return Bet_Size_Type ;
-   
-   
+
+
 end Bot_Types;
