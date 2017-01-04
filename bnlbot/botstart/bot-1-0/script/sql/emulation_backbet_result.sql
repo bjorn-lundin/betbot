05becc468445e4085b237f47edb6e83a832ed1c0
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2017-01-04 16:14:33 +0000

    fixed commision

--- bnlbot/botstart/bot-1-0/script/sql/emulation_backbet_result.sql
+++ bnlbot/botstart/bot-1-0/script/sql/emulation_backbet_result.sql
@@ -1,20 +1,38 @@
 ﻿-- assume 100:- as backbet
+with backbets as (
+  select 
+    betname,
+    marketid,
+    startts::date, 
+    count('a') as cnt,
+    sum(
+    case betwon -- comission i slutet
+      when false then -100
+      when true  then  100*(price-1.0) 
+      else 0
+    end
+    ) as SUM_PROFIT
+  from ABETS 
+  where true
+  and betwon is not null
+--  and status in ('SETTLED')
+  and insterrcode in ('INVALID_BET_SIZE')
+  and side = 'BACK'
+  and startts > '2017-01-01 00:00:00'
+  group by betname, marketid, startts::date
+)
 select 
   betname,
-  startts::date, 
-  side,   
-  betwon,
-  count('a') as cnt,
-  sum(
-  case betwon
-    when false then -100*(price-1.0)
-    when true  then 93.5 
-  end
-  ) as sp
-from ABETS 
-where 1=1
-and side = 'LAY'
-and startts > '2017-01-01 00:00:00'
-group by betname, startts::date, side, betwon 
-order by betname, startts::date, side, betwon
+  startts::date,
+  round(sum(
+      case SUM_PROFIT > 0
+        when TRUE  then SUM_PROFIT * (1.0 - (0.065))
+        when FALSE then SUM_PROFIT
+      end
+   ),2) as profit_incl_commision, 
+  round(sum(SUM_PROFIT),2) as profit_excl_commision, 
+  sum(cnt) as cnt
+from backbets
+group by betname, startts::date 
+order by betname, startts::date 
 
