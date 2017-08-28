f08c02cbcf4d84d049b552854a7bf92692860c28
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2017-08-28 11:03:10 +0000

    new laybet tested

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/config.ads
@@ -10,6 +10,7 @@ package Config is
                     Horse_Back_1_11_1_15_05_07_1_2_Plc_1_01,
                     Horse_Back_1_10_07_1_2_Plc_1_01,
                     Horse_Lay_1_05_10_1_2_Win_3_40,
+                    Horse_Lay_1_05_10_1_2_Win_3_50,
                     --Horse_Greenup_Lay_Back_Win_15_00_19_50,
                     --Horse_Greenup_Lay_Back_Win_20_00_29_00,
                     --Horse_Greenup_Lay_Back_Win_30_00_38_00,
