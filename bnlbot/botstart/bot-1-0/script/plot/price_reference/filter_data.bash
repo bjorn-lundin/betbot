2cd89035f08d384013981bb80f46ef7d56d562d6
bnl <b.f.lundin@gmail.com>
2018-01-15 22:33:08 +0000

    snabbare filter

--- bnlbot/botstart/bot-1-0/script/plot/price_reference/filter_data.bash
+++ bnlbot/botstart/bot-1-0/script/plot/price_reference/filter_data.bash
@@ -25,28 +25,25 @@ do
   if [ $OK == "true" ] ; then
 
     #lay=0002.42,tics=18 |  12.86
+    # GREENUP_LAY_FIXED_LOSS_TICS_05_85.00 | -16948.00
+
 
 
     f1=$(echo $line | cut -d'|' -f1)
     fsum=$(echo $line | cut -d'|' -f2)
 
-    #f1=lay=0002.42,tics=18 
-    #fsum=12.86
+    #f1=GREENUP_LAY_FIXED_LOSS_TICS_05_85.00 
+    #fsum=-16948.00
 
 
-    tmp1=$(echo $f1 | cut -d',' -f1)
-    price=$(echo $tmp1 | cut -d'=' -f2)
-
-
-    tmp2=$(echo $f1 | cut -d',' -f2)
-    dtics=$(echo $tmp2 | cut -d'=' -f2)
-
+    dtics=$(echo $f1 | cut -d'_' -f6)
+    price=$(echo $f1 | cut -d'_' -f7)
 
     price_new=$($BOT_TARGET/bin/price_to_tics --value=$price)
 
     echo "$price_new|$dtics|$fsum"
 
-    if [ $dtics == "90" ] ; then
+    if [ $price_new == "259" ] ; then
       echo ""
     fi
   fi
