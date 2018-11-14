3708531e5c96a8d33f97f63da783609924fae3d7
bnl <b.f.lundin@gmail.com>
2018-11-14 14:23:03 +0000

    added os_arch check, supporting macosx

--- bnlbot/botstart/bashrc.bash
+++ bnlbot/botstart/bashrc.bash
@@ -4,12 +4,31 @@ export TZ
 
 export HOSTNAME=$(hostname)
 
+OS=$(uname)
+
+case "${OS}" in
+    Linux)
+      bits=$(uname -m)
+      case ${bits} in
+        i686)    export OS_ARCHITECTURE=lnx_x64 ;;
+        x86_64)  export OS_ARCHITECTURE=lnx_x86 ;;
+        *)       echo "not supported bits ${bits}" ; exit 1 ;;
+      esac
+    ;;
+
+    Darwin)
+      export OS_ARCHITECTURE=drw_x64
+    ;;
+
+    *)
+     echo "not supported OS ${OS}" ; exit 1
+    ;;
+esac    
+
 export ADA_ROOT=/usr/local/ada/2017
-export OS_ARCHITECTURE=lnx_x64
 export ADA_PROJECT_PATH=$ADA_ROOT/aws/lib/gnat:$ADA_PROJECT_PATH
 export PATH=$ADA_ROOT/gprbuild/bin:$ADA_ROOT/gnat/bin:$PATH
 export BOT_XML_SOURCE=GNAT
-
 
 export BOT_START=$HOME/svn/botstart
 . $BOT_START/bot.bash bnl
