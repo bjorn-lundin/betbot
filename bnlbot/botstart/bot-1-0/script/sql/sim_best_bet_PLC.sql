e23f41a8dac4d7bdd3039cc8593f85464767e92e
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2018-11-14 13:28:45 +0000

    added clever queries

---
+++ bnlbot/botstart/bot-1-0/script/sql/sim_best_bet_PLC.sql
@@ -0,0 +1,26 @@
+﻿select
+  betname,
+  round(avg(sizematched),2) size,
+  --extract(year from betplaced) yr,
+  --extract(week from betplaced) wk,
+ count('a'), 
+  sum(profit) profit,
+  round(sum(
+    case when betwon then profit*0.935
+       else profit
+    end),2) profit2, 
+  round((100.0 * sum(profit)/sum(sizematched)),2) rate,
+  round((100.0 * 
+    sum(
+      case when betwon then profit*0.935
+         else profit
+      end)/
+      sum(sizematched)),2) rate2
+from abets
+where true  
+and status = 'MATCHED'
+and betname like 'PLC%'
+--and betplaced > '2018-09-01'
+group by betname --,yr, wk
+order by profit2 desc
+-- betname --, yr, wk
