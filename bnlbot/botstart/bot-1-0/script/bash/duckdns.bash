666edf48dfec0c5ce0ccedcdd5a7f391595ccf4f
bnl <b.f.lundin@gmail.com>
2018-11-19 11:56:01 +0000

    redirect stderr to /dev/null to avoid mails of no interest

--- bnlbot/botstart/bot-1-0/script/bash/duckdns.bash
+++ bnlbot/botstart/bot-1-0/script/bash/duckdns.bash
@@ -2,4 +2,4 @@
 domain=lundin.duckdns.org
 token=c223b17c-f292-4c23-9ead-41ed91e01704
 url="https://www.duckdns.org/update?domains=${domain}&token=${token}&ip="
-echo url=${url} | curl -k -o /var/log/duck.log -K -
+echo url=${url} | curl -k -o /var/log/duck.log -K - 2> /dev/null
