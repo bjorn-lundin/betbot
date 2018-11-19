a691a8b661398b48c217884301b2257b3a3aebf0
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2018-11-19 12:27:56 +0000

    date fixes

--- bnlbot/botstart/bot-1-0/source/ada/bot_ws/bot_web_server.adb
+++ bnlbot/botstart/bot-1-0/source/ada/bot_ws/bot_web_server.adb
@@ -257,13 +257,7 @@ procedure Bot_Web_Server is
     pragma Unreferenced(Request);
   begin
     Logging.Log ("Unknown", "Method : Unknown");
-    return AWS.Response.Acknowledge (Status_Code => Messages.[database]
-name=bnl
-username=bnl
-password=ld4BC9Q51FU9CYjC21gp
-port=5432
-host=localhost
-S405);
+    return AWS.Response.Acknowledge (Status_Code => Messages.S405);
   end Unknown;
   --------------------------------------
 
