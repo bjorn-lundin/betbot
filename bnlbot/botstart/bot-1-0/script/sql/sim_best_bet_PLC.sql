e003280e2a9b720300658f2eb4eff855af50ce39
bnl <b.f.lundin@gmail.com>
2018-11-28 07:31:01 +0000

    nys sqls

--- bnlbot/botstart/bot-1-0/script/sql/sim_best_bet_PLC.sql
+++ bnlbot/botstart/bot-1-0/script/sql/sim_best_bet_PLC.sql
@@ -1,26 +1,34 @@
 ﻿select
   betname,
-  round(avg(sizematched),2) size,
+  max(startts) max_startts,
+  round(avg(sizematched),2) avg_size,
+  round(avg(
+      case when betwon 
+         then pricematched
+      end),2) avg_pricematched2,
+  round(avg(pricematched),2) avg_pricematched,
   --extract(year from betplaced) yr,
   --extract(week from betplaced) wk,
- count('a'), 
+  count('a'), 
   sum(profit) profit,
   round(sum(
-    case when betwon then profit*0.935
+    case when betwon 
+       then profit*0.935
        else profit
     end),2) profit2, 
   round((100.0 * sum(profit)/sum(sizematched)),2) rate,
   round((100.0 * 
     sum(
-      case when betwon then profit*0.935
+      case when betwon 
+         then profit*0.935
          else profit
       end)/
       sum(sizematched)),2) rate2
 from abets
 where true  
 and status = 'MATCHED'
-and betname like 'PLC%'
---and betplaced > '2018-09-01'
+--and betname like '%1.28%00.00%'
 group by betname --,yr, wk
-order by profit2 desc
--- betname --, yr, wk
+order by -- betname,
+profit2 desc
+-- betname,max(startts),  --, yr, wk
---
+++ bnlbot/botstart/bot-1-0/script/sql/sim_best_rate.sql
@@ -0,0 +1,22 @@
+﻿
+select
+  betname,
+  round(avg(sizematched),2) size,
+  --extract(year from betplaced) yr,
+  --extract(week from betplaced) wk,
+ count('a'), 
+  sum(profit) profit,
+  round(sum(
+    case when betwon then profit*0.935
+       else profit
+    end),2) profit2,
+ 
+  round((100.0 * sum(profit)/sum(sizematched)),2) rate
+from abets
+where true  
+and status = 'MATCHED'
+and betname not like 'WIL%'
+--and betplaced > '2018-09-01'
+group by betname --,yr, wk
+order by rate desc , profit2 desc
+-- betname --, yr, wk
