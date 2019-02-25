406a987a64ae0c69c222dfa7279e2790317d69b1
bnl <bnl@fb950984-27ca-4460-a7f1-dc0a64b45e7a>
2019-02-25 21:50:34 +0000

    sql and script changes

--- bnlbot/botstart/bot-1-0/script/sql/sim_best_rate.sql
+++ bnlbot/botstart/bot-1-0/script/sql/sim_best_rate.sql
@@ -2,8 +2,8 @@
 select
   betname,
   round(avg(sizematched),2) size,
-  --extract(year from betplaced) yr,
-  --extract(week from betplaced) wk,
+  extract(year from betplaced) yr,
+  extract(week from betplaced) wk,
  count('a'), 
   sum(profit) profit,
   round(sum(
@@ -14,9 +14,9 @@ select
   round((100.0 * sum(profit)/sum(sizematched)),2) rate
 from abets
 where true  
-and status = 'MATCHED'
-and betname not like 'WIL%'
---and betplaced > '2018-09-01'
-group by betname --,yr, wk
-order by rate desc , profit2 desc
--- betname --, yr, wk
+and betname like '%PLC%'
+and status = 'SETTLED'
+and betplaced > '2018-11-01'
+group by betname, yr, wk
+order by yr, wk, betname, profit2 desc
+--order by betname, yr, wk, profit2 desc
