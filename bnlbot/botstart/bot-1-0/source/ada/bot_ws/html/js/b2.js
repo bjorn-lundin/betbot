443cbd6788ec25e19fdcfd879ddf5b2a89f3680e
bnl <b.f.lundin@gmail.com>
2019-06-30 11:28:13 +0000

    javascript fixes

--- bnlbot/botstart/bot-1-0/source/ada/bot_ws/html/js/b2.js
+++ bnlbot/botstart/bot-1-0/source/ada/bot_ws/html/js/b2.js
@@ -12,7 +12,7 @@ $.makeTable = function (mydata) {
     $.each(mydata, function (index, value) {
         var TableRow = "<tr>";
         $.each(value, function (key, val) {
-            console.log("key: " + key + " val: " + val);
+           // console.log("key: " + key + " val: " + val);
             if (typeof val == "string" && key == "betname") {
               TableRow += "<td>" + val.slice(0,18) + "</td>";
             } else if (typeof val == "string" && key == "betplaced") {
@@ -35,115 +35,121 @@ $(document).ready(function(){
          selectedRadio = $('input[name="username"]:checked', '#loginform').val();
          console.log("selectedRadio " + selectedRadio);
      });
-
-     $(document).on('click', '#submit', function() { // catch the form's submit event
-             // Send data to server through the Ajax call
-             // action is functionality we want to call and outputJSON is our data
-                 $.ajax({url: 'login',
-                     data: $('#loginform').serialize(),
-                     type: 'post',
-                     async: 'true',
-                     dataType: 'json',
-                     beforeSend: function() {
-                         // This callback function will trigger before data is sent
-                         console.log("beforeSend");
-                         $.mobile.loading( "show" );
-                     },
-                     complete: function() {
-                         // This callback function will trigger on data sent/received complete
-                         console.log("complete");
-                         $.mobile.loading( "hide" );
-                     },
-                     success: function (reply) {
-                         console.log("success");
-                         if(reply.result == "OK") {
-                            console.log("success OK");
-                            $.mobile.changePage("#menu");
-                         } else {
-                            console.log("success NOT OK");
-                         }
-                     },
-                     error: function (request,error) {
-                         console.log("error " + error);
-                     }
-                 });
-         return false; // cancel original event to prevent form submitting
-     });
-
-     $('#todays').on('tap', function() {
-         context ='todays_bets';
-         console.log("todays - context " + context);
-     });
-
-     $('#yesterday').on('tap', function() {
-         context ='yesterdays_bets';
-         console.log("yesterday - context " + context);
-     });
-
-     $('#thisweek').on('tap', function() {
-         context ='thisweeks_bets';
-         console.log("thisweek - context " + context);
-     });
-
-     $('#lastweek').on('tap', function() {
-         context ='lastweeks_bets';
-         console.log("lastweek - context " + context);
-     });
-
-     $('#thismonth').on('tap', function() {
-         context ='thismonths_bets';
-         console.log("thismonth - context " + context);
-     });
-
-     $('#lastmonth').on('tap', function() {
-         context ='lastmonths_bets';
-         console.log("lastmonth - context " + context);
-     });
-
-
-     $(document).on("pageshow","#viewer",function(){
-       // var para = "<p id='tobedeleted'>ny kod från event</p>";
-       // $('#tobedeleted').empty();
-       // $('#viewer_content').append(para);
-       $.ajax({url: '/0',
-           data: {'context' : context},
-           type: 'get',
-           async: 'true',
-           dataType: 'json',
-           beforeSend: function() {
-               // This callback function will trigger before data is sent
-               console.log("beforeSend");
-               $.mobile.loading( "show" );
-           },
-           complete: function() {
-               // This callback function will trigger on data sent/received complete
-               console.log("complete");
-               $.mobile.loading( "hide" );
-           },
-           success: function (reply) {
-               console.log("success");
-               if(reply.result == "OK") {
-                  console.log("success OK");
-
-                  $('#viewer_content').empty();
-                  $('#total').text("Resultat:"+ reply.total + " kr" );
-
-                  //fill the table
-                  var table = $.makeTable(reply.datatable);
-                  $('#viewer_content').html(table).trigger('create')
-
-
-               } else {
-                  console.log("success NOT OK");
-               }
-           },
-           error: function (request,error) {
-               console.log("error " + error);
-           }
-       });
-
-
-     });     
+     
+//  $( document ).ajaxError(function() {
+//    $('#viewer_content').empty();
+//    $('#total').text("Triggered ajaxError handler."  );
+//  });     
+       
+  
+  $(document).on('click', '#submit', function() { // catch the form's submit event
+      // Send data to server through the Ajax call
+      // action is functionality we want to call and outputJSON is our data
+      $.ajax({url: 'login',
+          data: $('#loginform').serialize(),
+          type: 'post',
+          async: 'true',
+          dataType: 'json',
+          beforeSend: function() {
+              // This callback function will trigger before data is sent
+              console.log("beforeSend");
+              $.mobile.loading( "show" );
+          },
+          complete: function() {
+              // This callback function will trigger on data sent/received complete
+              console.log("complete");
+              $.mobile.loading( "hide" );
+          },
+          success: function (reply) {
+              console.log("success");
+              if(reply.result == "OK") {
+                 console.log("success OK");
+                 $.mobile.changePage("#menu");
+              } else {
+                 console.log("success NOT OK");
+              }
+          },
+          error: function (request,error) {
+              console.log("error " + error);
+          }
+      });
+      return false; // cancel original event to prevent form submitting
+  });
+  
+  $('#todays').on('tap', function() {
+      context ='todays_bets';
+      console.log("todays - context " + context);
+  });
+  
+  $('#yesterday').on('tap', function() {
+      context ='yesterdays_bets';
+      console.log("yesterday - context " + context);
+  });
+  
+  $('#thisweek').on('tap', function() {
+      context ='thisweeks_bets';
+      console.log("thisweek - context " + context);
+  });
+  
+  $('#lastweek').on('tap', function() {
+      context ='lastweeks_bets';
+      console.log("lastweek - context " + context);
+  });
+  
+  $('#thismonth').on('tap', function() {
+      context ='thismonths_bets';
+      console.log("thismonth - context " + context);
+  });
+  
+  $('#lastmonth').on('tap', function() {
+      context ='lastmonths_bets';
+      console.log("lastmonth - context " + context);
+  });
+  
+  
+  $(document).on("pageshow","#viewer",function(){
+    // var para = "<p id='tobedeleted'>ny kod från event</p>";
+    // $('#tobedeleted').empty();
+    // $('#viewer_content').append(para);
+      $.ajax({url: '/0',
+          data: {'context' : context},
+          type: 'get',
+          async: 'true',
+          dataType: 'json',
+          beforeSend: function() {
+              // This callback function will trigger before data is sent
+              console.log("beforeSend");
+              $.mobile.loading( "show" );
+          },
+          complete: function() {
+              // This callback function will trigger on data sent/received complete
+              console.log("complete");
+              $.mobile.loading( "hide" );
+          },
+          success: function (reply) {
+              console.log("success");
+              if(reply.result == "OK") {
+                 console.log("success OK");
+    
+                 $('#viewer_content').empty();
+                 $('#total').text("Resultat:"+ reply.total + " kr" );
+    
+                 //fill the table
+                 var table = $.makeTable(reply.datatable);
+                 $('#viewer_content').html(table).trigger('create')
+    
+    
+              } else {
+                 console.log("success NOT OK");
+              }
+          },
+          error: function (request,error) {
+              console.log("error " + error);
+          }
+    });
+  
+  
+  });     
      
      
 });  
