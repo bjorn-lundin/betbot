9393a898deb5fafcf51eb0a084a6b7d7bb177e88
bnl <b.f.lundin@gmail.com>
2021-04-14 13:55:33 +0000

    better makefile

--- bnlbot/botstart/bot-1-0/source/ada/local/bot/aws_mail.adb
+++ bnlbot/botstart/bot-1-0/source/ada/local/bot/aws_mail.adb
@@ -61,22 +61,22 @@ procedure Aws_Mail is
   end Get_From_Std_In;
 
 
-
   procedure Mail(Subject : String) is
      T       : Calendar2.Time_Type := Calendar2.Clock;
      use AWS;
-     SMTP_Server_Name : constant String := "email-smtp.eu-west-1.amazonaws.com";
+  --   SMTP_Server_Name : constant String := "email-smtp.eu-west-1.amazonaws.com";
+     SMTP_Server_Name : constant String := "email-smtp.eu-north-1.amazonaws.com";
      Status : SMTP.Status;
   begin
     Ada.Directories.Set_Directory(Ada.Environment_Variables.Value("BOT_CONFIG") & "/sslcert");
     declare
       Auth : aliased constant SMTP.Authentication.Plain.Credential :=
-                                SMTP.Authentication.Plain.Initialize ("AKIAYGPN2VOGCGGBI4XE",
-                                                "Ag9otCKVee7ObYIO0Np2A6avUmZfjIGAUupYkPOB1sQf"); -- fixed by java-tool
+                                  SMTP.Authentication.Plain.Initialize ("AKIA4CCYWRUF6WBFHS4O",
+                                                  "BOYbIW5ox8Vq9+6tUkqUpo4J7gy/a7u/tErewqGDFDWW"); -- fixed by java-tool
 
       SMTP_Server : SMTP.Receiver := SMTP.Client.Initialize
                                   (SMTP_Server_Name,
-                                   Port       => 2465,
+                                   Port       => 465,
                                    Secure     => True,
                                    Credential => Auth'Unchecked_Access);
       use Ada.Characters.Latin_1;
@@ -90,7 +90,7 @@ procedure Aws_Mail is
                 );
     begin
       SMTP.Client.Send(Server  => SMTP_Server,
-                       From    => SMTP.E_Mail ("Alarm Betbot", "betbot@nonobet.com"),
+                       From    => SMTP.E_Mail ("Alarm Betbot", "b.f.lundin@gmail.com"),
                        To      => Receivers,
                        Subject => Subject,
                        Message => Msg,
