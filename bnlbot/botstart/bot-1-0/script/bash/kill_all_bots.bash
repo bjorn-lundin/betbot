b740ef4d89d74c2d85a157c206d4c061b0e6f33b
Björn Lundin <b.f.lundin@gmail.com>
2022-08-10 19:11:18 +0200

    new path and better grep

--- bnlbot/botstart/bot-1-0/script/bash/kill_all_bots.bash
+++ bnlbot/botstart/bot-1-0/script/bash/kill_all_bots.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LIST=$(ps -ef | grep /bnlbot/botstart/bot-1-0/target/bin | awk  '{print $2}')
+LIST=$(ps -ef | grep botstart/bot-1-0/target/bin | grep -v grep | awk  '{print $2}')
 
-#echo "$0 japp" > /bnlbot/botstart/bot-1-0/bevis.txt
+echo "$0 japp $(date)" > /tmp/kill_all_bots.txt
 touch /var/lock/bot
 
 for pid in $LIST ; do
