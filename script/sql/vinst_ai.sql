8891cddb7ab2e4e035fdc38dca339c9807ae0558
Björn Lundin <b.f.lundin@gmail.com>
2024-12-19 17:01:34 +0100

    new headlines

--- script/sql/vinst_ai.sql
+++ script/sql/vinst_ai.sql
@@ -33,9 +33,9 @@ group by b.betname, b.betwon
 order by b.betname, b.betwon)
 select
   substring(coalesce(bnw, bnl),19) as betname,
-  round(10*(( ow * 0.95) - coalesce(cl,0)),2) as profit,
-  round(10 * ow * 0.95,2) as netprofit,
-  round(coalesce(cl,0) * 10,2) as netloss ,
+  round(10*(( ow * 0.95) - coalesce(cl,0)),2) as netprofit,
+  round(10 * ow * 0.95,2) as grossprofit,
+  round(coalesce(cl,0) * 10,2) as grossloss ,
   round((coalesce(sw,1) / coalesce(cw,1)),2) as avg_w_odds ,
   round((coalesce(sl,1) / coalesce(cl,1)),2) as avg_l_odds ,
   round( 100.0* (1.0*coalesce(cw,0) / (1.0*coalesce(cw,0) + 1.0*coalesce(cl,0) )),2) as winrate ,
