bde7a1914163c1031f41753a63f00d857a3532c8
Björn Lundin <b.f.lundin@gmail.com>
2024-12-20 15:30:17 +0100

    less columns and better rounding

--- script/sql/vinst_ai.sql
+++ script/sql/vinst_ai.sql
@@ -24,7 +24,7 @@ betname as bnl,
 betwon as bwl
 from abets b, aprices p
 where 1=1
-and b.betplaced::date >= '2024-11-11'
+and b.betplaced::date >= '2024-12-01'
 and b.betname like 'HORSE_BACK_AI_%'
 and b.marketid = p.marketid
 and b.selectionid = p.selectionid
@@ -33,14 +33,19 @@ group by b.betname, b.betwon
 order by b.betname, b.betwon)
 select
   substring(coalesce(bnw, bnl),19) as betname,
-  round(10*(( ow * 0.95) - coalesce(cl,0)),2) as netprofit,
-  round(10 * ow * 0.95,2) as grossprofit,
-  round(coalesce(cl,0) * 10,2) as grossloss ,
+  round(10*(( ow * 0.95) - coalesce(cl,0)),0) as netprofit,
+  round(10 * ow * 0.95,0) as grossprofit,
+  round(coalesce(cl,0) * 10,0) as grossloss ,
   round((coalesce(sw,1) / coalesce(cw,1)),2) as avg_w_odds ,
   round((coalesce(sl,1) / coalesce(cl,1)),2) as avg_l_odds ,
-  round( 100.0* (1.0*coalesce(cw,0) / (1.0*coalesce(cw,0) + 1.0*coalesce(cl,0) )),2) as winrate ,
-  round(100.0*(10*(( ow * 0.95) - coalesce(cl,0)) / (10*1.0*coalesce(cw,0) + 10*1.0*coalesce(cl,0) )),2) as riskrate,
-  cw, sw, ow, cl, sl, ol 
+  round( 100.0* (1.0*coalesce(cw,0) / (1.0*coalesce(cw,0) + 1.0*coalesce(cl,0) )),1) as winrate ,
+  round(100.0*(10*(( ow * 0.95) - coalesce(cl,0)) / (10*1.0*coalesce(cw,0) + 10*1.0*coalesce(cl,0) )),1) as riskrate,
+  cw, 
+  --sw, 
+  --ow, 
+  cl 
+  --sl, 
+  --ol 
 from w
 full outer join l on w.bnw = l.bnl
 order by 2 desc
